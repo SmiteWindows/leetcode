ff8789c34adffa38183fda5b712bfec94c302ed1
SmiteWindows <x1627283564@hotmail.com>
2020-03-30 16:50:09 +0800

    202003301650

--- src/linked_list/leetcode203.rs
+++ src/linked_list/leetcode203.rs
@@ -12,7 +12,63 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
-
+/// Runtime: 4 ms
+/// Memory Usage: 2.7 MB
 pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
-    todo!()
+    let mut head=head;
+    let mut p=&mut head;
+    while p.is_some() {
+        if p.as_ref()?.val==val {
+            *p=p.take()?.next.take();
+        }else{
+            p=&mut p.as_mut()?.next;
+        }
+    }
+    head
 }
+#[test]
+fn test1_203() {
+    let l1 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 6,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode {
+                            val: 5,
+                            next: Some(Box::new(ListNode { val: 6, next: None })),
+                        })),
+                    })),
+                })),
+            })),
+        })),
+    }));
+    let l2 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 3,
+                next: Some(Box::new(ListNode {
+                    val: 4,
+                    next: Some(Box::new(ListNode { val: 5, next: None })),
+                })),
+            })),
+        })),
+    }));
+    assert_eq!(remove_elements(l1, 6), l2);
+}
+// let mut sentinel = Some(Box::new(ListNode { val: 0, next: head }));
+// let mut p = &mut sentinel;
+// while let Some(node) = &mut p.as_mut()?.next {
+//     if node.val == val {
+//         p.as_mut()?.next = node.next.take();
+//     } else {
+//         p = &mut p.as_mut()?.next;
+//     }
+// }
+// sentinel?.next
