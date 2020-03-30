d544a184f21a05643841af14956d326763483bef
SmiteWindows <x1627283564@hotmail.com>
2020-03-30 13:04:14 +0800

    202003301305

--- src/linked_list/leetcode876.rs
+++ src/linked_list/leetcode876.rs
@@ -20,8 +20,7 @@ pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let mut fast = &head;
     let mut slow = &head;
     while fast.is_some() && fast.as_ref()?.next.is_some() {
-        fast = &fast.as_ref()?.next;
-        fast = &fast.as_ref()?.next;
+        fast = &fast.as_ref()?.next.as_ref()?.next;
         slow = &slow.as_ref()?.next;
     }
     slow.clone()
