062a57e73bdc9906f0d66a40d4f243e7be5f0e81
SmiteWindows <x1627283564@hotmail.com>
2020-03-30 19:04:01 +0800

    202003301903

--- src/linked_list/leetcode206.rs
+++ src/linked_list/leetcode206.rs
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/reverse-linked-list/
+/// 反转一个单链表。你可以迭代或递归地反转链表。
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -12,7 +13,45 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
-
+/// Runtime: 0 ms
+/// Memory Usage: 2.3 MB
 pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    todo!()
+    let mut head = head;
+    let mut prev = None;
+    while let Some(mut node)=head{
+        head= node.next.take();
+        node.next =prev;
+        prev=Some(node);
+    }
+    prev
+}
+#[test]
+fn test1_206() {
+    let l1 = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 3,
+                next: Some(Box::new(ListNode {
+                    val: 4,
+                    next: Some(Box::new(ListNode { val: 5, next: None })),
+                })),
+            })),
+        })),
+    }));
+    let l2 = Some(Box::new(ListNode {
+        val: 5,
+        next: Some(Box::new(ListNode {
+            val: 4,
+            next: Some(Box::new(ListNode {
+                val: 3,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode { val: 1, next: None })),
+                })),
+            })),
+        })),
+    }));
+    assert_eq!(l2, reverse_list(l1));
 }
