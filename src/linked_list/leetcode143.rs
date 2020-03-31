c02049f2fa21efdc3f37f5a80161ebf739140d71
SmiteWindows <x1627283564@hotmail.com>
2020-03-31 20:37:53 +0800

    202003312037

--- src/linked_list/leetcode143.rs
+++ src/linked_list/leetcode143.rs
@@ -12,9 +12,33 @@ impl ListNode {
         Self { next: None, val }
     }
 }
-
+/// Runtime: 8 ms
+/// Memory Usage: 4.1 MB
+use std::collections::VecDeque;
 pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
-    todo!()
+    let mut p=head.take();
+    let mut deque:VecDeque<Option<Box<ListNode>>>=VecDeque::new();
+    while let Some(mut n)=p{
+        p=n.next.take();
+        deque.push_back(Some(n));
+    }
+    let mut stack:Vec<Option<Box<ListNode>>>=vec![];
+    let mut direction=true;
+    while !deque.is_empty(){
+        if direction {
+            stack.push(deque.pop_front().unwrap());
+        } else {
+            stack.push(deque.pop_back().unwrap());
+        }
+        direction=!direction;
+    }
+    let mut prev: Option<Box<ListNode>> = None;
+    while let Some(last)=stack.pop(){
+        let mut node=last.unwrap();
+        node.next =prev;
+        prev=Some(node);
+    }
+    *head=prev;
 }
 #[test]
 fn test1_143() {
