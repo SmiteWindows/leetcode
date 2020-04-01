0c1a6b181106d2266f79eec8d526e88c186d4ff1
SmiteWindows <x1627283564@hotmail.com>
2020-04-01 17:37:13 +0800

    202004011737

--- src/linked_list/leetcode817.rs
+++ src/linked_list/leetcode817.rs
@@ -12,7 +12,67 @@ impl ListNode {
         Self { next: None, val }
     }
 }
-
+/// Runtime: 8 ms
+/// Memory Usage: 2.8 MB
+use std::collections::HashSet;
 pub fn num_components(head: Option<Box<ListNode>>, g: Vec<i32>) -> i32 {
-    todo!()
+    let mut head = head;
+    let mut gset = HashSet::new();
+    let mut res = 0;
+    for x in g {
+        gset.insert(x);
+    }
+    let mut cur = &mut head;
+    while cur.is_some() {
+        if gset.contains(&cur.as_mut().unwrap().val)
+            && (cur.as_mut().unwrap().next.is_none()
+                || !gset.contains(&cur.as_mut().unwrap().next.as_mut().unwrap().val))
+        {
+            res += 1;
+        }
+        cur = &mut cur.as_mut().unwrap().next;
+    }
+    res
+}
+#[test]
+fn test1_817() {
+    let l1 = Some(Box::new(ListNode {
+        val: 0,
+        next: Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode { val: 3, next: None })),
+            })),
+        })),
+    }));
+    let l2 = Some(Box::new(ListNode {
+        val: 0,
+        next: Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode { val: 4, next: None })),
+                })),
+            })),
+        })),
+    }));
+    let l3 = Some(Box::new(ListNode {
+        val: 3,
+        next: Some(Box::new(ListNode {
+            val: 4,
+            next: Some(Box::new(ListNode {
+                val: 0,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode { val: 1, next: None })),
+                })),
+            })),
+        })),
+    }));
+    assert_eq!(num_components(l3, vec![4]), 1);
+    assert_eq!(num_components(l1, vec![0, 1, 3]), 2);
+    assert_eq!(num_components(l2, vec![0, 3, 1, 4]), 2);
 }
