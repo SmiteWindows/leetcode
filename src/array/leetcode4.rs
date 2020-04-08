e7ecebd9d986acbd75463e59bff77f8610406148
SmiteWindows <x1627283564@hotmail.com>
2020-04-08 18:28:57 +0800

    202004081828

--- src/array/leetcode4.rs
+++ src/array/leetcode4.rs
@@ -1,4 +1,18 @@
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
 pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    let m=nums1.len();
+    let n=nums2.len();
     todo!()
 }
+// array binary_search divide_and_conquer
+// #[test]
+// fn test1_4(){
+//     let nums1 =vec![1,3];
+//     let nums2 =vec![2];
+//     let res=2.0;
+//     assert_eq!(res, find_median_sorted_arrays(nums1,nums2));
+//     let nums1 =vec![1,2];
+//     let nums2 =vec![3,4];
+//     let res=2.5;
+//     assert_eq!(res, find_median_sorted_arrays(nums1,nums2));
+// }
