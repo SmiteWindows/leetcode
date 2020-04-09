501c2ba3355d03beaf74f013079913db0b1791af
SmiteWindows <x1627283564@hotmail.com>
2020-04-09 10:26:48 +0800

    202004091026

--- src/breadth_first_search/leetcode101.rs
+++ src/breadth_first_search/leetcode101.rs
@@ -22,3 +22,35 @@ use std::{cell::RefCell, rc::Rc};
 pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
     todo!()
 }
+// tree depth_first_search breadth_first_search
+// #[test]
+// fn test2_101() {
+//     let root1 = Some(Rc::new(RefCell::new(TreeNode {
+//         val: 1,
+//         left: Some(Rc::new(RefCell::new(TreeNode {
+//             val: 2,
+//             left: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+//             right: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+//         }))),
+//         right: Some(Rc::new(RefCell::new(TreeNode {
+//             val: 2,
+//             left: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+//             right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+//         }))),
+//     })));
+//     assert_eq!(true,is_symmetric(root1));
+//     let root2 = Some(Rc::new(RefCell::new(TreeNode {
+//         val: 1,
+//         left: Some(Rc::new(RefCell::new(TreeNode {
+//             val: 2,
+//             left: None,
+//             right: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+//         }))),
+//         right: Some(Rc::new(RefCell::new(TreeNode {
+//             val: 2,
+//             left: None,
+//             right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+//         }))),
+//     })));
+//     assert_eq!(false,is_symmetric(root2));
+// }
--- src/depth_first_search/leetcode101.rs
+++ src/depth_first_search/leetcode101.rs
@@ -18,7 +18,52 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, rc::Rc};
-
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-    todo!()
+    fn is_mirror(p: Option<&Rc<RefCell<TreeNode>>>, q: Option<&Rc<RefCell<TreeNode>>>) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (None, _) | (_, None) => false,
+            (Some(pr), Some(qr)) => {
+                let (pr, qr) = (pr.as_ref().borrow(), qr.as_ref().borrow());
+                (pr.val == qr.val)
+                    && is_mirror(pr.right.as_ref(), qr.left.as_ref())
+                    && is_mirror(pr.left.as_ref(), qr.right.as_ref())
+            }
+        }
+    }
+    is_mirror(root.as_ref(), root.as_ref())
+}
+// tree depth_first_search breadth_first_search
+#[test]
+fn test3_101() {
+    let root1 = Some(Rc::new(RefCell::new(TreeNode {
+        val: 1,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+            right: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+            right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+        }))),
+    })));
+    assert_eq!(true,is_symmetric(root1));
+    let root2 = Some(Rc::new(RefCell::new(TreeNode {
+        val: 1,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+        }))),
+    })));
+    assert_eq!(false,is_symmetric(root2));
 }
