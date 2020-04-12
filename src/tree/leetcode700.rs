4080a6a65b381b373bc8f48b61aa8104c9abac4b
SmiteWindows <x1627283564@hotmail.com>
2020-04-12 12:54:13 +0800

    202004121254

--- src/tree/leetcode700.rs
+++ src/tree/leetcode700.rs
@@ -18,15 +18,48 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, rc::Rc};
-
+// Runtime: 4 ms
+// Memory Usage: 2.5 MB
 pub fn search_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    // if let Some(node) = root {
+    //     if node.borrow().val == val {
+    //         Some(node)
+    //     } else if node.borrow().val > val {
+    //         search_bst(node.borrow().left.clone(), val)
+    //     } else {
+    //         search_bst(node.borrow().right.clone(), val)
+    //     }
+    // } else {
+    //     None
+    // }
+    fn search(root: Option<&Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = root {
+            if node.as_ref().borrow().val == val {
+                Some(node.clone())
+            } else if node.as_ref().borrow().val > val {
+                search(node.as_ref().borrow().left.as_ref(), val)
+            } else {
+                search(node.as_ref().borrow().right.as_ref(), val)
+            }
+        } else {
+            None
+        }
+    }
+    search(root.as_ref(), val)
 }
 // tree
 #[test]
-#[ignore]
 fn test1_700() {
-    let root = Some(Rc::new(RefCell::new(TreeNode {
+    let t1 = Some(Rc::new(RefCell::new(TreeNode {
+        val: 4,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: Some(Rc::new(RefCell::new(TreeNode::new(1)))),
+            right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode::new(7)))),
+    })));
+    let t2 = Some(Rc::new(RefCell::new(TreeNode {
         val: 4,
         left: Some(Rc::new(RefCell::new(TreeNode {
             val: 2,
@@ -40,5 +73,6 @@ fn test1_700() {
         left: Some(Rc::new(RefCell::new(TreeNode::new(1)))),
         right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
     })));
-    assert_eq!(res, search_bst(root, 2));
+    assert_eq!(res, search_bst(t1, 2));
+    assert_eq!(None, search_bst(t2, 5));
 }
