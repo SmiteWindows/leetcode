b3b901a98579640b9fb30638778080a25ab7c212
SmiteWindows <x1627283564@hotmail.com>
2020-04-13 14:59:37 +0800

    202004131459

--- src/tree/leetcode226.rs
+++ src/tree/leetcode226.rs
@@ -18,13 +18,24 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, rc::Rc};
-
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    fn invert(root: Option<&Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = root {
+            let left = invert(node.borrow().left.as_ref());
+            let right = invert(node.borrow().right.as_ref());
+            node.as_ref().borrow_mut().left = right;
+            node.as_ref().borrow_mut().right = left;
+            Some(node.clone())
+        } else {
+            None
+        }
+    }
+    invert(root.as_ref())
 }
 // tree
 #[test]
-#[ignore]
 fn test1_226() {
     let root = Some(Rc::new(RefCell::new(TreeNode {
         val: 4,
