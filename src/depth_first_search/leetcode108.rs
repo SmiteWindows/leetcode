ab11f924c6e220f3b3b97605361247ac283abad6
SmiteWindows <x1627283564@hotmail.com>
2020-04-13 15:49:24 +0800

    202004131549

--- src/depth_first_search/leetcode108.rs
+++ src/depth_first_search/leetcode108.rs
@@ -18,13 +18,27 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, rc::Rc};
-
+// Runtime: 0 ms
+// Memory Usage: 2.9 MB
 pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    fn helper(left: i32, right: i32, nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if left > right {
+            return None;
+        }
+        let mut p = (left + right) as usize / 2;
+        if (left + right) % 2 == 1 {
+            p += 1;
+        }
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(nums[p]))));
+        root.as_ref()?.borrow_mut().left = helper(left, p as i32 - 1, nums);
+        root.as_ref()?.borrow_mut().right = helper(p as i32 + 1, right, nums);
+        root
+    }
+    let n = nums.len() as i32 - 1;
+    helper(0, n, nums.as_slice())
 }
 // tree depth_first_search
 #[test]
-#[ignore]
 fn test2_108() {
     let nums = vec![-10, -3, 0, 5, 9];
     let res = Some(Rc::new(RefCell::new(TreeNode {
--- src/tree/leetcode108.rs
+++ src/tree/leetcode108.rs
@@ -18,26 +18,37 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, rc::Rc};
-
+// Runtime: 0 ms
+// Memory Usage: 2.8 MB
 pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    fn helper(left: i32, right: i32, nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if left > right {
+            return None;
+        }
+        let p = (left + right) as usize / 2;
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(nums[p]))));
+        root.as_ref()?.borrow_mut().left = helper(left, p as i32 - 1, nums);
+        root.as_ref()?.borrow_mut().right = helper(p as i32 + 1, right, nums);
+        root
+    }
+    let n = nums.len() as i32 - 1;
+    helper(0, n, nums.as_slice())
 }
 // tree depth_first_search
 #[test]
-#[ignore]
 fn test1_108() {
     let nums = vec![-10, -3, 0, 5, 9];
     let res = Some(Rc::new(RefCell::new(TreeNode {
         val: 0,
         left: Some(Rc::new(RefCell::new(TreeNode {
-            val: -3,
-            left: Some(Rc::new(RefCell::new(TreeNode::new(-10)))),
-            right: None,
+            val: -10,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode::new(-3)))),
         }))),
         right: Some(Rc::new(RefCell::new(TreeNode {
-            val: 9,
-            left: Some(Rc::new(RefCell::new(TreeNode::new(5)))),
-            right: None,
+            val: 5,
+            left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode::new(9)))),
         }))),
     })));
     assert_eq!(res, sorted_array_to_bst(nums));
