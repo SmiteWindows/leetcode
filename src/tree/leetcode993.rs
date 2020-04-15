6d41a293037687b732c6cf75e205fcc181d9d25b
SmiteWindows <x1627283564@hotmail.com>
2020-04-15 10:14:23 +0800

    202004151014

--- src/tree/leetcode993.rs
+++ src/tree/leetcode993.rs
@@ -17,14 +17,45 @@ impl TreeNode {
         }
     }
 }
-use std::{cell::RefCell, rc::Rc};
-
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn is_cousins(root: Option<Rc<RefCell<TreeNode>>>, x: i32, y: i32) -> bool {
-    todo!()
+    fn walk(
+        root: Option<&Rc<RefCell<TreeNode>>>,
+        par: Option<Rc<RefCell<TreeNode>>>,
+        depth: &mut HashMap<i32, i32>,
+        parent: &mut HashMap<i32, Option<Rc<RefCell<TreeNode>>>>,
+    ) {
+        if let Some(node) = root {
+            if let Some(n) = par.as_ref() {
+                let val = n.borrow().val;
+                depth.insert(node.borrow().val, 1 + depth.get(&val).unwrap());
+            } else {
+                depth.insert(node.borrow().val, 0);
+            }
+            parent.insert(node.borrow().val, par);
+            walk(
+                node.borrow().left.as_ref(),
+                Some(node.clone()),
+                depth,
+                parent,
+            );
+            walk(
+                node.borrow().right.as_ref(),
+                Some(node.clone()),
+                depth,
+                parent,
+            );
+        }
+    }
+    let mut depth = HashMap::new();
+    let mut parent = HashMap::new();
+    walk(root.as_ref(), None, &mut depth, &mut parent);
+    depth.get(&x) == depth.get(&y) && parent.get(&x) != parent.get(&y)
 }
 // tree breadth_first_search
 #[test]
-#[ignore]
 fn test1_993() {
     let t1 = Some(Rc::new(RefCell::new(TreeNode {
         val: 1,
