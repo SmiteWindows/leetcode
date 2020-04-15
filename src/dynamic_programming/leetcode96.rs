48d9f29ec5ddf1bde65a08ea726ff9549e4b9af4
SmiteWindows <x1627283564@hotmail.com>
2020-04-15 19:25:35 +0800

    202004151925

--- src/dynamic_programming/leetcode96.rs
+++ src/dynamic_programming/leetcode96.rs
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/unique-binary-search-trees/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn num_trees(n: i32) -> i32 {
-    todo!()
+    let n=n as usize;
+    let mut g=vec![0;n+1];
+    g[0] =1;
+    g[1]=1;
+    for i in 2..=n{
+        for j in 1..=i{
+            g[i]+=g[j-1]*g[i-j];
+        }
+    }
+    g[n]
 }
 // tree dynamic_programming
 #[test]
-#[ignore]
 fn test2_96() {
     assert_eq!(num_trees(3), 5);
+    assert_eq!(num_trees(19), 1767263190);
 }
--- src/tree/leetcode96.rs
+++ src/tree/leetcode96.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/unique-binary-search-trees/
+use std::convert::TryInto;
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn num_trees(n: i32) -> i32 {
-    todo!()
+    let mut catalan =1 as i64;
+    let n=n as i64;
+    for i in 0..n {
+        catalan=catalan*2*(2*i+1)/(i+2);
+    }
+    catalan.try_into().unwrap()
 }
 // tree dynamic_programming
 #[test]
-#[ignore]
 fn test1_96() {
     assert_eq!(num_trees(3), 5);
+    assert_eq!(num_trees(19), 1767263190);
 }
