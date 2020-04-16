0cee376b1d7304aaa168853a2258e73f88aae40a
SmiteWindows <x1627283564@hotmail.com>
2020-04-16 18:41:24 +0800

    202004161841

--- src/depth_first_search/leetcode105.rs
+++ src/depth_first_search/leetcode105.rs
@@ -18,56 +18,13 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
-// Runtime: 0 ms
-// Memory Usage: 2.8 MB
-pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-    fn add_node(
-        preorder: &[i32],
-        inorder: &[i32],
-        map: &HashMap<i32, usize>,
-        pre_root_index: usize,
-        left: usize,
-        right: usize,
-    ) -> Option<Rc<RefCell<TreeNode>>> {
-        if left > right {
-            return None;
-        }
-        let cur = preorder[pre_root_index];
-        let mut node = Some(Rc::new(RefCell::new(TreeNode::new(cur))));
-        if left == right {
-            return node;
-        }
 
-        let ino_root_index = map[&cur];
-        node.as_mut().unwrap().borrow_mut().left = add_node(
-            preorder,
-            inorder,
-            map,
-            pre_root_index + 1,
-            left,
-            ino_root_index - 1,
-        );
-        node.as_mut().unwrap().borrow_mut().right = add_node(
-            preorder,
-            inorder,
-            map,
-            pre_root_index + 1 + ino_root_index - left,
-            ino_root_index + 1,
-            right,
-        );
-        node
-    }
-    if preorder.is_empty() {
-        return None;
-    }
-    let mut map = HashMap::new();
-    for (index, num) in inorder.iter().enumerate() {
-        map.insert(*num, index);
-    }
-    add_node(&preorder, &inorder, &map, 0, 0, preorder.len() - 1)
+pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+    todo!()
 }
 // tree depth_first_search array
 #[test]
+#[ignore]
 fn test3_105() {
     let res = Some(Rc::new(RefCell::new(TreeNode {
         val: 3,
