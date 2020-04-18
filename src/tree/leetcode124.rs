9df1865787261c78e72b9641cfb7625f0b76dcc4
SmiteWindows <x1627283564@hotmail.com>
2020-04-18 16:37:56 +0800

    202004181637

--- src/tree/leetcode124.rs
+++ src/tree/leetcode124.rs
@@ -17,14 +17,27 @@ impl TreeNode {
         }
     }
 }
-use std::{cell::RefCell, rc::Rc};
-
+use std::{cell::RefCell, cmp::max, rc::Rc};
+// Runtime: 4 ms
+// Memory Usage: 4.2 MB
 pub fn max_path_sum(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    todo!()
+    fn max_gain(root: Option<&Rc<RefCell<TreeNode>>>, max_sum: &mut i32) -> i32 {
+        if let Some(node) = root {
+            let val=node.borrow().val;
+            let left_gain = max(max_gain(node.borrow().left.as_ref(), max_sum), 0);
+            let right_gain = max(max_gain(node.borrow().right.as_ref(), max_sum), 0);
+            *max_sum = max(*max_sum, val + left_gain + right_gain);
+            val + max(left_gain, right_gain)
+        } else {
+            0
+        }
+    }
+    let mut max_sum = std::i32::MIN;
+    max_gain(root.as_ref(), &mut max_sum);
+    max_sum
 }
 // tree depth_first_search
 #[test]
-#[ignore]
 fn test1_124() {
     let root1 = Some(Rc::new(RefCell::new(TreeNode {
         val: 1,
