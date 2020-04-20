e397c2c37ac47a16317febea09c296872f0ec839
SmiteWindows <x1627283564@hotmail.com>
2020-04-20 18:02:03 +0800

    202004201801

--- src/linked_list/leetcode1367.rs
+++ src/linked_list/leetcode1367.rs
@@ -16,8 +16,8 @@ impl ListNode {
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
     pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub left: Option<Rc<RefCell<Self>>>,
+    pub right: Option<Rc<RefCell<Self>>>,
 }
 
 impl TreeNode {
