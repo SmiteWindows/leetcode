b0041515cff36cdea072d808e43e0f21f870c28c
SmiteWindows <x1627283564@hotmail.com>
2020-04-21 20:36:44 +0800

    202004212036

--- src/tree/leetcode623.rs
+++ src/tree/leetcode623.rs
@@ -18,17 +18,42 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, rc::Rc};
-
+// Runtime: 0 ms
+// Memory Usage: 2.7 MB
 pub fn add_one_row(
     root: Option<Rc<RefCell<TreeNode>>>,
     v: i32,
     d: i32,
 ) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    fn insert(root: Option<&mut Rc<RefCell<TreeNode>>>, value: i32, depth: i32, n: i32) {
+        if let Some(node) = root {
+            let mut node = node.borrow_mut();
+            if depth == n - 1 {
+                let mut t = node.left.take();
+                node.left = Some(Rc::new(RefCell::new(TreeNode::new(value))));
+                node.left.as_mut().unwrap().borrow_mut().left = t;
+                t = node.right.take();
+                node.right = Some(Rc::new(RefCell::new(TreeNode::new(value))));
+                node.right.as_mut().unwrap().borrow_mut().right = t;
+            }
+            {
+                insert(node.left.as_mut(), value, depth + 1, n);
+                insert(node.right.as_mut(), value, depth + 1, n);
+            }
+        }
+    }
+
+    if d == 1 {
+        let mut n = TreeNode::new(v);
+        n.left = root;
+        return Some(Rc::new(RefCell::new(n)));
+    }
+    let mut root = root;
+    insert(root.as_mut(), v, 1, d);
+    root
 }
 // tree
 #[test]
-#[ignore]
 fn test1_623() {
     let t1 = Some(Rc::new(RefCell::new(TreeNode {
         val: 4,
