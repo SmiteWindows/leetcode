c2b9b0fbd9827dbb234cd38fd50aeb9a3153c9e8
SmiteWindows <x1627283564@hotmail.com>
2020-04-21 19:32:44 +0800

    202004211932

--- src/tree/leetcode652.rs
+++ src/tree/leetcode652.rs
@@ -18,46 +18,41 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
-
+// Runtime: 12 ms
+// Memory Usage: 10.4 MB
 pub fn find_duplicate_subtrees(
     root: Option<Rc<RefCell<TreeNode>>>,
 ) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
     fn walk(
         root: Option<&Rc<RefCell<TreeNode>>>,
-        trees: &mut HashMap<String, i32>,
-        count: &mut HashMap<i32, i32>,
+        count: &mut HashMap<String, i32>,
         ans: &mut Vec<Option<Rc<RefCell<TreeNode>>>>,
-        t: i32,
-    ) -> i32 {
+    ) -> String {
         if let Some(n) = root {
             let node = n.borrow();
             let serial = node.val.to_string()
                 + ","
-                + &walk(node.left.as_ref(), trees, count, ans, t).to_string()
+                + &walk(node.left.as_ref(), count, ans)
                 + ","
-                + &walk(node.right.as_ref(), trees, count, ans, t).to_string();
-            let uid = *trees.entry(serial).or_insert(t + 1);
-            let v = *count.entry(uid).or_default() + 1;
-            count.insert(uid, v);
-            if count.get(&uid).unwrap() == &2 {
-                dbg!();
+                + &walk(node.right.as_ref(), count, ans);
+            let v = *count.entry(serial.clone()).or_default() + 1;
+            count.insert(serial.clone(), v);
+            if count.get(&serial).unwrap() == &2 {
                 ans.push(Some(n.clone()));
             }
-            uid
+            serial
         } else {
-            0
+            String::from("#")
         }
     }
 
-    let mut trees = HashMap::new();
     let mut count = HashMap::new();
     let mut ans = Vec::new();
-    walk(root.as_ref(), &mut trees, &mut count, &mut ans, 1);
+    walk(root.as_ref(), &mut count, &mut ans);
     ans
 }
 // tree
 #[test]
-#[ignore]
 fn test1_652() {
     let t = Some(Rc::new(RefCell::new(TreeNode {
         val: 1,
@@ -76,12 +71,12 @@ fn test1_652() {
             right: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
         }))),
     })));
-    let r1 = Some(Rc::new(RefCell::new(TreeNode {
+    let r2 = Some(Rc::new(RefCell::new(TreeNode {
         val: 2,
         left: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
         right: None,
     })));
-    let r2 = Some(Rc::new(RefCell::new(TreeNode::new(4))));
+    let r1 = Some(Rc::new(RefCell::new(TreeNode::new(4))));
     let res = vec![r1, r2];
     assert_eq!(res, find_duplicate_subtrees(t));
 }
