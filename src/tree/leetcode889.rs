68eb8c09de078ef4fe362391462d979afeba1290
SmiteWindows <x1627283564@hotmail.com>
2020-04-23 15:17:18 +0800

    202004231517

--- src/tree/leetcode889.rs
+++ src/tree/leetcode889.rs
@@ -18,16 +18,36 @@ impl TreeNode {
     }
 }
 use std::{cell::RefCell, rc::Rc};
-
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn construct_from_pre_post(pre: Vec<i32>, post: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    fn helper(
+        i: &mut usize,
+        j: &mut usize,
+        pre: &[i32],
+        post: &[i32],
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let val = pre[*i];
+        *i += 1;
+        let mut left = None;
+        let mut right = None;
+        if val != post[*j] {
+            left = helper(i, j, pre, post);
+        }
+        if val != post[*j] {
+            right = helper(i, j, pre, post);
+        }
+        *j += 1;
+        Some(Rc::new(RefCell::new(TreeNode { val, left, right })))
+    }
+
+    helper(&mut 0, &mut 0, &pre, &post)
 }
 // tree
 #[test]
-#[ignore]
 fn test1_889() {
-    let pre = vec![1, 2, 4, 5, 3, 6, 7]; // root left right
-    let post = vec![4, 5, 2, 6, 7, 3, 1]; // left right root
+    let pre = vec![1, 2, 4, 5, 3, 6, 7];
+    let post = vec![4, 5, 2, 6, 7, 3, 1];
     let res = Some(Rc::new(RefCell::new(TreeNode {
         val: 1,
         left: Some(Rc::new(RefCell::new(TreeNode {
