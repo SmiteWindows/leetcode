4e0b3b44eef215b98c30dd5d742a2d69c9ccfdb0
SmiteWindows <x1627283564@hotmail.com>
2020-04-25 15:40:29 +0800

    202004251540

--- src/tree/leetcode1123.rs
+++ src/tree/leetcode1123.rs
@@ -17,14 +17,28 @@ impl TreeNode {
         }
     }
 }
-use std::{cell::RefCell, rc::Rc};
-
+use std::{cell::RefCell, rc::Rc, cmp::Ordering};
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn lca_deepest_leaves(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    fn helper(root: Option<&Rc<RefCell<TreeNode>>>) -> (usize, Option<Rc<RefCell<TreeNode>>>) {
+        if let Some(node) = root {
+            let left = helper(node.borrow().left.as_ref());
+            let right = helper(node.borrow().right.as_ref());
+            match left.0.cmp(&right.0) {
+                Ordering::Equal => (left.0 + 1, Some(node.clone())),
+                Ordering::Less => (right.0 + 1, right.1),
+                Ordering::Greater => (left.0 + 1, left.1),
+            }
+        } else {
+            (0, None)
+        }
+    }
+
+    helper(root.as_ref()).1
 }
 // tree depth_first_search
 #[test]
-#[ignore]
 fn test1_1123() {
     let t1 = Some(Rc::new(RefCell::new(TreeNode {
         val: 1,
