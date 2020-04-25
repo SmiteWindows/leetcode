1cdea6de0d12aab66bdd5896326abdb0ff9c9e25
SmiteWindows <x1627283564@hotmail.com>
2020-04-25 15:03:53 +0800

    202004251503

--- src/depth_first_search/leetcode1110.rs
+++ src/depth_first_search/leetcode1110.rs
@@ -17,17 +17,52 @@ impl TreeNode {
         }
     }
 }
-use std::{cell::RefCell, rc::Rc};
-
+use std::{cell::RefCell, collections::HashSet, rc::Rc};
+// Runtime: 4 ms
+// Memory Usage: 2.1 MB
 pub fn del_nodes(
     root: Option<Rc<RefCell<TreeNode>>>,
     to_delete: Vec<i32>,
 ) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
-    todo!()
+    fn helper(
+        root: Option<&Rc<RefCell<TreeNode>>>,
+        is_add: bool,
+        delete_set: &mut HashSet<i32>,
+        res: &mut Vec<Option<Rc<RefCell<TreeNode>>>>,
+    ) -> bool {
+        if let Some(node) = root {
+            if delete_set.contains(&node.borrow().val) {
+                if helper(node.borrow().left.as_ref(), true, delete_set, res) {
+                    node.borrow_mut().left = None;
+                }
+                if helper(node.borrow().right.as_ref(), true, delete_set, res) {
+                    node.borrow_mut().right = None;
+                }
+                return true;
+            }
+            if is_add {
+                res.push(Some(node.clone()));
+            }
+            if helper(node.borrow().left.as_ref(), false, delete_set, res) {
+                node.borrow_mut().left = None;
+            }
+            if helper(node.borrow().right.as_ref(), false, delete_set, res) {
+                node.borrow_mut().right = None;
+            }
+        }
+        false
+    }
+
+    let mut delete_set = HashSet::with_capacity(to_delete.len());
+    let mut res = Vec::new();
+    for delete in to_delete {
+        delete_set.insert(delete);
+    }
+    helper(root.as_ref(), true, &mut delete_set, &mut res);
+    res
 }
 // tree depth_first_search
 #[test]
-#[ignore]
 fn test2_1110() {
     let root = Some(Rc::new(RefCell::new(TreeNode {
         val: 1,
@@ -47,13 +82,9 @@ fn test2_1110() {
         left: Some(Rc::new(RefCell::new(TreeNode {
             val: 2,
             left: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
-            right: Some(Rc::new(RefCell::new(TreeNode::new(5)))),
+            right: None,
         }))),
-        right: Some(Rc::new(RefCell::new(TreeNode {
-            val: 3,
-            left: Some(Rc::new(RefCell::new(TreeNode::new(6)))),
-            right: Some(Rc::new(RefCell::new(TreeNode::new(7)))),
-        }))),
+        right: None,
     })));
     let s2 = Some(Rc::new(RefCell::new(TreeNode::new(6))));
     let s3 = Some(Rc::new(RefCell::new(TreeNode::new(7))));
