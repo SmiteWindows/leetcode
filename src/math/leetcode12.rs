be7b84e741eeee116b513564b6b1110c546dc845
SmiteWindows <x1627283564@hotmail.com>
2020-04-30 13:22:40 +0800

    202004301322

--- src/math/leetcode12.rs
+++ src/math/leetcode12.rs
@@ -3,3 +3,12 @@ pub fn int_to_roman(num: i32) -> String {
     todo!()
 }
 // math string
+#[test]
+#[ignore]
+fn test2_12() {
+    assert_eq!(int_to_roman(3), String::from("III"));
+    assert_eq!(int_to_roman(4), String::from("IV"));
+    assert_eq!(int_to_roman(9), String::from("IX"));
+    assert_eq!(int_to_roman(58), String::from("LVIII"));
+    assert_eq!(int_to_roman(1994), String::from("MCMXCIV"));
+}
--- src/math/leetcode13.rs
+++ src/math/leetcode13.rs
@@ -2,3 +2,13 @@
 pub fn roman_to_int(s: String) -> i32 {
     todo!()
 }
+// math string
+#[test]
+#[ignore]
+fn test2_13() {
+    assert_eq!(roman_to_int(String::from("III")), 3);
+    assert_eq!(roman_to_int(String::from("IV")), 4);
+    assert_eq!(roman_to_int(String::from("IX")), 9);
+    assert_eq!(roman_to_int(String::from("LVIII")), 58);
+    assert_eq!(roman_to_int(String::from("MCMXCIV")), 1994);
+}
