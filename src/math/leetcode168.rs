2fc580647008aaeabba78c53ce260028fa071889
SmiteWindows <x1627283564@hotmail.com>
2020-04-30 15:57:36 +0800

    202004301557

--- src/math/leetcode168.rs
+++ src/math/leetcode168.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/excel-sheet-column-title/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn convert_to_title(n: i32) -> String {
-    todo!()
+    let mut n = n;
+    let mut v = vec![];
+    while n > 0 {
+        let x = ((n - 1) % 26) as u8;
+        let c = (x + b'A') as char;
+        v.insert(0, c);
+        n = (n - 1) / 26;
+    }
+    v.iter().collect()
 }
 // math
 #[test]
-#[ignore]
 fn test1_168() {
     assert_eq!(convert_to_title(1), String::from("A"));
     assert_eq!(convert_to_title(28), String::from("AB"));
--- src/math/leetcode171.rs
+++ src/math/leetcode171.rs
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/excel-sheet-column-number/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn title_to_number(s: String) -> i32 {
-    todo!()
+    s.bytes()
+        .fold(0, |sum, c| sum * 26 + i32::from(c) - 'A' as i32 + 1)
 }
 // math
 #[test]
-#[ignore]
 fn test1_171() {
     assert_eq!(title_to_number(String::from("A")), 1);
     assert_eq!(title_to_number(String::from("AB")), 28);
