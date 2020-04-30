9bd9eb37801c22b21e381274c524f05a1aae2af5
SmiteWindows <x1627283564@hotmail.com>
2020-04-30 20:02:09 +0800

    202004302002

--- src/dynamic_programming/leetcode368.rs
+++ src/dynamic_programming/leetcode368.rs
@@ -2,3 +2,10 @@
 pub fn largest_divisible_subset(nums: Vec<i32>) -> Vec<i32> {
     todo!()
 }
+// math dynamic_programming
+#[test]
+#[ignore]
+fn test2_368() {
+    assert_eq!(largest_divisible_subset(vec![1, 2, 3]), vec![1, 2]);
+    assert_eq!(largest_divisible_subset(vec![1, 2, 4, 8]), vec![1, 2, 4, 8]);
+}
--- src/math/leetcode372.rs
+++ src/math/leetcode372.rs
@@ -2,3 +2,10 @@
 pub fn super_pow(a: i32, b: Vec<i32>) -> i32 {
     todo!()
 }
+// math
+#[test]
+#[ignore]
+fn test1_372() {
+    assert_eq!(super_pow(2, vec![3]), 8);
+    assert_eq!(super_pow(2, vec![1, 0]), 1024);
+}
