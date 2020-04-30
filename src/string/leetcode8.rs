cffff5d685119ee2cb63b6a071f1209d35c481d7
SmiteWindows <x1627283564@hotmail.com>
2020-04-30 12:51:49 +0800

    202004301251

--- src/string/leetcode8.rs
+++ src/string/leetcode8.rs
@@ -2,3 +2,13 @@
 pub fn my_atoi(str: String) -> i32 {
     todo!()
 }
+// math string
+#[test]
+#[ignore]
+fn test1_8() {
+    assert_eq!(my_atoi(String::from("42")), 42);
+    assert_eq!(my_atoi(String::from("   -42")), -42);
+    assert_eq!(my_atoi(String::from("4193 with words")), 4193);
+    assert_eq!(my_atoi(String::from("words and 987")), 0);
+    assert_eq!(my_atoi(String::from("-91283472332")), -2147483648);
+}
