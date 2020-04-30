e123677581482a06fe7d5aebe3b0f8a53fbcee3c
SmiteWindows <x1627283564@hotmail.com>
2020-04-30 20:35:58 +0800

    202004302035

--- src/math/leetcode335.rs
+++ src/math/leetcode335.rs
@@ -4,6 +4,7 @@ pub fn is_self_crossing(x: Vec<i32>) -> bool {
 }
 // math
 #[test]
+#[ignore]
 fn test1_335() {
     assert_eq!(is_self_crossing(vec![2, 1, 1, 2]), true);
     assert_eq!(is_self_crossing(vec![1, 2, 3, 4]), false);
