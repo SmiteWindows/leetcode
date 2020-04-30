99985adc9c379ee06597df42a1f95b30855bee95
SmiteWindows <x1627283564@hotmail.com>
2020-04-30 19:38:17 +0800

    202004301938

--- src/binary_search/leetcode367.rs
+++ src/binary_search/leetcode367.rs
@@ -1,10 +1,27 @@
 // https://leetcode.com/problems/valid-perfect-square/
+use std::cmp::Ordering;
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn is_perfect_square(num: i32) -> bool {
-    todo!()
+    if num < 2 {
+        return true;
+    }
+    let num = num as i64;
+    let mut left = 2_i64;
+    let mut right = num / 2;
+    while left <= right {
+        let x = left + (right - left) / 2;
+        let n = x * x;
+        match n.cmp(&num) {
+            Ordering::Equal => return true,
+            Ordering::Less => left = x + 1,
+            Ordering::Greater => right = x - 1,
+        }
+    }
+    false
 }
 // math binary_search
 #[test]
-#[ignore]
 fn test2_367() {
     assert_eq!(is_perfect_square(16), true);
     assert_eq!(is_perfect_square(14), false);
--- src/math/leetcode367.rs
+++ src/math/leetcode367.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/valid-perfect-square/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn is_perfect_square(num: i32) -> bool {
-    todo!()
+    if num < 2 {
+        return true;
+    }
+    let num = num as i64;
+    let mut x = num / 2;
+    while x * x > num {
+        x = (x + num / x) / 2;
+    }
+    x * x == num
 }
 // math binary_search
 #[test]
-#[ignore]
 fn test1_367() {
     assert_eq!(is_perfect_square(16), true);
     assert_eq!(is_perfect_square(14), false);
