0f235a147625d2efd4492c78387829d7d98227d1
SmiteWindows <x1627283564@hotmail.com>
2020-05-01 20:40:01 +0800

    202005012039

--- src/dynamic_programming/leetcode517.rs
+++ src/dynamic_programming/leetcode517.rs
@@ -4,6 +4,7 @@ pub fn find_min_moves(machines: Vec<i32>) -> i32 {
 }
 // math dynamic_programming
 #[test]
+#[ignore]
 fn test2_517() {
     assert_eq!(find_min_moves(vec![1, 0, 5]), 3);
     assert_eq!(find_min_moves(vec![0, 3, 0]), 2);
--- src/dynamic_programming/leetcode523.rs
+++ src/dynamic_programming/leetcode523.rs
@@ -4,6 +4,7 @@ pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
 }
 // math dynamic_programming
 #[test]
+#[ignore]
 fn test2_523() {
     assert_eq!(check_subarray_sum(vec![23, 2, 4, 6, 7], 6), true);
     assert_eq!(check_subarray_sum(vec![23, 2, 6, 4, 7], 6), true);
