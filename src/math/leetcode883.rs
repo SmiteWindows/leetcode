995d67a2a58a90772a53bf99a48d6d2d8a719735
SmiteWindows <x1627283564@hotmail.com>
2020-05-01 20:35:32 +0800

    202005012035

--- src/math/leetcode883.rs
+++ src/math/leetcode883.rs
@@ -7,8 +7,8 @@ pub fn projection_area(grid: Vec<Vec<i32>>) -> i32 {
 #[ignore]
 fn test1_883() {
     assert_eq!(projection_area(vec![vec![2]]), 5);
-    assert_eq!(projection_area(vec![vec![1, 2]], vec![3, 4]), 17);
-    assert_eq!(projection_area(vec![vec![1, 0]], vec![0, 2]), 8);
+    assert_eq!(projection_area(vec![vec![1, 2]]), 17);
+    assert_eq!(projection_area(vec![vec![1, 0]]), 8);
     assert_eq!(
         projection_area(vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]]),
         14
