486b04364ef37a2169b83ebc809c2dfbe620904e
SmiteWindows <x1627283564@hotmail.com>
2020-05-02 16:18:57 +0800

    202005021618

--- src/math/leetcode1238.rs
+++ src/math/leetcode1238.rs
@@ -7,5 +7,5 @@ pub fn circular_permutation(n: i32, start: i32) -> Vec<i32> {
 #[ignore]
 fn test1_1238() {
     assert_eq!(circular_permutation(2, 3), vec![3, 2, 0, 1]);
-    assert_eq!(circular_permutation(3, 2), vec![[2, 6, 7, 5, 4, 0, 1, 3]]);
+    assert_eq!(circular_permutation(3, 2), vec![2, 6, 7, 5, 4, 0, 1, 3]);
 }
