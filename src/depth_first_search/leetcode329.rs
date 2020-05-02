83c080dd62f8ca60b792a42cfe104e72db113a13
SmiteWindows <x1627283564@hotmail.com>
2020-05-02 20:05:29 +0800

    202005022005

--- src/depth_first_search/leetcode329.rs
+++ src/depth_first_search/leetcode329.rs
@@ -2,3 +2,16 @@
 pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
     todo!()
 }
+// depth_first_search topological_sort memoization
+#[test]
+#[ignore]
+fn test2_329() {
+    assert_eq!(
+        longest_increasing_path(vec![vec![9, 9, 4], vec![6, 6, 8], vec![2, 1, 1]]),
+        4
+    );
+    assert_eq!(
+        longest_increasing_path(vec![vec![3, 4, 5], vec![3, 2, 6], vec![2, 2, 1]]),
+        4
+    );
+}
--- src/topological_sort/leetcode1203.rs
+++ src/topological_sort/leetcode1203.rs
@@ -2,3 +2,44 @@
 pub fn sort_items(n: i32, m: i32, group: Vec<i32>, before_items: Vec<Vec<i32>>) -> Vec<i32> {
     todo!()
 }
+// depth_first_search topological_sort graph
+#[test]
+#[ignore]
+fn test1_1203() {
+    assert_eq!(
+        sort_items(
+            8,
+            2,
+            vec![-1, -1, 1, 0, 0, 1, 0, -1],
+            vec![
+                vec![],
+                vec![6],
+                vec![5],
+                vec![6],
+                vec![3, 6],
+                vec![],
+                vec![],
+                vec![]
+            ]
+        ),
+        vec![6, 3, 4, 1, 5, 2, 0, 7]
+    );
+    assert_eq!(
+        sort_items(
+            8,
+            2,
+            vec![-1, -1, 1, 0, 0, 1, 0, -1],
+            vec![
+                vec![],
+                vec![6],
+                vec![5],
+                vec![6],
+                vec![3],
+                vec![],
+                vec![4],
+                vec![]
+            ]
+        ),
+        vec![]
+    );
+}
