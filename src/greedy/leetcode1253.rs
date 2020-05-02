ff55531f3c53979572504010ea186cac3eda92b0
SmiteWindows <x1627283564@hotmail.com>
2020-05-02 16:27:09 +0800

    202005021627

--- src/greedy/leetcode1253.rs
+++ src/greedy/leetcode1253.rs
@@ -10,7 +10,7 @@ fn test2_1253() {
         reconstruct_matrix(2, 1, vec![1, 1, 1]),
         vec![vec![1, 1, 0], vec![0, 0, 1]]
     );
-    assert_eq!(reconstruct_matrix(2, 3, vec![2, 2, 1, 1]), vec![]);
+    assert_eq!(reconstruct_matrix(2, 3, vec![2, 2, 1, 1]), vec![vec![]]);
     assert_eq!(
         reconstruct_matrix(5, 5, vec![2, 1, 2, 0, 1, 0, 1, 2, 0, 1]),
         vec![
