1fb3c1cb8e460b2892ce2230cd96aa45c996c7b6
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 20:31:14 +0800

    202005082031

--- src/stack/leetcode1047.rs
+++ src/stack/leetcode1047.rs
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 pub fn remove_duplicates(s: String) -> String {
-    todo!()
+    let mut stack = vec![];
+    for c in s.chars() {
+        if let Some(&top) = stack.last() {
+            if top == c {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        } else {
+            stack.push(c)
+        }
+    }
+    stack.iter().collect()
 }
 // stack
 #[test]
-#[ignore]
 fn test1_1047() {
     assert_eq!(
         remove_duplicates(String::from("abbaca")),
