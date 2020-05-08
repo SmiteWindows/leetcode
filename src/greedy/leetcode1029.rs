0532b2559f8f9112c5a83cb0a7dd38e1de301b85
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 18:13:26 +0800

    202005081813

--- src/greedy/leetcode1029.rs
+++ src/greedy/leetcode1029.rs
@@ -1,10 +1,16 @@
 // https://leetcode.com/problems/two-city-scheduling/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn two_city_sched_cost(costs: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let n = costs.len();
+    let mut diffs: Vec<i32> = costs.iter().map(|v| v[0] - v[1]).collect();
+    diffs.sort_unstable();
+    let sum_of_b = costs.iter().fold(0, |sum, v| sum + v[1]);
+    let sum_of_diff: i32 = diffs.iter().take(n / 2).sum();
+    sum_of_b + sum_of_diff
 }
 // greedy
 #[test]
-#[ignore]
 fn test1_1029() {
     assert_eq!(
         two_city_sched_cost(vec![
