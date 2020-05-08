c4ec718826efd9d87d883a4ccd341ee5e6b5d650
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 09:17:29 +0800

    202005080917

--- src/two_pointers/leetcode27.rs
+++ src/two_pointers/leetcode27.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/remove-element/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-    todo!()
+    let n = nums.len();
+    let mut size = 0_usize;
+    for i in 0..n {
+        if nums[i] != val {
+            nums[size] = nums[i];
+            size += 1;
+        }
+    }
+    size as i32
 }
 // array two_pointers
 #[test]
@@ -8,8 +18,8 @@ pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
 fn test1_27() {
     let mut nums1 = vec![3, 2, 2, 3];
     assert_eq!(remove_element(&mut nums1, 3), 2);
-    assert_eq!(nums1, vec![2, 2]);
+    assert_eq!(&nums1[..2], &vec![2, 2][..]);
     let mut nums2 = vec![0, 1, 2, 2, 3, 0, 4, 2];
     assert_eq!(remove_element(&mut nums2, 2), 5);
-    assert_eq!(nums2, vec![0, 1, 3, 0, 4]);
+    assert_eq!(&nums2[..5], &vec![0, 1, 3, 0, 4][..]);
 }
