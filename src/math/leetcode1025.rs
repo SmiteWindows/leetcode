51be8adc7ac1ae508c92b734c4be5ef76e9f781c
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 17:34:47 +0800

    202005081734

--- src/math/leetcode1025.rs
+++ src/math/leetcode1025.rs
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/divisor-game/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn divisor_game(n: i32) -> bool {
-    todo!()
+    n % 2 == 0
 }
 // math dynamic_programming
 #[test]
-#[ignore]
 fn test1_1025() {
     assert_eq!(divisor_game(2), true);
     assert_eq!(divisor_game(3), false);
