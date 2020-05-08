6fdfd7068645e524c6a91d1297db02785fe5fbc2
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 17:16:50 +0800

    202005081716

--- src/array/leetcode1018.rs
+++ src/array/leetcode1018.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/binary-prefix-divisible-by-5/
+// Runtime: 4 ms
+// Memory Usage: 2.3 MB
 pub fn prefixes_div_by5(a: Vec<i32>) -> Vec<bool> {
-    todo!()
+    let mut x = 0;
+    let n = a.len();
+    let mut res = vec![false; n];
+    for (i, val) in a.iter().enumerate() {
+        x = (x * 2 + val) % 5;
+        res[i] = x == 0;
+    }
+    res
 }
 // array
 #[test]
-#[ignore]
 fn test1_1018() {
     assert_eq!(prefixes_div_by5(vec![0, 1, 1]), vec![true, false, false]);
     assert_eq!(prefixes_div_by5(vec![1, 1, 1]), vec![false, false, false]);
