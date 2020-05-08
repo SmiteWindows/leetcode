79aaac87e8a2e4619364a2dec020ea190ab9ff1a
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 17:27:04 +0800

    202005081727

--- src/stack/leetcode1021.rs
+++ src/stack/leetcode1021.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/remove-outermost-parentheses/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn remove_outer_parentheses(s: String) -> String {
-    todo!()
+    let mut res = String::from("");
+    let mut count = 0;
+    for c in s.chars() {
+        if c == '(' {
+            count += 1;
+            if count > 1 {
+                res.push(c);
+            }
+        } else {
+            count -= 1;
+            if count != 0 {
+                res.push(c);
+            }
+        }
+    }
+    res
 }
 // stack
 #[test]
-#[ignore]
 fn test1_1021() {
     assert_eq!(
         remove_outer_parentheses(String::from("(()())(())")),
