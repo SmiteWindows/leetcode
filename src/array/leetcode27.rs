3572d1fe4b0726a453688123f31766c761cd89d7
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 08:18:46 +0800

    202005080818

--- src/array/leetcode27.rs
+++ src/array/leetcode27.rs
@@ -8,6 +8,8 @@ pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
 fn test2_27() {
     let mut nums1 = vec![3, 2, 2, 3];
     assert_eq!(remove_element(&mut nums1, 3), 2);
+    assert_eq!(nums1, vec![2, 2]);
     let mut nums2 = vec![0, 1, 2, 2, 3, 0, 4, 2];
     assert_eq!(remove_element(&mut nums2, 2), 5);
+    assert_eq!(nums2, vec![0, 1, 3, 0, 4]);
 }
