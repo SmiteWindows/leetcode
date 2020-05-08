29a5dbc5542d7a14cf95c1ca09bc91acd63bec05
SmiteWindows <x1627283564@hotmail.com>
2020-05-08 20:53:50 +0800

    202005082053

--- src/array/leetcode1051.rs
+++ src/array/leetcode1051.rs
@@ -1,10 +1,16 @@
 // https://leetcode.com/problems/height-checker/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn height_checker(heights: Vec<i32>) -> i32 {
-    todo!()
+    let mut sorted = heights.to_vec();
+    sorted.sort_unstable();
+    heights
+        .iter()
+        .zip(sorted.iter())
+        .fold(0, |sum, (a, b)| if a != b { sum + 1 } else { sum })
 }
 // array
 #[test]
-#[ignore]
 fn test1_1051() {
     assert_eq!(height_checker(vec![1, 1, 4, 2, 1, 3]), 3);
     assert_eq!(height_checker(vec![5, 1, 2, 3, 4]), 5);
--- src/stack/leetcode1047.rs
+++ src/stack/leetcode1047.rs
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn remove_duplicates(s: String) -> String {
     let mut stack = vec![];
     for c in s.chars() {
