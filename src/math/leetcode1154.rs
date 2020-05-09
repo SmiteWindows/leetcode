d6b0185cfd35081287efe9ff400f222963c08e19
SmiteWindows <x1627283564@hotmail.com>
2020-05-09 11:55:41 +0800

    202005091155

--- src/math/leetcode1154.rs
+++ src/math/leetcode1154.rs
@@ -1,10 +1,24 @@
 // https://leetcode.com/problems/day-of-the-year/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn day_of_year(date: String) -> i32 {
-    todo!()
+    let a: Vec<&str> = date.split_terminator('-').collect();
+    let year = a[0].parse::<usize>().unwrap();
+    let month = a[1].parse::<usize>().unwrap();
+    let day = a[2].parse::<usize>().unwrap();
+    let mut days: Vec<usize> = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let mut sum = 0;
+    if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) {
+        days[1] += 1;
+    }
+    for d in days.iter().take(month - 1) {
+        sum += d;
+    }
+    sum += day;
+    sum as i32
 }
 // math
 #[test]
-#[ignore]
 fn test1_1154() {
     assert_eq!(day_of_year(String::from("2019-01-09")), 9);
     assert_eq!(day_of_year(String::from("2019-02-10")), 41);
