9c3fbcd1c732543dea1744e6bf97bddc90426821
SmiteWindows <x1627283564@hotmail.com>
2020-05-09 12:20:26 +0800

    202005091220

--- src/hash_table/leetcode1160.rs
+++ src/hash_table/leetcode1160.rs
@@ -1,10 +1,32 @@
 // https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+// Runtime: 8 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashMap;
 pub fn count_characters(words: Vec<String>, chars: String) -> i32 {
-    todo!()
+    let mut hs: HashMap<char, i32> = HashMap::new();
+    for c in chars.chars() {
+        *hs.entry(c).or_default() += 1;
+    }
+    let mut sum = 0;
+    for w in words {
+        let mut hs=hs.clone();
+        let mut valid = true;
+        for c in w.chars() {
+            let count = hs.entry(c).or_default();
+            *count -= 1;
+            if *count < 0 {
+                valid = false;
+                break;
+            }
+        }
+        if valid {
+            sum += w.len();
+        }
+    }
+    sum as i32
 }
 // hash_table array
 #[test]
-#[ignore]
 fn test1_1160() {
     assert_eq!(
         count_characters(
