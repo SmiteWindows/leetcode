d748fe249da04e5f996cb856558910df2d3f5a2d
SmiteWindows <x1627283564@hotmail.com>
2020-05-09 12:59:36 +0800

    202005091259

--- src/string/leetcode1170.rs
+++ src/string/leetcode1170.rs
@@ -1,10 +1,30 @@
 // https://leetcode.com/problems/compare-strings-by-frequency-of-the-smallest-character/
+// Runtime: 0 ms
+// Memory Usage: 2.3 MB
 pub fn num_smaller_by_frequency(queries: Vec<String>, words: Vec<String>) -> Vec<i32> {
-    todo!()
+    fn f(s: &str) -> usize {
+        let mut count = vec![0; 26];
+        let mut min = b'z';
+        for b in s.bytes() {
+            min = min.min(b);
+            count[(b - b'a') as usize] += 1;
+        }
+        count[(min - b'a') as usize]
+    }
+
+    let f_queries: Vec<usize> = queries.iter().map(|s| f(s)).collect();
+    let f_words: Vec<usize> = words.iter().map(|s| f(s)).collect();
+    let mut counts = vec![0; 12];
+    for f in f_words {
+        counts[f] += 1;
+    }
+    for i in (1..10).rev() {
+        counts[i] += counts[i + 1];
+    }
+    f_queries.into_iter().map(|f| counts[f + 1]).collect()
 }
 // array string
 #[test]
-#[ignore]
 fn test1_1170() {
     assert_eq!(
         num_smaller_by_frequency(vec![String::from("cbd")], vec![String::from("zaaaz")]),
