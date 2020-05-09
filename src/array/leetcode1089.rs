bcc539d4e6a611e78b1a79f2edadc35c0abecbaf
SmiteWindows <x1627283564@hotmail.com>
2020-05-09 09:35:25 +0800

    202005090935

--- src/array/leetcode1089.rs
+++ src/array/leetcode1089.rs
@@ -1,10 +1,40 @@
 // https://leetcode.com/problems/duplicate-zeros/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn duplicate_zeros(arr: &mut Vec<i32>) {
-    todo!()
+    let n = arr.len();
+    let m = arr.iter().filter(|&x| *x == 0).count();
+    let mut i = n - 1;
+    let mut j = m + n - 1;
+    while i > 0 {
+        if arr[i] != 0 {
+            if j < n {
+                arr[j] = arr[i];
+            }
+            i -= 1;
+            j -= 1;
+        } else {
+            if j < n {
+                arr[j] = 0;
+            }
+            i -= 1;
+            j -= 1;
+            if j < n {
+                arr[j] = 0;
+            }
+            j -= 1;
+        }
+    }
+    if arr[i] != 0 {
+        arr[j] = arr[i];
+    } else {
+        arr[j] = 0;
+        j -= 1;
+        arr[j] = 0;
+    }
 }
 // array
 #[test]
-#[ignore]
 fn test1_1089() {
     let mut arr1 = vec![1, 0, 2, 3, 0, 4, 5, 0];
     duplicate_zeros(&mut arr1);
--- src/bit_manipulation/leetcode1342.rs
+++ src/bit_manipulation/leetcode1342.rs
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn number_of_steps(num: i32) -> i32 {
-    todo!()
+    let mut num=num;
+    let mut res = 0;
+    while num != 0 {
+        if num & 1 == 1 {
+            num -= 1;
+        } else {
+            num >>= 1;
+        }
+        res += 1;
+    }
+    res
 }
 // bit_manipulation
 #[test]
-#[ignore]
 fn test1_1342() {
     assert_eq!(number_of_steps(14), 6);
     assert_eq!(number_of_steps(8), 4);
