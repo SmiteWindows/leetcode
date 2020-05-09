f5af3c0922ddfb16781a688475c3241f70576f9a
SmiteWindows <x1627283564@hotmail.com>
2020-05-09 11:40:57 +0800

    202005091140

--- src/recursion/leetcode1137.rs
+++ src/recursion/leetcode1137.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/n-th-tribonacci-number/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn tribonacci(n: i32) -> i32 {
-    todo!()
+    let n = n as usize;
+    let mut a: Vec<i32> = vec![0; 38];
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 1;
+    for i in 3..=n {
+        a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+    }
+    a[n]
 }
 // recursion
 #[test]
-#[ignore]
 fn test1_1137() {
     assert_eq!(tribonacci(4), 4);
     assert_eq!(tribonacci(25), 1389537);
