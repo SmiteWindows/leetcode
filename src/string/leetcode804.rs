d43920322e889048930c3cca4fc3bc0ae338b9c9
SmiteWindows <x1627283564@hotmail.com>
2020-05-10 18:00:01 +0800

    202005101759

--- src/string/leetcode804.rs
+++ src/string/leetcode804.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/unique-morse-code-words/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashSet;
 pub fn unique_morse_representations(words: Vec<String>) -> i32 {
-    todo!()
+    let map = [
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+        "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
+        "--..",
+    ];
+    let mut morse = HashSet::new();
+    for w in words {
+        let mut s = "".to_string();
+        for c in w.chars() {
+            let m = map[(c as u8 - b'a') as usize];
+            s += m;
+        }
+        morse.insert(s);
+    }
+    morse.len() as i32
 }
 // string
 #[test]
-#[ignore]
 fn test1_804() {
     assert_eq!(
         unique_morse_representations(vec![
