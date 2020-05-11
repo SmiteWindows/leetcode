18d5c011493c786e82bb478d97492bef27bfe9de
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 15:03:21 +0800

    202005111503

--- src/array/leetcode1295.rs
+++ src/array/leetcode1295.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn find_numbers(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn digits(mut n: i32) -> i32 {
+        let mut res = 0;
+        while n != 0 {
+            n /= 10;
+            res += 1;
+        }
+        res
+    }
+
+    let mut res = 0;
+    for n in nums {
+        if digits(n) % 2 == 0 {
+            res += 1;
+        }
+    }
+    res
 }
 // array
 #[test]
-#[ignore]
 fn test1_1295() {
     assert_eq!(find_numbers(vec![12, 345, 2, 6, 7896]), 2);
     assert_eq!(find_numbers(vec![555, 901, 482, 1771]), 1);
