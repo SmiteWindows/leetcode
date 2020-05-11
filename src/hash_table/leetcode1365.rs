aadc95fda35c18b6db9e4788e64215873ca24ac7
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 14:35:06 +0800

    202005111435

--- src/hash_table/leetcode1365.rs
+++ src/hash_table/leetcode1365.rs
@@ -1,10 +1,23 @@
 // https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+// Runtime: 0 ms
+// Memory Usage: 1.9 MB
 pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut count = vec![0; 101];
+    for &x in &nums {
+        count[x as usize] += 1;
+    }
+    for i in 0..100 {
+        count[i + 1] += count[i]
+    }
+    let mut res = vec![];
+    for &x in &nums {
+        let v = if x == 0 { 0 } else { count[(x - 1) as usize] };
+        res.push(v)
+    }
+    res
 }
 // hash_table array
 #[test]
-#[ignore]
 fn test1_1365() {
     assert_eq!(
         smaller_numbers_than_current(vec![8, 1, 2, 2, 3]),
