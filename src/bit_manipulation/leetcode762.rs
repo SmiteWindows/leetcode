22160cdc4bc134bae1a1e7559a8a6aec347197aa
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 16:40:35 +0800

    202005111640

--- src/bit_manipulation/leetcode762.rs
+++ src/bit_manipulation/leetcode762.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn count_prime_set_bits(l: i32, r: i32) -> i32 {
-    todo!()
+    let mut count = 0;
+    for i in l..=r {
+        match i.count_ones() {
+            2 | 3 | 5 | 7 | 11 | 13 | 17 | 19 => count += 1,
+            _ => {}
+        }
+    }
+    count
 }
 // bit_manipulation
 #[test]
-#[ignore]
 fn test1_762() {
     assert_eq!(count_prime_set_bits(6, 10), 4);
     assert_eq!(count_prime_set_bits(10, 15), 5);
