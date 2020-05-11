861437e6bed4c6506d5921f224610f26d4623e0d
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 13:53:43 +0800

    202005111353

--- src/hash_table/leetcode771.rs
+++ src/hash_table/leetcode771.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/jewels-and-stones/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashSet;
 pub fn num_jewels_in_stones(j: String, s: String) -> i32 {
-    todo!()
+    let mut hs = HashSet::new();
+    for c in j.chars() {
+        hs.insert(c);
+    }
+    let mut res = 0;
+    for c in s.chars() {
+        if hs.contains(&c) {
+            res += 1;
+        }
+    }
+    res
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_771() {
     assert_eq!(
         num_jewels_in_stones(String::from("aA"), String::from("aAAbbbb")),
