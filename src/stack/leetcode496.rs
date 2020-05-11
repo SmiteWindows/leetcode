234c215edd301de2a239b8e4656cb68f3abdaba2
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 17:44:45 +0800

    202005111744

--- src/stack/leetcode496.rs
+++ src/stack/leetcode496.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/next-greater-element-i/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashMap;
 pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut stack = vec![];
+    let mut hm = HashMap::new();
+    for x in nums2 {
+        while let Some(last) = stack.pop() {
+            if last > x {
+                stack.push(last);
+                break;
+            } else {
+                hm.insert(last, x);
+            }
+        }
+        stack.push(x);
+    }
+    nums1.iter().map(|x| *hm.get(x).unwrap_or(&-1)).collect()
 }
 // stack
 #[test]
-#[ignore]
 fn test1_496() {
     assert_eq!(
         next_greater_element(vec![4, 1, 2], vec![1, 3, 4, 2]),
