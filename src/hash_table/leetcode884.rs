887ba2bd4111b2816b1ce67928105dd5f840676a
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 17:22:36 +0800

    202005111722

--- src/hash_table/leetcode884.rs
+++ src/hash_table/leetcode884.rs
@@ -1,17 +1,33 @@
 // https://leetcode.com/problems/uncommon-words-from-two-sentences/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::BTreeMap;
 pub fn uncommon_from_sentences(a: String, b: String) -> Vec<String> {
-    todo!()
+    let mut hs: BTreeMap<&str, i32> = BTreeMap::new();
+    a.split_whitespace().for_each(|s| {
+        *hs.entry(s).or_default() += 1;
+    });
+    b.split_whitespace().for_each(|s| {
+        *hs.entry(s).or_default() += 1;
+    });
+    let mut res = vec![];
+    for (s, v) in hs {
+        if v == 1 {
+            res.push(s.to_string());
+        }
+    }
+    res
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_884() {
     assert_eq!(
         uncommon_from_sentences(
             String::from("this apple is sweet"),
             String::from("this apple is sour")
         ),
-        vec![String::from("sweet"), String::from("sour")]
+        // vec![String::from("sweet"), String::from("sour")]
+        vec![String::from("sour"), String::from("sweet")]
     );
     assert_eq!(
         uncommon_from_sentences(String::from("apple apple"), String::from("banana")),
