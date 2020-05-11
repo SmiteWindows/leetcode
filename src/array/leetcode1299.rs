542df866d99b8720351e95e4d6680df53d8c1594
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 09:20:08 +0800

    202005110920

--- src/array/leetcode1299.rs
+++ src/array/leetcode1299.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+// Runtime: 4 ms
+// Memory Usage: 2.2 MB
 pub fn replace_elements(arr: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut arr = arr;
+    let mut max = -1;
+    let n = arr.len();
+    for i in (0..n).rev() {
+        let t = max;
+        max = i32::max(arr[i], max);
+        arr[i] = t;
+    }
+    arr
 }
 // array
 #[test]
-#[ignore]
 fn test1_1299() {
     assert_eq!(
         replace_elements(vec![17, 18, 5, 4, 6, 1]),
