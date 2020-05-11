33b41a21c66a67514ec514e4cd89a0845dd5b5de
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 18:04:42 +0800

    202005111804

--- src/backtracking/leetcode784.rs
+++ src/backtracking/leetcode784.rs
@@ -1,10 +1,37 @@
 // https://leetcode.com/problems/letter-case-permutation/
+// Runtime: 0 ms
+// Memory Usage: 2.3 MB
 pub fn letter_case_permutation(s: String) -> Vec<String> {
-    todo!()
+    fn permutation(s: &[char], n: usize, i: usize, t: &mut String, res: &mut Vec<String>) {
+        if i == n {
+            res.push(t.clone());
+        } else {
+            if s[i].is_alphabetic() {
+                let lower: char = s[i].to_ascii_lowercase();
+                let upper: char = s[i].to_ascii_uppercase();
+                t.push(lower);
+                permutation(s, n, i + 1, t, res);
+                t.pop();
+                t.push(upper);
+                permutation(s, n, i + 1, t, res);
+                t.pop();
+            } else {
+                t.push(s[i]);
+                permutation(s, n, i + 1, t, res);
+                t.pop();
+            }
+        }
+    }
+
+    let n = s.len();
+    let s: Vec<char> = s.chars().collect();
+    let mut res = vec![];
+    let mut t = "".to_string();
+    permutation(&s, n, 0, &mut t, &mut res);
+    res
 }
 // backtracking bit_manipulation
 #[test]
-#[ignore]
 fn test1_784() {
     assert_eq!(
         letter_case_permutation(String::from("a1b2")),
