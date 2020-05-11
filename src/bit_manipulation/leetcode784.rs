6d85ec3fb68f440e2a30c8158fffbf3d39cf3da0
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 18:29:28 +0800

    202005111829

--- src/bit_manipulation/leetcode784.rs
+++ src/bit_manipulation/leetcode784.rs
@@ -1,17 +1,44 @@
 // https://leetcode.com/problems/letter-case-permutation/
+// Runtime: 4 ms
+// Memory Usage: 2.4 MB
 pub fn letter_case_permutation(s: String) -> Vec<String> {
-    todo!()
+    let mut b = 0;
+    for c in s.chars() {
+        if c.is_alphabetic() {
+            b += 1;
+        }
+    }
+    let mut res = vec![];
+    for bits in 0..1 << b {
+        let mut b = 0;
+        let mut word = "".to_string();
+        for letter in s.chars() {
+            if letter.is_alphabetic() {
+                if ((bits >> b) & 1) == 1 {
+                    word.push(letter.to_ascii_uppercase());
+                    b += 1;
+                } else {
+                    word.push(letter.to_ascii_lowercase());
+                    b += 1;
+                }
+            } else {
+                word.push(letter);
+            }
+        }
+
+        res.push(word.to_string());
+    }
+    res
 }
 // backtracking bit_manipulation
 #[test]
-#[ignore]
 fn test2_784() {
     assert_eq!(
         letter_case_permutation(String::from("a1b2")),
         vec![
             String::from("a1b2"),
-            String::from("a1B2"),
             String::from("A1b2"),
+            String::from("a1B2"),
             String::from("A1B2")
         ]
     );
