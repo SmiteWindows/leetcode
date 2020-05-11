265bd15c80a72b8ac73f626173ddec843b4b394d
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 14:43:15 +0800

    202005111443

--- src/array/leetcode1313.rs
+++ src/array/leetcode1313.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/decompress-run-length-encoded-list/
+// Runtime: 4 ms
+// Memory Usage: 2.2 MB
 pub fn decompress_rl_elist(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut res = vec![];
+    for p in nums.chunks(2) {
+        let a = p[0] as usize;
+        let b = p[1];
+        for _ in 0..a {
+            res.push(b);
+        }
+    }
+    res
 }
 // array
 #[test]
-#[ignore]
 fn test1_1313() {
     assert_eq!(decompress_rl_elist(vec![1, 2, 3, 4]), vec![2, 4, 4, 4]);
     assert_eq!(decompress_rl_elist(vec![1, 1, 2, 3]), vec![1, 3, 3]);
