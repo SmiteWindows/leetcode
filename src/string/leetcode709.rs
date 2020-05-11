a86f41e3c272992f6181b3749533fc783396f605
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 10:07:01 +0800

    202005111006

--- src/string/leetcode709.rs
+++ src/string/leetcode709.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/to-lower-case/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn to_lower_case(str: String) -> String {
-    todo!()
+    str.chars()
+    .map(|c| {
+        if c as u8 >= b'A' && c as u8 <= b'Z' {
+            (c as u8 + (b'a' - b'A')) as char
+        } else {
+            c
+        }
+    })
+    .collect()
 }
 // string
 #[test]
-#[ignore]
 fn test1_709() {
     assert_eq!(to_lower_case(String::from("Hello")), String::from("hello"));
     assert_eq!(to_lower_case(String::from("here")), String::from("here"));
