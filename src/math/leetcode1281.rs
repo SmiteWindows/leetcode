06e632b6b566bb05c77fcf7271d924b033761342
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 14:52:20 +0800

    202005111452

--- src/math/leetcode1281.rs
+++ src/math/leetcode1281.rs
@@ -1,10 +1,20 @@
 // https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn subtract_product_and_sum(n: i32) -> i32 {
-    todo!()
+    let mut n = n;
+    let mut product = 1;
+    let mut sum = 0;
+    while n != 0 {
+        let d = n % 10;
+        product *= d;
+        sum += d;
+        n /= 10;
+    }
+    product - sum
 }
 // math
 #[test]
-#[ignore]
 fn test1_1281() {
     assert_eq!(subtract_product_and_sum(234), 15);
     assert_eq!(subtract_product_and_sum(4421), 21);
