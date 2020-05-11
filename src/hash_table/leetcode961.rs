421b5f0308dea91cea4478b6eeb183f6b7b986a2
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 16:26:33 +0800

    202005111626

--- src/hash_table/leetcode961.rs
+++ src/hash_table/leetcode961.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashSet;
 pub fn repeated_n_times(a: Vec<i32>) -> i32 {
-    todo!()
+    let mut hs: HashSet<i32> = HashSet::new();
+    for x in a {
+        if !hs.insert(x) {
+            return x;
+        }
+    }
+    unreachable!()
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_961() {
     assert_eq!(repeated_n_times(vec![1, 2, 3, 3]), 3);
     assert_eq!(repeated_n_times(vec![2, 1, 2, 5, 3, 2]), 2);
