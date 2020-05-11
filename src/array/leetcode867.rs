51f5828b55a25862222e47c3949635dec9475e81
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 19:05:36 +0800

    202005111905

--- src/array/leetcode867.rs
+++ src/array/leetcode867.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/transpose-matrix/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn transpose(a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+    let n = a.len();
+    let m = a[0].len();
+    let mut res: Vec<Vec<i32>> = vec![vec![0; n]; m];
+    for i in 0..n {
+        for j in 0..m {
+            res[j][i] = a[i][j];
+        }
+    }
+    res
 }
 // array
 #[test]
-#[ignore]
 fn test1_867() {
     assert_eq!(
         transpose(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]),
