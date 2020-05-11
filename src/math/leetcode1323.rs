debeb5c6a2d9b460c0896007cd284960784e6373
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 09:36:19 +0800

    202005110936

--- src/math/leetcode1323.rs
+++ src/math/leetcode1323.rs
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/maximum-69-number/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn maximum69_number(num: i32) -> i32 {
-    todo!()
+    num.to_string().replacen("6", "9", 1).parse().unwrap()
 }
 // math
 #[test]
-#[ignore]
 fn test1_1323() {
     assert_eq!(maximum69_number(9669), 9969);
     assert_eq!(maximum69_number(9996), 9999);
