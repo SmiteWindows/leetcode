e6724877129f2d50d735640a7f32a77b550cece1
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 06:42:30 +0800

    202005120642

--- src/string/leetcode657.rs
+++ src/string/leetcode657.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/robot-return-to-origin/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn judge_circle(moves: String) -> bool {
-    todo!()
+    let mut x = 0;
+    let mut y = 0;
+    for c in moves.chars() {
+        match c {
+            'U' => y += 1,
+            'D' => y -= 1,
+            'L' => x -= 1,
+            'R' => x += 1,
+            _ => (),
+        }
+    }
+    x == 0 && y == 0
 }
 // string
 #[test]
-#[ignore]
 fn test1_657() {
     assert_eq!(judge_circle(String::from("UD")), true);
     assert_eq!(judge_circle(String::from("LL")), false);
