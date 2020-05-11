e70f3fb6c68a21be52ed08e574e840b2afc1ef4e
SmiteWindows <x1627283564@hotmail.com>
2020-05-11 15:33:05 +0800

    202005111533

--- src/array/leetcode832.rs
+++ src/array/leetcode832.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/flipping-an-image/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn flip_and_invert_image(a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut a = a;
+    let n = a.len();
+    for i in 0..n {
+        let mut l = 0;
+        let mut r = n - 1;
+        while l < r {
+            a[i].swap(l, r);
+            l += 1;
+            r -= 1;
+        }
+        for j in 0..n {
+            a[i][j] = 1 - a[i][j];
+        }
+    }
+    a
 }
 // array
 #[test]
-#[ignore]
 fn test1_832() {
     assert_eq!(
         flip_and_invert_image(vec![vec![1, 1, 0], vec![1, 0, 1], vec![0, 0, 0]]),
