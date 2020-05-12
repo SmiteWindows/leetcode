b25425d02ac92bc38ab618c461417120cec6fb6d
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 06:29:48 +0800

    202005130629

--- src/hash_table/leetcode811.rs
+++ src/hash_table/leetcode811.rs
@@ -1,6 +1,35 @@
 // https://leetcode.com/problems/subdomain-visit-count/
+// Runtime: 4 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashMap;
 pub fn subdomain_visits(cpdomains: Vec<String>) -> Vec<String> {
-    todo!()
+    fn parse(s: String) -> (Vec<String>, usize) {
+        let mut domains: Vec<String> = vec![];
+        let mut iter = s.split_whitespace();
+        let count: usize = iter.next().unwrap().parse::<usize>().unwrap();
+        let domain: String = iter.next().unwrap().parse::<String>().unwrap();
+        for (i, c) in domain.chars().enumerate() {
+            if c == '.' {
+                let subdomain = &domain[i + 1..];
+                domains.push(subdomain.to_string());
+            }
+        }
+        domains.push(domain);
+        (domains, count)
+    }
+
+    let mut res = vec![];
+    let mut hm: HashMap<String, usize> = HashMap::new();
+    for s in cpdomains {
+        let (domains, count) = parse(s);
+        for domain in domains {
+            *hm.entry(domain).or_default() += count;
+        }
+    }
+    for (domain, count) in hm {
+        res.push(format!("{} {}", count, domain));
+    }
+    res
 }
 // hash_table
 #[test]
