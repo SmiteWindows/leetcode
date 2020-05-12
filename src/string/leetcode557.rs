9634e0a91d5a84ab72b1e1f674648d7c08331373
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 07:21:20 +0800

    202005130721

--- src/string/leetcode557.rs
+++ src/string/leetcode557.rs
@@ -1,10 +1,16 @@
 // https://leetcode.com/problems/reverse-words-in-a-string-iii/
+// Runtime: 4 ms
+// Memory Usage: 2.3 MB
 pub fn reverse_words(s: String) -> String {
-    todo!()
+    let words: Vec<String> = s
+        .split_whitespace()
+        .map(|s| s.chars().rev().collect())
+        .collect();
+    let res: String = words.join(" ");
+    res
 }
 // string
 #[test]
-#[ignore]
 fn test1_557() {
     assert_eq!(
         reverse_words(String::from("Let's take LeetCode contest")),
