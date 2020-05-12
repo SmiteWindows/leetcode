7780b56be69fcb6a9f9e095b039a542f0ac64619
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 09:35:11 +0800

    202005120935

---
+++ src/other/leetcode806.rs
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/number-of-lines-to-write-string/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+pub fn number_of_lines(widths: Vec<i32>, s: String) -> Vec<i32> {
+    let mut lines = 0;
+    let mut start = 0;
+    for b in s.bytes() {
+        let w = widths[(b - b'a') as usize];
+        if start + w > 100 {
+            lines += 1;
+            start = w;
+        } else {
+            start += w;
+        }
+    }
+    vec![lines + 1, start]
+}
+#[test]
+fn test1_806() {
+    assert_eq!(
+        number_of_lines(
+            vec![
+                10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                10, 10, 10, 10, 10
+            ],
+            String::from("abcdefghijklmnopqrstuvwxyz")
+        ),
+        vec![3, 60]
+    );
+    assert_eq!(
+        number_of_lines(
+            vec![
+                4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                10, 10, 10, 10, 10
+            ],
+            String::from("bbbcccdddaaa")
+        ),
+        vec![2, 4]
+    );
+}
