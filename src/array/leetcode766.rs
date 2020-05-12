0c8adc4d56ffd520dcf08695f1568eb5624b9561
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 08:16:11 +0800

    202005120816

--- src/array/leetcode766.rs
+++ src/array/leetcode766.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/toeplitz-matrix/
 pub fn is_toeplitz_matrix(matrix: Vec<Vec<i32>>) -> bool {
-    todo!()
+    let n = matrix.len();
+    let m = matrix[0].len();
+    for i in 0..n - 1 {
+        for j in 0..m - 1 {
+            if matrix[i][j] != matrix[i + 1][j + 1] {
+                return false;
+            }
+        }
+    }
+    true
 }
 // array
 #[test]
-#[ignore]
 fn test1_766() {
     assert_eq!(
         is_toeplitz_matrix(vec![vec![1, 2, 3, 4], vec![5, 1, 2, 3], vec![9, 5, 1, 2]]),
