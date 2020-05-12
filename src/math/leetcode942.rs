4e8fdcb06a57ba6b1f34fdbad671b2c95c02e386
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 08:01:07 +0800

    202005120801

--- src/math/leetcode942.rs
+++ src/math/leetcode942.rs
@@ -1,10 +1,31 @@
 // https://leetcode.com/problems/di-string-match/
+// Runtime: 4 ms
+// Memory Usage: 2.3 MB
 pub fn di_string_match(s: String) -> Vec<i32> {
-    todo!()
+    let mut l = 0;
+    let mut r = s.len() as i32;
+    let mut res: Vec<i32> = s
+        .chars()
+        .map(|c| match c {
+            'I' => {
+                let val = l;
+                l += 1;
+                val
+            }
+            'D' => {
+                let val = r;
+                r -= 1;
+                val
+            }
+            _ => unreachable!(),
+        })
+        .collect();
+    {}
+    res.push(l);
+    res
 }
 // math
 #[test]
-#[ignore]
 fn test1_942() {
     assert_eq!(di_string_match(String::from("IDID")), vec![0, 4, 1, 3, 2]);
     assert_eq!(di_string_match(String::from("III")), vec![0, 1, 2, 3]);
