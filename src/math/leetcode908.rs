20c01257ed39eedde13b411a18a191b57b5c791b
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 10:23:13 +0800

    202005121023

--- src/math/leetcode908.rs
+++ src/math/leetcode908.rs
@@ -1,10 +1,13 @@
 // https://leetcode.com/problems/smallest-range-i/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn smallest_range_i(a: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let max = a.iter().max().unwrap();
+    let min = a.iter().min().unwrap();
+    i32::max(0, max - min - 2 * k)
 }
 // math
 #[test]
-#[ignore]
 fn test1_908() {
     assert_eq!(smallest_range_i(vec![1], 0), 0);
     assert_eq!(smallest_range_i(vec![0, 10], 2), 6);
