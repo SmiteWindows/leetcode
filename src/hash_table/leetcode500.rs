c07cfe762a5246ade8519145c50cdc337ef95a1a
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 09:02:37 +0800

    202005120902

--- src/hash_table/leetcode500.rs
+++ src/hash_table/leetcode500.rs
@@ -1,10 +1,28 @@
 // https://leetcode.com/problems/keyboard-row/
+use std::collections::HashMap;
 pub fn find_words(words: Vec<String>) -> Vec<String> {
-    todo!()
+    let rows = vec![
+        String::from("qwertyuiopQWERTYUIOP"),
+        String::from("asdfghjklASDFGHJKL"),
+        String::from("zxcvbnmZXCVBNM"),
+    ];
+    let mut hm: HashMap<char, usize> = HashMap::new();
+    for i in 0..3 {
+        for c in rows[i].chars() {
+            hm.insert(c, i);
+        }
+    }
+    let mut res = vec![];
+    for word in words {
+        let rows: Vec<usize> = word.chars().map(|c| *hm.get(&c).unwrap()).collect();
+        if rows.windows(2).all(|w| w[0] == w[1]) {
+            res.push(word);
+        }
+    }
+    res
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_500() {
     assert_eq!(
         find_words(vec![
