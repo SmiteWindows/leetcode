c8ff138f1f9fdbfa45a8ad77c44efc174d9812c2
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 11:56:20 +0800

    202005121156

--- src/bit_manipulation/leetcode461.rs
+++ src/bit_manipulation/leetcode461.rs
@@ -1,10 +1,15 @@
 // https://leetcode.com/problems/hamming-distance/
 pub fn hamming_distance(x: i32, y: i32) -> i32 {
-    todo!()
+    let mut z = x ^ y;
+    let mut distance = 0;
+    for _ in 0..32 {
+        distance += z & 1;
+        z >>= 1;
+    }
+    distance
 }
 // bit_manipulation
 #[test]
-#[ignore]
 fn test1_461() {
     assert_eq!(hamming_distance(1, 4), 2);
 }
