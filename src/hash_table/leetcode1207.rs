0738ef08250ed89bbd3512c8365942808932f69b
SmiteWindows <x1627283564@hotmail.com>
2020-05-12 13:42:21 +0800

    202005121342

--- src/hash_table/leetcode1207.rs
+++ src/hash_table/leetcode1207.rs
@@ -1,10 +1,22 @@
-// https://leetcode.com/problems/unique-number-of-occurrences/
+// // https://leetcode.com/problems/unique-number-of-occurrences/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::{HashMap, HashSet};
 pub fn unique_occurrences(arr: Vec<i32>) -> bool {
-    todo!()
+    let mut hm: HashMap<i32, i32> = HashMap::new();
+    for x in arr {
+        *hm.entry(x).or_default() += 1;
+    }
+    let mut hs = HashSet::new();
+    for x in hm.values() {
+        if !hs.insert(x) {
+            return false;
+        }
+    }
+    true
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_1207() {
     assert_eq!(unique_occurrences(vec![1, 2, 2, 1, 1, 3]), true);
     assert_eq!(unique_occurrences(vec![1, 2]), false);
