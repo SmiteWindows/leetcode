a27b01f41eb2336c79446753b4ee4abcc3b9a809
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 15:12:56 +0800

    202005131512

--- src/array/leetcode1184.rs
+++ src/array/leetcode1184.rs
@@ -1,10 +1,27 @@
 // https://leetcode.com/problems/distance-between-bus-stops/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn distance_between_bus_stops(distance: Vec<i32>, start: i32, destination: i32) -> i32 {
-    todo!()
+    let mut start = start as usize;
+    let mut destination = destination as usize;
+    if destination < start {
+        std::mem::swap(&mut start, &mut destination);
+    }
+    let n = distance.len();
+    let mut left = 0;
+    let mut right = 0;
+    for i in 0..n {
+        let j = start + i;
+        if j < destination {
+            left += distance[j % n];
+        } else {
+            right += distance[j % n];
+        }
+    }
+    left.min(right)
 }
 // array
 #[test]
-#[ignore]
 fn test1_1184() {
     assert_eq!(distance_between_bus_stops(vec![1, 2, 3, 4], 0, 1), 1);
     assert_eq!(distance_between_bus_stops(vec![1, 2, 3, 4], 0, 2), 3);
