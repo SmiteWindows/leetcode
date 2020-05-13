0d60ca60ce3797a0724e9c21f6a133f956aed4c4
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 11:51:35 +0800

    202005131151

--- src/hash_table/leetcode387.rs
+++ src/hash_table/leetcode387.rs
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/first-unique-character-in-a-string/
+// Runtime: 16 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashMap;
 pub fn first_uniq_char(s: String) -> i32 {
-    todo!()
+    let mut hm= HashMap::new();
+    for c in s.chars() {
+        *hm.entry(c).or_default() += 1;
+    }
+    for (i, c) in s.chars().enumerate() {
+        if let Some(1) = hm.get(&c) {
+            return i as i32;
+        }
+    }
+    -1
 }
 // hash_table string
 #[test]
-#[ignore]
 fn test1_387() {
     assert_eq!(first_uniq_char(String::from("leetcode")), 0);
     assert_eq!(first_uniq_char(String::from("loveleetcode")), 2);
