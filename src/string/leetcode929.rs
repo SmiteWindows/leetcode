c5ced1ce89bd27bba448b9f24694f4c35627575a
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 09:01:54 +0800

    202005130901

--- src/string/leetcode929.rs
+++ src/string/leetcode929.rs
@@ -1,10 +1,40 @@
 // https://leetcode.com/problems/unique-email-addresses/
+// Runtime: 4 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashSet;
+#[derive(PartialEq, Eq, Hash)]
+struct Email {
+    local_name: String,
+    domain_name: String,
+}
+
+impl Email {
+    fn new(s: String) -> Self {
+        let mut iter = s.split('@');
+        let left: String = iter.next().unwrap().to_string();
+        let domain_name: String = iter.next().unwrap().to_string();
+        let mut local_name: String = left.split('+').next().unwrap().to_string();
+        local_name.retain(|c| c != '.');
+        Email {
+            local_name,
+            domain_name,
+        }
+    }
+}
+
 pub fn num_unique_emails(emails: Vec<String>) -> i32 {
-    todo!()
+    let mut hs: HashSet<Email> = HashSet::new();
+    let emails: Vec<Email> = emails
+        .iter()
+        .map(|email| Email::new(email.to_string()))
+        .collect();
+    for email in emails {
+        hs.insert(email);
+    }
+    hs.len() as i32
 }
 // string
 #[test]
-#[ignore]
 fn test1_929() {
     assert_eq!(
         num_unique_emails(vec![
