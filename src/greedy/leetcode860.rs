30c26fe50dca2b9bc8cc244ce9abff5fceab9f79
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 10:16:10 +0800

    202005131016

--- src/greedy/leetcode860.rs
+++ src/greedy/leetcode860.rs
@@ -1,10 +1,39 @@
 // https://leetcode.com/problems/lemonade-change/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn lemonade_change(bills: Vec<i32>) -> bool {
-    todo!()
+    let mut b5 = 0;
+    let mut b10 = 0;
+    for b in bills {
+        match b {
+            5 => {
+                b5 += 1;
+            }
+            10 => {
+                if b5 > 0 {
+                    b10 += 1;
+                    b5 -= 1;
+                } else {
+                    return false;
+                }
+            }
+            20 => {
+                if b10 > 0 && b5 > 0 {
+                    b10 -= 1;
+                    b5 -= 1;
+                } else if b5 >= 3 {
+                    b5 -= 3;
+                } else {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+    true
 }
 // greedy
 #[test]
-#[ignore]
 fn test1_860() {
     assert_eq!(lemonade_change(vec![5, 5, 5, 10, 20]), true);
     assert_eq!(lemonade_change(vec![5, 5, 10]), true);
