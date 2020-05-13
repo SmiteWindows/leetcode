b2f4243439182a17f3aec5428a90879e137cb3ca
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 10:47:03 +0800

    202005131046

--- src/hash_table/leetcode447.rs
+++ src/hash_table/leetcode447.rs
@@ -1,10 +1,36 @@
 // https://leetcode.com/problems/number-of-boomerangs/
+// Runtime: 44 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashMap;
 pub fn number_of_boomerangs(points: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    fn distance_square(a: &[i32], b: &[i32]) -> i32 {
+        (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1])
+    }
+
+    let n = points.len();
+    let mut hm: HashMap<i32, i32> = HashMap::new();
+    let mut sum = 0;
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            let a = &points[i];
+            let b = &points[j];
+            let distance_square = distance_square(a, b);
+            *hm.entry(distance_square).or_default() += 1;
+        }
+        for &value in hm.values() {
+            if value > 1 {
+                sum += value * (value - 1);
+            }
+        }
+        hm.clear();
+    }
+    sum
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_447() {
     assert_eq!(
         number_of_boomerangs(vec![vec![0, 0], vec![1, 0], vec![2, 0]]),
