3ae93b583b8b042428a7ef85f1951a8a25090944
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 14:22:54 +0800

    202005131422

--- src/string/leetcode937.rs
+++ src/string/leetcode937.rs
@@ -1,10 +1,34 @@
 // https://leetcode.com/problems/reorder-data-in-log-files/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+use std::cmp::Ordering::{Equal, Greater, Less};
 pub fn reorder_log_files(logs: Vec<String>) -> Vec<String> {
-    todo!()
+    let mut logs = logs;
+    logs.sort_by(|a, b| {
+        let i = a.find(' ').unwrap();
+        let j = b.find(' ').unwrap();
+        let ar = &a[i + 1..];
+        let br = &b[j + 1..];
+        let ac = a.chars().last().unwrap();
+        let bc = b.chars().last().unwrap();
+        match (ac.is_digit(10), bc.is_digit(10)) {
+            (true, true) => Equal,
+            (true, false) => Greater,
+            (false, true) => Less,
+            (false, false) => {
+                let ordering = ar.cmp(br);
+                if ordering == Equal {
+                    a.cmp(b)
+                } else {
+                    ordering
+                }
+            }
+        }
+    });
+    logs
 }
 // string
 #[test]
-#[ignore]
 fn test1_937() {
     assert_eq!(
         reorder_log_files(vec![
