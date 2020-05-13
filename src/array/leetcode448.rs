f55ee9f5e1c9206479c350e0f373ed587fbe63ad
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 19:50:27 +0800

    202005131950

--- src/array/leetcode448.rs
+++ src/array/leetcode448.rs
@@ -1,10 +1,20 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut nums = nums;
+    let mut res = Vec::new();
+    for i in 0..nums.len() {
+        let t = nums[i].abs() as usize - 1;
+        if nums[t] > 0 {
+            nums[t] = -nums[t];
+        }
+    }
+    for i in 0..nums.len() {
+        if nums[i] > 0 { res.push(i as i32 + 1); }
+    }
+    res
 }
 // array
 #[test]
-#[ignore]
 fn test1_448() {
     assert_eq!(
         find_disappeared_numbers(vec![4, 3, 2, 7, 8, 2, 3, 1]),
