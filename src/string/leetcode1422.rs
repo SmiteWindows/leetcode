0b90c5163a85ff524e4a54727c3b6ae8a6ce8912
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 14:43:41 +0800

    202005131443

--- src/string/leetcode1422.rs
+++ src/string/leetcode1422.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn max_score(s: String) -> i32 {
-    todo!()
+    let s: Vec<char> = s.chars().collect();
+    let n = s.len();
+    let mut zeros = vec![0; n + 1];
+    for i in 0..n {
+        zeros[i + 1] = zeros[i] + if s[i] == '0' { 1 } else { 0 };
+    }
+
+    let mut ones = vec![0; n + 1];
+    for i in (0..n).rev() {
+        ones[i] = ones[i + 1] + if s[i] == '1' { 1 } else { 0 };
+    }
+    let mut res = 0;
+    for i in 1..n {
+        res = res.max(zeros[i] + ones[i]);
+    }
+    res
 }
 // string
 #[test]
-#[ignore]
 fn test1_1422() {
     assert_eq!(max_score(String::from("011101")), 5);
     assert_eq!(max_score(String::from("00111")), 5);
