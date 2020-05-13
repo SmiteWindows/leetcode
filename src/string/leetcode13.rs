1dc9fc9577269e08cc7bdd183e80bfb6686dc450
SmiteWindows <x1627283564@hotmail.com>
2020-05-13 18:21:02 +0800

    202005131820

--- src/string/leetcode13.rs
+++ src/string/leetcode13.rs
@@ -1,10 +1,35 @@
 // https://leetcode.com/problems/roman-to-integer/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashMap;
 pub fn roman_to_int(s: String) -> i32 {
-    todo!()
+    let map: HashMap<char, i32> = vec![
+        ('I', 1),
+        ('V', 5),
+        ('X', 10),
+        ('L', 50),
+        ('C', 100),
+        ('D', 500),
+        ('M', 1000),
+    ]
+    .into_iter()
+    .collect();
+    let mut sum = 0;
+    let mut last = 0;
+    for c in s.chars() {
+        if let Some(&v) = map.get(&c) {
+            if v > last {
+                sum += v - last - last;
+            } else {
+                sum += v
+            }
+            last = v;
+        }
+    }
+    sum
 }
 // math string
 #[test]
-#[ignore]
 fn test1_13() {
     assert_eq!(roman_to_int(String::from("III")), 3);
     assert_eq!(roman_to_int(String::from("IV")), 4);
