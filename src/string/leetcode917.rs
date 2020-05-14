ef4203982d93c1193a81a2562eba35bced865b6f
SmiteWindows <x1627283564@hotmail.com>
2020-05-14 08:21:09 +0800

    202005140821

--- src/string/leetcode917.rs
+++ src/string/leetcode917.rs
@@ -1,10 +1,31 @@
 // https://leetcode.com/problems/reverse-only-letters/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn reverse_only_letters(s: String) -> String {
-    todo!()
+    let n = s.len();
+    if n == 0 {
+        return "".to_string();
+    }
+    let mut a: Vec<char> = s.chars().collect();
+    let mut i = 0;
+    let mut j = n - 1;
+    while i < j {
+        while i < j && !a[i].is_alphabetic() {
+            i += 1;
+        }
+        while i < j && !a[j].is_alphabetic() {
+            j -= 1;
+        }
+        if i < j {
+            a.swap(i, j);
+        }
+        i += 1;
+        j -= 1;
+    }
+    a.iter().collect()
 }
 // string
 #[test]
-#[ignore]
 fn test1_917() {
     assert_eq!(
         reverse_only_letters(String::from("ab-cd")),
