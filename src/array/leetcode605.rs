d3d2ea0c23a924241f73a391689f422449e0060a
SmiteWindows <x1627283564@hotmail.com>
2020-05-14 21:13:52 +0800

    202005142113

--- src/array/leetcode605.rs
+++ src/array/leetcode605.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/can-place-flowers/
 pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
-    todo!()
+    let mut flowerbed = flowerbed;
+    let m = flowerbed.len();
+    let mut sum = 0;
+    for i in 0..m {
+        if flowerbed[i] == 0 {
+            if i == 0 {
+                sum += 1;
+                flowerbed[i] = 1;
+            } else if flowerbed[i - 1] == 0 {
+                sum += 1;
+                flowerbed[i] = 1;
+            }
+        } else if i > 0 && flowerbed[i - 1] == 1 {
+            sum -= 1;
+        }
+    }
+    sum >= n
 }
 // array
 #[test]
-#[ignore]
 fn test1_605() {
     assert_eq!(can_place_flowers(vec![1, 0, 0, 0, 1], 1), true);
     assert_eq!(can_place_flowers(vec![1, 0, 0, 0, 1], 2), false);
