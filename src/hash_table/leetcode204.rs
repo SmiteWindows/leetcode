7b0fbbc0ced33a8ce35c83921fdb01a308513e8f
SmiteWindows <x1627283564@hotmail.com>
2020-05-14 20:15:32 +0800

    202005142015

--- src/hash_table/leetcode204.rs
+++ src/hash_table/leetcode204.rs
@@ -1,10 +1,29 @@
 // https://leetcode.com/problems/count-primes/
+// Runtime: 4 ms
+// Memory Usage: 3.3 MB
 pub fn count_primes(n: i32) -> i32 {
-    todo!()
+    if n <= 2 {
+        return 0;
+    }
+    let n = n as usize;
+    let mut a = vec![true; n];
+    a[0] = false;
+    a[1] = false;
+    let mut i = 2;
+    while i * i < n {
+        if a[i] {
+            let mut j = 2;
+            while j * i < n {
+                a[i * j] = false;
+                j += 1;
+            }
+        }
+        i += 1;
+    }
+    a.iter().fold(0, |sum, &b| if b { sum + 1 } else { sum })
 }
 // math hash_table
 #[test]
-#[ignore]
 fn test2_204() {
     assert_eq!(count_primes(10), 4);
 }
