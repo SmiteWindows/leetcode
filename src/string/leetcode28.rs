2b6609861bef5a5761ea59f8b7b9ff7a6c8b46c3
SmiteWindows <x1627283564@hotmail.com>
2020-05-14 10:00:50 +0800

    202005141000

--- src/string/leetcode28.rs
+++ src/string/leetcode28.rs
@@ -1,10 +1,14 @@
 // https://leetcode.com/problems/implement-strstr/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn str_str(haystack: String, needle: String) -> i32 {
-    todo!()
+    match haystack.find(&needle) {
+        Some(index) => index as i32,
+        None => -1,
+    }
 }
 // string two_pointers
 #[test]
-#[ignore]
 fn test2_28() {
     assert_eq!(str_str(String::from("hello"), String::from("ll")), 2);
     assert_eq!(str_str(String::from("aaaaa"), String::from("bba")), -1);
