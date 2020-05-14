c94a78e06e4024b83c53f0d0d1af67c25eb7a87d
SmiteWindows <x1627283564@hotmail.com>
2020-05-14 08:59:09 +0800

    202005140859

--- src/string/leetcode859.rs
+++ src/string/leetcode859.rs
@@ -1,10 +1,42 @@
 // https://leetcode.com/problems/buddy-strings/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashSet;
 pub fn buddy_strings(a: String, b: String) -> bool {
-    todo!()
+    let ac: Vec<char> = a.chars().collect();
+    let bc: Vec<char> = b.chars().collect();
+    let n = ac.len();
+    let m = bc.len();
+    if n != m {
+        return false;
+    }
+    if ac == bc {
+        let mut hs: HashSet<char> = HashSet::new();
+        let mut sum = 0;
+        for &c in &ac {
+            if !hs.insert(c) {
+                sum += 1;
+            }
+        }
+        sum != 0
+    } else {
+        let mut pair = vec![];
+        for i in 0..n {
+            if ac[i] != bc[i] {
+                pair.push(i);
+            }
+        }
+        if pair.len() == 2 {
+            let i = pair[0];
+            let j = pair[1];
+            ac[i] == bc[j] && ac[j] == bc[i]
+        } else {
+            false
+        }
+    }
 }
 // string
 #[test]
-#[ignore]
 fn test1_859() {
     assert_eq!(buddy_strings(String::from("ab"), String::from("ba")), true);
     assert_eq!(buddy_strings(String::from("ab"), String::from("ab")), false);
