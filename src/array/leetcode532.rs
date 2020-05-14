31ca979b89f76e527fa2c236494891c8f8b1a416
SmiteWindows <x1627283564@hotmail.com>
2020-05-14 20:48:05 +0800

    202005142048

--- src/array/leetcode532.rs
+++ src/array/leetcode532.rs
@@ -1,10 +1,43 @@
 // https://leetcode.com/problems/k-diff-pairs-in-an-array/
+use std::{
+    cmp::Ordering::{Equal, Greater, Less},
+    collections::{HashMap, HashSet},
+};
 pub fn find_pairs(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut res = 0;
+    let mut hs = HashSet::new();
+    let mut hm = HashMap::new();
+    match k.cmp(&0) {
+        Equal => {
+            for x in nums {
+                let e = hm.entry(x).or_default();
+                if let 1 = *e {
+                    res += 1;
+                }
+                *e += 1;
+            }
+        }
+        Greater => {
+            for x in nums {
+                if hs.contains(&x) {
+                    continue;
+                } else {
+                    hs.insert(x);
+                    if hs.contains(&(x + k)) {
+                        res += 1;
+                    }
+                    if hs.contains(&(x - k)) {
+                        res += 1;
+                    }
+                }
+            }
+        }
+        Less => {}
+    }
+    res
 }
 // array two_pointers
 #[test]
-#[ignore]
 fn test2_532() {
     assert_eq!(find_pairs(vec![3, 1, 4, 1, 5], 2), 2);
     assert_eq!(find_pairs(vec![1, 2, 3, 4, 5], 1), 4);
