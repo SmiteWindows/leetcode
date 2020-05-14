7bd85824cd2d7a7ec7540bba65d554844f2b3a93
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 07:50:08 +0800

    202005150750

--- src/string/leetcode58.rs
+++ src/string/leetcode58.rs
@@ -1,10 +1,15 @@
 // https://leetcode.com/problems/length-of-last-word/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn length_of_last_word(s: String) -> i32 {
-    todo!()
+    if let Some(last) = s.split_whitespace().last() {
+        last.len() as i32
+    } else {
+        0
+    }
 }
 // string
 #[test]
-#[ignore]
 fn test1_58() {
     assert_eq!(length_of_last_word(String::from("Hello World")), 5);
 }
