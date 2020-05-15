d4ade939fdcb1c11c65b0435e8a1b7a9c14946a9
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 08:39:56 +0800

    202005150839

--- src/array/leetcode914.rs
+++ src/array/leetcode914.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
+use std::collections::HashMap;
 pub fn has_groups_size_x(deck: Vec<i32>) -> bool {
-    todo!()
+    fn gcd(a: i32, b: i32) -> i32 {
+        if a == 0 {
+            b
+        } else {
+            gcd(b % a, a)
+        }
+    }
+
+    let mut hm = HashMap::new();
+    let mut max = 0;
+    for x in deck {
+        *hm.entry(x).or_default() += 1;
+    }
+    for &v in hm.values() {
+        max = gcd(max, v);
+    }
+    max >= 2
 }
 // math array
 #[test]
-#[ignore]
 fn test2_914() {
     assert_eq!(has_groups_size_x(vec![1, 2, 3, 4, 4, 3, 2, 1]), true);
     assert_eq!(has_groups_size_x(vec![1, 1, 1, 2, 2, 2, 3, 3]), false);
