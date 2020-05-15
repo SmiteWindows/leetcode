4b9de66896832f7a18f547e4101d07c8d92d101e
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 20:23:47 +0800

    202005152023

--- src/array/leetcode1394.rs
+++ src/array/leetcode1394.rs
@@ -1,10 +1,24 @@
 // https://leetcode.com/problems/find-lucky-integer-in-an-array/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashMap;
 pub fn find_lucky(arr: Vec<i32>) -> i32 {
-    todo!()
+    let mut hm = HashMap::new();
+    for x in arr {
+        *hm.entry(x).or_default() += 1;
+    }
+    if let Some(x) = hm
+        .into_iter()
+        .filter_map(|(k, v)| if k == v { Some(k) } else { None })
+        .max()
+    {
+        x
+    } else {
+        -1
+    }
 }
 // array
 #[test]
-#[ignore]
 fn test1_1394() {
     assert_eq!(find_lucky(vec![2, 2, 3, 4]), 2);
     assert_eq!(find_lucky(vec![1, 2, 2, 3, 3, 3]), 3);
