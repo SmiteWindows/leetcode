b9d654dff7e90fa57fd80cc54616faa309849166
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 15:19:45 +0800

    202005151519

--- src/dynamic_programming/leetcode121.rs
+++ src/dynamic_programming/leetcode121.rs
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn max_profit(prices: Vec<i32>) -> i32 {
-    todo!()
+    let mut min_price = i32::MAX;
+    let mut max_profit = 0;
+    for (i, &price) in prices.iter().enumerate() {
+        min_price = min_price.min(price);
+        max_profit = max_profit.max(price - min_price);
+    }
+    max_profit
 }
 // dynamic_programming array
 #[test]
-#[ignore]
 fn test1_121() {
     assert_eq!(max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
     assert_eq!(max_profit(vec![7, 6, 4, 3, 1]), 0);
