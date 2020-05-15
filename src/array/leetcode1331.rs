63fbb98f1860ba9ad647921b61f6a720fd1a3fce
SmiteWindows <x1627283564@hotmail.com>
2020-05-16 07:53:09 +0800

    202005160753

--- src/array/leetcode1331.rs
+++ src/array/leetcode1331.rs
@@ -1,10 +1,20 @@
 // https://leetcode.com/problems/rank-transform-of-an-array/
+// Runtime: 32 ms
+// Memory Usage: 5.5 MB
+use std::collections::{BTreeSet, HashMap};
 pub fn array_rank_transform(arr: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut bts = BTreeSet::new();
+    let mut hm = HashMap::new();
+    for &x in &arr {
+        bts.insert(x);
+    }
+    for (i, x) in bts.into_iter().enumerate() {
+        hm.insert(x, i as i32 + 1);
+    }
+    arr.into_iter().map(|x| hm[&x]).collect()
 }
 // array
 #[test]
-#[ignore]
 fn test1_1331() {
     assert_eq!(array_rank_transform(vec![40, 10, 20, 30]), vec![4, 1, 2, 3]);
     assert_eq!(array_rank_transform(vec![100, 100, 100]), vec![1, 1, 1]);
