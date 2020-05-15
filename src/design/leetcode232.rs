50049d57565102045cffbe65b8ab1e4828cf1c4e
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 13:17:43 +0800

    202005151317

--- src/design/leetcode232.rs
+++ src/design/leetcode232.rs
@@ -1,6 +1,58 @@
 // https://leetcode.com/problems/implement-queue-using-stacks/
-struct MyQueue {}
+// Runtime: 0 ms
+// Memory Usage: 2 MB
+struct MyQueue {
+    stack: Vec<i32>,
+    temp: Vec<i32>,
+}
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyQueue {
+    /** Initialize your data structure here. */
+    fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+            temp: Vec::new(),
+        }
+    }
 
+    /** Push element x to the back of queue. */
+    fn push(&mut self, x: i32) {
+        self.stack.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    fn pop(&mut self) -> i32 {
+        while let Some(x) = self.stack.pop() {
+            self.temp.push(x);
+        }
+        let res = self.temp.pop().unwrap();
+        while let Some(x) = self.temp.pop() {
+            self.stack.push(x)
+        }
+        res
+    }
+
+    /** Get the front element. */
+    fn peek(&mut self) -> i32 {
+        while let Some(x) = self.stack.pop() {
+            self.temp.push(x);
+        }
+        let res = self.temp.pop().unwrap();
+        self.stack.push(res);
+        while let Some(x) = self.temp.pop() {
+            self.stack.push(x)
+        }
+        res
+    }
+
+    /** Returns whether the queue is empty. */
+    fn empty(&self) -> bool {
+        self.stack.is_empty()
+    }
+}
 /**
  * Your MyQueue object will be instantiated and called as such:
  * let obj = MyQueue::new();
@@ -9,33 +61,12 @@ struct MyQueue {}
  * let ret_3: i32 = obj.peek();
  * let ret_4: bool = obj.empty();
  */
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl MyQueue {
-    /** Initialize your data structure here. */
-    fn new() -> Self {
-        todo!()
-    }
-
-    /** Push element x to the back of queue. */
-    fn push(&self, x: i32) {
-        todo!()
-    }
-
-    /** Removes the element from in front of queue and returns that element. */
-    fn pop(&self) -> i32 {
-        todo!()
-    }
-
-    /** Get the front element. */
-    fn peek(&self) -> i32 {
-        todo!()
-    }
-
-    /** Returns whether the queue is empty. */
-    fn empty(&self) -> bool {
-        todo!()
-    }
+#[test]
+fn test2_232() {
+    let mut queue = MyQueue::new();
+    queue.push(1);
+    queue.push(2);
+    assert_eq!(queue.peek(), 1);
+    assert_eq!(queue.pop(), 1);
+    assert_eq!(queue.empty(), false);
 }
