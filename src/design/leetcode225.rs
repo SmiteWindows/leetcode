bffebc541bdaec4507bcc71c0d360a9954471bac
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 12:39:24 +0800

    202005151239

--- src/design/leetcode225.rs
+++ src/design/leetcode225.rs
@@ -1,6 +1,42 @@
 // https://leetcode.com/problems/implement-stack-using-queues/
-struct MyStack {}
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::VecDeque;
+struct MyStack {
+    queue: VecDeque<i32>,
+}
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyStack {
+    /** Initialize your data structure here. */
+    fn new() -> Self {
+        Self {
+            queue: VecDeque::new(),
+        }
+    }
 
+    /** Push element x onto stack. */
+    fn push(&mut self, x: i32) {
+        self.queue.push_back(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    fn pop(&mut self) -> i32 {
+        self.queue.pop_back().unwrap()
+    }
+
+    /** Get the top element. */
+    fn top(&self) -> i32 {
+        *self.queue.back().unwrap()
+    }
+
+    /** Returns whether the stack is empty. */
+    fn empty(&self) -> bool {
+        self.queue.is_empty()
+    }
+}
 /**
  * Your MyStack object will be instantiated and called as such:
  * let obj = MyStack::new();
@@ -9,33 +45,12 @@ struct MyStack {}
  * let ret_3: i32 = obj.top();
  * let ret_4: bool = obj.empty();
  */
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl MyStack {
-    /** Initialize your data structure here. */
-    fn new() -> Self {
-        todo!()
-    }
-
-    /** Push element x onto stack. */
-    fn push(&self, x: i32) {
-        todo!()
-    }
-
-    /** Removes the element on top of the stack and returns that element. */
-    fn pop(&self) -> i32 {
-        todo!()
-    }
-
-    /** Get the top element. */
-    fn top(&self) -> i32 {
-        todo!()
-    }
-
-    /** Returns whether the stack is empty. */
-    fn empty(&self) -> bool {
-        todo!()
-    }
+#[test]
+fn test2_225() {
+    let mut stack = MyStack::new();
+    stack.push(1);
+    stack.push(2);
+    assert_eq!(stack.top(), 2);
+    assert_eq!(stack.pop(), 2);
+    assert_eq!(stack.empty(), false);
 }
