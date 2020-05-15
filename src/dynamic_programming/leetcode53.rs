80d0d1bd0753cacb58e59798b55aace56f782601
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 13:39:53 +0800

    202005151339

--- src/dynamic_programming/leetcode53.rs
+++ src/dynamic_programming/leetcode53.rs
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/maximum-subarray/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut prev = 0;
+    let mut max = std::i32::MIN;
+    for &val in nums.iter() {
+        prev = val.max(prev + val);
+        max = max.max(prev);
+    }
+    max
 }
 // divide_and_conquer array dynamic_programming
 #[test]
-#[ignore]
 fn test3_53() {
     assert_eq!(max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
 }
