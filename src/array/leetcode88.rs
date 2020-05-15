1f9f118c198175e6a4168f1fc9a382cc7c014efd
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 10:22:19 +0800

    202005151022

--- src/array/leetcode88.rs
+++ src/array/leetcode88.rs
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/merge-sorted-array/
 pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-    todo!()
+    nums1.truncate(m as usize);
+    nums2.truncate(n as usize);
+    nums1.append(nums2);
+    nums1.sort_unstable();
 }
 // array two_pointers
 #[test]
-#[ignore]
 fn test2_88() {
     let mut nums1 = vec![1, 2, 3, 0, 0, 0];
     let mut nums2 = vec![2, 5, 6];
