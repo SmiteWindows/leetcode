c9baf6e0038ad713dc675800d1f4b92b8fe9144b
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 20:11:37 +0800

    202005152011

--- src/array/leetcode1413.rs
+++ src/array/leetcode1413.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn min_start_value(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut sum = 0;
+    let mut start = 1;
+    for val in nums.iter() {
+        sum += val;
+        if start + sum < 1 {
+            start = start.max(1 - sum);
+        }
+    }
+    start
 }
 // array
 #[test]
-#[ignore]
 fn test1_1413() {
     assert_eq!(min_start_value(vec![-3, 2, -3, 4, 2]), 5);
     assert_eq!(min_start_value(vec![1, 2]), 1);
