491fa913c45b13853b90dc56dc4ff4799f831e20
SmiteWindows <x1627283564@hotmail.com>
2020-05-15 10:04:56 +0800

    202005151004

--- src/design/leetcode155.rs
+++ src/design/leetcode155.rs
@@ -1,6 +1,70 @@
 // https://leetcode.com/problems/min-stack/
-struct MinStack {}
+// Runtime: 4 ms
+// Memory Usage: 5.4 MB
+struct MinStack {
+    nums: Vec<i32>,
+    mins: Vec<i32>,
+    top: i32,
+    min: i32,
+}
 
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MinStack {
+    /** initialize your data structure here. */
+    fn new() -> Self {
+        Self {
+            nums: vec![],
+            mins: vec![],
+            top: 0,
+            min: 0,
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.nums.push(x);
+        if let Some(&min) = self.mins.last() {
+            self.mins.push(x.min(min))
+        } else {
+            self.mins.push(x)
+        }
+        self.min = if let Some(&last) = self.mins.last() {
+            last
+        } else {
+            0
+        };
+        self.top = if let Some(&last) = self.nums.last() {
+            last
+        } else {
+            0
+        };
+    }
+
+    fn pop(&mut self) {
+        self.nums.pop();
+        self.mins.pop();
+        self.min = if let Some(&last) = self.mins.last() {
+            last
+        } else {
+            0
+        };
+        self.top = if let Some(&last) = self.nums.last() {
+            last
+        } else {
+            0
+        };
+    }
+
+    fn top(&self) -> i32 {
+        self.top
+    }
+
+    fn get_min(&self) -> i32 {
+        self.min
+    }
+}
 /**
  * Your MinStack object will be instantiated and called as such:
  * let obj = MinStack::new();
@@ -9,29 +73,14 @@ struct MinStack {}
  * let ret_3: i32 = obj.top();
  * let ret_4: i32 = obj.get_min();
  */
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl MinStack {
-    /** initialize your data structure here. */
-    fn new() -> Self {
-        todo!()
-    }
-
-    fn push(&self, x: i32) {
-        todo!()
-    }
-
-    fn pop(&self) {
-        todo!()
-    }
-
-    fn top(&self) -> i32 {
-        todo!()
-    }
-
-    fn get_min(&self) -> i32 {
-        todo!()
-    }
+#[test]
+fn test2_155() {
+    let mut min_stack = MinStack::new();
+    min_stack.push(-2);
+    min_stack.push(0);
+    min_stack.push(-3);
+    assert_eq!(min_stack.get_min(), -3);
+    min_stack.pop();
+    assert_eq!(min_stack.top(), 0);
+    assert_eq!(min_stack.get_min(), -2);
 }
