2866e0d9f108160c10d7bdbb5671986ca7c58f2c
SmiteWindows <x1627283564@hotmail.com>
2020-05-16 17:32:42 +0800

    202005161732

--- src/stack/leetcode1381.rs
+++ src/stack/leetcode1381.rs
@@ -1,11 +1,56 @@
 // https://leetcode.com/problems/design-a-stack-with-increment-operation/
-struct CustomStack {}
-
+// Runtime: 4 ms
+// Memory Usage: 2.9 MB
+struct CustomStack {
+    stack: Vec<i32>,
+    increment: Vec<i32>,
+    n: usize,
+    max_size: usize,
+}
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
+impl CustomStack {
+    fn new(max_size: i32) -> Self {
+        let max_size = max_size as usize;
+        let increment = vec![0; 1_usize + max_size];
+        Self {
+            stack: vec![],
+            increment,
+            n: 0,
+            max_size,
+        }
+    }
 
+    fn push(&mut self, x: i32) {
+        if self.n != self.max_size {
+            self.stack.push(x);
+            self.n += 1;
+        }
+    }
+
+    fn pop(&mut self) -> i32 {
+        if let Some(mut top) = self.stack.pop() {
+            self.increment[self.n - 1] += self.increment[self.n];
+            top += self.increment[self.n];
+            self.increment[self.n] = 0;
+            self.n -= 1;
+            top
+        } else {
+            -1
+        }
+    }
+
+    fn increment(&mut self, k: i32, val: i32) {
+        let k = k as usize;
+        if k > self.n {
+            self.increment[self.n] += val;
+        } else {
+            self.increment[k] += val;
+        }
+    }
+}
 /**
  * Your CustomStack object will be instantiated and called as such:
  * let obj = CustomStack::new(maxSize);
@@ -13,16 +58,19 @@ struct CustomStack {}
  * let ret_2: i32 = obj.pop();
  * obj.increment(k, val);
  */
-impl CustomStack {
-    fn new(max_size: i32) -> Self {
-        todo!()
-    }
-
-    fn push(&self, x: i32) {}
-
-    fn pop(&self) -> i32 {
-        todo!()
-    }
-
-    fn increment(&self, k: i32, val: i32) {}
+#[test]
+fn test1_1381() {
+    let mut stack = CustomStack::new(3);
+    stack.push(1);
+    stack.push(2);
+    assert_eq!(stack.pop(), 2);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.increment(5, 100);
+    stack.increment(2, 100);
+    assert_eq!(stack.pop(), 103);
+    assert_eq!(stack.pop(), 202);
+    assert_eq!(stack.pop(), 201);
+    assert_eq!(stack.pop(), -1);
 }
