5fafcd2224e6b1cc4fd4552071922eb598a8a1fa
SmiteWindows <x1627283564@hotmail.com>
2020-05-16 10:09:18 +0800

    202005161009

--- src/array/leetcode888.rs
+++ src/array/leetcode888.rs
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/fair-candy-swap/
+// Runtime: 8 ms
+// Memory Usage: 2.4 MB
+use std::collections::HashSet;
 pub fn fair_candy_swap(a: Vec<i32>, b: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let sum_a: i32 = a.iter().sum();
+    let sum_b: i32 = b.iter().sum();
+    let hs: HashSet<i32> = b.into_iter().collect();
+    for x in a {
+        let y = x + (sum_b - sum_a) / 2;
+        if hs.contains(&y) {
+            return vec![x, y];
+        }
+    }
+    unreachable!();
 }
 // array
 #[test]
-#[ignore]
 fn test1_888() {
     assert_eq!(fair_candy_swap(vec![1, 1], vec![2, 2]), vec![1, 2]);
     assert_eq!(fair_candy_swap(vec![1, 2], vec![2, 3]), vec![1, 2]);
