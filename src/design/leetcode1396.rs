2874996ee03d8460f85c27a9458f397fad3de1f8
SmiteWindows <x1627283564@hotmail.com>
2020-05-16 16:50:52 +0800

    202005161650

--- src/design/leetcode1396.rs
+++ src/design/leetcode1396.rs
@@ -1,5 +1,47 @@
 // https://leetcode.com/problems/design-underground-system/
-struct UndergroundSystem {}
+// Runtime: 36 ms
+// Memory Usage: 16.9 MB
+use std::collections::HashMap;
+#[derive(Default)]
+struct UndergroundSystem {
+    time: HashMap<String, HashMap<String, (i32, i32)>>,
+    customer: HashMap<i32, (String, i32)>,
+}
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl UndergroundSystem {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn check_in(&mut self, id: i32, station_name: String, t: i32) {
+        self.customer.insert(id, (station_name, t));
+    }
+
+    fn check_out(&mut self, id: i32, station_name: String, t: i32) {
+        let (start_station, start_t) = self.customer.remove(&id).expect("in");
+        let (sum, count) = self
+            .time
+            .entry(start_station)
+            .or_default()
+            .entry(station_name)
+            .or_default();
+        *sum += t - start_t;
+        *count += 1;
+    }
+
+    fn get_average_time(&mut self, start_station: String, end_station: String) -> f64 {
+        let (sum, count) = self
+            .time
+            .entry(start_station)
+            .or_default()
+            .entry(end_station)
+            .or_default();
+        *sum as f64 / *count as f64
+    }
+}
 /**
  * Your UndergroundSystem object will be instantiated and called as such:
  * let obj = UndergroundSystem::new();
@@ -7,25 +49,36 @@ struct UndergroundSystem {}
  * obj.check_out(id, stationName, t);
  * let ret_3: f64 = obj.get_average_time(startStation, endStation);
  */
-
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl UndergroundSystem {
-    fn new() -> Self {
-        todo!()
-    }
-
-    fn check_in(&self, id: i32, station_name: String, t: i32) {
-        todo!()
-    }
-
-    fn check_out(&self, id: i32, station_name: String, t: i32) {
-        todo!()
-    }
-
-    fn get_average_time(&self, start_station: String, end_station: String) -> f64 {
-        todo!()
-    }
+#[test]
+fn test1_1396() {
+    let mut obj = UndergroundSystem::new();
+    obj.check_in(45, "Leyton".to_string(), 3);
+    obj.check_in(32, "Paradise".to_string(), 8);
+    obj.check_in(27, "Leyton".to_string(), 10);
+    obj.check_out(45, "Waterloo".to_string(), 15);
+    obj.check_out(27, "Waterloo".to_string(), 20);
+    obj.check_out(32, "Cambridge".to_string(), 22);
+    let t = obj.get_average_time("Paradise".to_string(), "Cambridge".to_string());
+    assert!((t - 14.0).abs() <= f64::EPSILON);
+    let t = obj.get_average_time("Leyton".to_string(), "Waterloo".to_string());
+    assert!((t - 11.0).abs() <= f64::EPSILON);
+    obj.check_in(10, "Leyton".to_string(), 24);
+    let t = obj.get_average_time("Leyton".to_string(), "Waterloo".to_string());
+    assert!((t - 11.0).abs() <= f64::EPSILON);
+    obj.check_out(10, "Waterloo".to_string(), 38);
+    let t = obj.get_average_time("Leyton".to_string(), "Waterloo".to_string());
+    assert!((t - 12.0).abs() <= f64::EPSILON);
+    let mut obj = UndergroundSystem::new();
+    obj.check_in(10, "Leyton".to_string(), 3);
+    obj.check_out(10, "Paradise".to_string(), 8);
+    let t = obj.get_average_time("Leyton".to_string(), "Paradise".to_string());
+    assert!((t - 5.0).abs() <= f64::EPSILON);
+    obj.check_in(5, "Leyton".to_string(), 10);
+    obj.check_out(5, "Paradise".to_string(), 16);
+    let t = obj.get_average_time("Leyton".to_string(), "Paradise".to_string());
+    assert!((t - 5.5).abs() <= f64::EPSILON);
+    // obj.check_in(2, "Leyton".to_string(), 21);
+    // obj.check_out(2, "Paradise".to_string(), 30);
+    // let t = obj.get_average_time("Leyton".to_string(), "Paradise".to_string());
+    // assert!((t - 6.66667).abs() <= f64::EPSILON);
 }
