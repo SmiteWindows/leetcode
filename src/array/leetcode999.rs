15f872b7d93628002098d2550882ca2455421625
SmiteWindows <x1627283564@hotmail.com>
2020-05-16 08:34:43 +0800

    202005160834

--- src/array/leetcode999.rs
+++ src/array/leetcode999.rs
@@ -1,10 +1,87 @@
 // https://leetcode.com/problems/available-captures-for-rook/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+struct Chess {
+    r: usize,
+    c: usize,
+}
+
+enum Direction {
+    Right,
+    Down,
+    Left,
+    Up,
+}
+
 pub fn num_rook_captures(board: Vec<Vec<char>>) -> i32 {
-    todo!()
+    fn search_rook(board: &[Vec<char>]) -> Chess {
+        for r in 0..8 {
+            for c in 0..8 {
+                if board[r][c] == 'R' {
+                    return Chess { r, c };
+                }
+            }
+        }
+        unreachable!()
+    }
+
+    fn search_pawn(board: &[Vec<char>], rook: &Chess, direction: Direction) -> bool {
+        let mut r = rook.r;
+        let mut c = rook.c;
+
+        match direction {
+            Direction::Right => {
+                while c + 1 < 8 && board[r][c + 1] != 'B' {
+                    c += 1;
+                    if board[r][c] == 'p' {
+                        return true;
+                    }
+                }
+            }
+            Direction::Down => {
+                while r + 1 < 8 && board[r + 1][c] != 'B' {
+                    r += 1;
+                    if board[r][c] == 'p' {
+                        return true;
+                    }
+                }
+            }
+            Direction::Left => {
+                while c > 0 && board[r][c - 1] != 'B' {
+                    c -= 1;
+                    if board[r][c] == 'p' {
+                        return true;
+                    }
+                }
+            }
+            Direction::Up => {
+                while r > 0 && board[r - 1][c] != 'B' {
+                    r -= 1;
+                    if board[r][c] == 'p' {
+                        return true;
+                    }
+                }
+            }
+        }
+        false
+    }
+
+    let mut sum = 0;
+    let rook: Chess = search_rook(&board);
+    for direction in vec![
+        Direction::Right,
+        Direction::Down,
+        Direction::Left,
+        Direction::Up,
+    ] {
+        if search_pawn(&board, &rook, direction) {
+            sum += 1;
+        }
+    }
+    sum
 }
 // array
 #[test]
-#[ignore]
 fn test1_999() {
     assert_eq!(
         num_rook_captures(vec![
