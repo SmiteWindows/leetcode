612d3a8189591bc575310dd529be7d7a23188060
SmiteWindows <x1627283564@hotmail.com>
2020-05-16 09:04:16 +0800

    202005160904

--- src/array/leetcode989.rs
+++ src/array/leetcode989.rs
@@ -1,10 +1,27 @@
 //https://leetcode.com/problems/add-to-array-form-of-integer/
+// Runtime: 12 ms
+// Memory Usage: 2.2 MB
 pub fn add_to_array_form(a: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!()
+    let mut a = a;
+    let mut k = k;
+    let mut i = a.len() - 1;
+    while k > 0 {
+        let sum = a[i] + k;
+        a[i] = sum % 10;
+        k = sum / 10;
+        if i > 0 {
+            i -= 1;
+        } else {
+            a.insert(0, 0);
+        }
+    }
+    if a.len() > 1 && a[0] == 0 {
+        a.remove(0);
+    }
+    a
 }
 // array
 #[test]
-#[ignore]
 fn test1_989() {
     assert_eq!(add_to_array_form(vec![1, 2, 0, 0], 34), vec![1, 2, 3, 4]);
     assert_eq!(add_to_array_form(vec![2, 7, 4], 181), vec![4, 5, 5]);
