c01a9a407834a9ea489d5fc39659a27065f1fc7f
SmiteWindows <x1627283564@hotmail.com>
2020-05-16 09:55:50 +0800

    202005160955

--- src/array/leetcode896.rs
+++ src/array/leetcode896.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/monotonic-array/
+// Runtime: 8 ms
+// Memory Usage: 2.4 MB
 pub fn is_monotonic(a: Vec<i32>) -> bool {
-    todo!()
+    let mut increasing = true;
+    let mut decreasing = true;
+    let n = a.len();
+    for i in 1..n {
+        if a[i] > a[i - 1] {
+            decreasing = false;
+        }
+        if a[i] < a[i - 1] {
+            increasing = false;
+        }
+    }
+    increasing || decreasing
 }
 // array
 #[test]
-#[ignore]
 fn test1_896() {
     assert_eq!(is_monotonic(vec![1, 2, 2, 3]), true);
     assert_eq!(is_monotonic(vec![6, 5, 4, 4]), true);
