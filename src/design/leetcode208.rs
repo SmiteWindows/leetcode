eee0482c76f11c4f28d5582f2d2f1b9945b36ae8
SmiteWindows <x1627283564@hotmail.com>
2020-05-18 16:42:51 +0800

    202005181642

--- src/design/leetcode208.rs
+++ src/design/leetcode208.rs
@@ -1,13 +1,12 @@
 // https://leetcode.com/problems/implement-trie-prefix-tree/
-struct Trie {}
-
-/**
- * Your Trie object will be instantiated and called as such:
- * let obj = Trie::new();
- * obj.insert(word);
- * let ret_2: bool = obj.search(word);
- * let ret_3: bool = obj.starts_with(prefix);
- */
+// Runtime: 20 ms
+// Memory Usage: 11.7 MB
+use std::collections::HashMap;
+#[derive(Default)]
+struct Trie {
+    children: HashMap<char, Trie>,
+    end: bool,
+}
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
@@ -15,21 +14,62 @@ struct Trie {}
 impl Trie {
     /** Initialize your data structure here. */
     fn new() -> Self {
-        todo!()
+        Self {
+            children: HashMap::new(),
+            end: false,
+        }
     }
 
     /** Inserts a word into the trie. */
-    fn insert(&self, word: String) {
-        todo!()
+    fn insert(&mut self, word: String) {
+        let mut trie = self;
+        for c in word.chars() {
+            trie = trie.children.entry(c).or_default();
+        }
+        trie.end = true;
     }
 
     /** Returns if the word is in the trie. */
     fn search(&self, word: String) -> bool {
-        todo!()
+        let mut trie = self;
+        for c in word.chars() {
+            if let Some(child) = trie.children.get(&c) {
+                trie = child;
+            } else {
+                return false;
+            }
+        }
+        trie.end
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     fn starts_with(&self, prefix: String) -> bool {
-        todo!()
+        let mut trie = self;
+        for c in prefix.chars() {
+            if let Some(child) = trie.children.get(&c) {
+                trie = child;
+            } else {
+                return false;
+            }
+        }
+        true
     }
+}
+/**
+ * Your Trie object will be instantiated and called as such:
+ * let obj = Trie::new();
+ * obj.insert(word);
+ * let ret_2: bool = obj.search(word);
+ * let ret_3: bool = obj.starts_with(prefix);
+ */
+// design trie
+#[test]
+fn test2_208() {
+    let mut trie = Trie::new();
+    trie.insert("apple".to_string());
+    assert_eq!(trie.search("apple".to_string()), true);
+    assert_eq!(trie.search("app".to_string()), false);
+    assert_eq!(trie.starts_with("app".to_string()), true);
+    trie.insert("app".to_string());
+    assert_eq!(trie.search("app".to_string()), true);
 }
--- src/trie/leetcode208.rs
+++ src/trie/leetcode208.rs
@@ -1,11 +1,60 @@
 // https://leetcode.com/problems/implement-trie-prefix-tree/
-struct Trie {}
-
+// Runtime: 20 ms
+// Memory Usage: 11.7 MB
+use std::collections::HashMap;
+#[derive(Default)]
+struct Trie {
+    children: HashMap<char, Trie>,
+    end: bool,
+}
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
+impl Trie {
+    /** Initialize your data structure here. */
+    fn new() -> Self {
+        Self {
+            children: HashMap::new(),
+            end: false,
+        }
+    }
 
+    /** Inserts a word into the trie. */
+    fn insert(&mut self, word: String) {
+        let mut trie = self;
+        for c in word.chars() {
+            trie = trie.children.entry(c).or_default();
+        }
+        trie.end = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    fn search(&self, word: String) -> bool {
+        let mut trie = self;
+        for c in word.chars() {
+            if let Some(child) = trie.children.get(&c) {
+                trie = child;
+            } else {
+                return false;
+            }
+        }
+        trie.end
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    fn starts_with(&self, prefix: String) -> bool {
+        let mut trie = self;
+        for c in prefix.chars() {
+            if let Some(child) = trie.children.get(&c) {
+                trie = child;
+            } else {
+                return false;
+            }
+        }
+        true
+    }
+}
 /**
  * Your Trie object will be instantiated and called as such:
  * let obj = Trie::new();
@@ -13,25 +62,15 @@ struct Trie {}
  * let ret_2: bool = obj.search(word);
  * let ret_3: bool = obj.starts_with(prefix);
  */
-impl Trie {
-    /** Initialize your data structure here. */
-    fn new() -> Self {
-        todo!()
-    }
-
-    /** Inserts a word into the trie. */
-    fn insert(&self, word: String) {
-        todo!()
-    }
-
-    /** Returns if the word is in the trie. */
-    fn search(&self, word: String) -> bool {
-        todo!()
-    }
-
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    fn starts_with(&self, prefix: String) -> bool {
-        todo!()
-    }
-}
 // design trie
+#[test]
+fn test1_208() {
+    let mut trie = Trie::new();
+    trie.insert("apple".to_string());
+    assert_eq!(trie.search("apple".to_string()), true);
+    assert_eq!(trie.search("app".to_string()), false);
+    assert_eq!(trie.starts_with("app".to_string()), true);
+    trie.insert("app".to_string());
+    assert_eq!(trie.search("app".to_string()), true);
+}
+
