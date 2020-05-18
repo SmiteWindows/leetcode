cfe3335f91f7d862f0190b075f54267c525a8ded
SmiteWindows <x1627283564@hotmail.com>
2020-05-18 14:57:28 +0800

    202005181457

--- src/heap/leetcode295.rs
+++ src/heap/leetcode295.rs
@@ -1,11 +1,12 @@
 // https://leetcode.com/problems/find-median-from-data-stream/
-struct MedianFinder {}
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * let obj = MedianFinder::new();
- * obj.add_num(num);
- * let ret_2: f64 = obj.find_median();
- */
+// Runtime: 28 ms
+// Memory Usage: 11 MB
+use std::{cmp::Reverse, collections::BinaryHeap};
+#[derive(Default)]
+struct MedianFinder {
+    lo: BinaryHeap<i32>,
+    hi: BinaryHeap<Reverse<i32>>,
+}
 
 /**
  * `&self` means the method takes an immutable reference.
@@ -14,14 +15,39 @@ struct MedianFinder {}
 impl MedianFinder {
     /** initialize your data structure here. */
     fn new() -> Self {
-        todo!()
+        Self::default()
     }
 
-    fn add_num(&self, num: i32) {
-        todo!()
+    fn add_num(&mut self, num: i32) {
+        self.hi.push(Reverse(num));
+        let smallest = self.hi.pop().unwrap().0;
+        self.lo.push(smallest);
+        if self.lo.len() > self.hi.len() + 1 {
+            self.hi.push(Reverse(self.lo.pop().unwrap()));
+        }
     }
 
     fn find_median(&self) -> f64 {
-        todo!()
+        if (self.hi.len() + self.lo.len()) % 2 == 0 {
+            (self.hi.peek().unwrap().0 + *self.lo.peek().unwrap()) as f64 / 2.0
+        } else {
+            *self.lo.peek().unwrap() as f64
+        }
     }
+}
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * let obj = MedianFinder::new();
+ * obj.add_num(num);
+ * let ret_2: f64 = obj.find_median();
+ */
+// design heap
+#[test]
+fn test1_295() {
+    let mut obj = MedianFinder::new();
+    obj.add_num(1);
+    obj.add_num(2);
+    assert!((obj.find_median() - 1.5) <= f64::EPSILON);
+    obj.add_num(3);
+    assert!((obj.find_median() - 2.0) <= f64::EPSILON);
 }
