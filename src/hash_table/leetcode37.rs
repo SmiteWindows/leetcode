5a99ab098d4b5401f27c4d5c834b995cf0a29e0f
SmiteWindows <x1627283564@hotmail.com>
2020-05-20 21:25:38 +0800

    202005202125

--- src/hash_table/leetcode37.rs
+++ src/hash_table/leetcode37.rs
@@ -1,10 +1,60 @@
 // https://leetcode.com/problems/sudoku-solver/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
-    todo!()
+    fn dfs(
+        start: usize,
+        board: &mut Vec<Vec<char>>,
+        rows: &mut Vec<u32>,
+        cols: &mut Vec<u32>,
+        zones: &mut Vec<Vec<u32>>,
+    ) -> bool {
+        if start == 81 {
+            return true;
+        }
+        let i = start / 9;
+        let j = start % 9;
+        if board[i][j] == '.' {
+            for k in (0..9).rev() {
+                let bit = 1 << k;
+                if rows[i] & bit == 0 && cols[j] & bit == 0 && zones[i / 3][j / 3] & bit == 0 {
+                    board[i][j] = (b'1' + k as u8) as char;
+                    rows[i] |= bit;
+                    cols[j] |= bit;
+                    zones[i / 3][j / 3] |= bit;
+                    if dfs(start + 1, board, rows, cols, zones) {
+                        return true;
+                    }
+                    board[i][j] = '.';
+                    rows[i] &= !bit;
+                    cols[j] &= !bit;
+                    zones[i / 3][j / 3] &= !bit;
+                }
+            }
+            false
+        } else {
+            dfs(start + 1, board, rows, cols, zones)
+        }
+    }
+
+    let mut rows = vec![0; 9];
+    let mut cols = vec![0; 9];
+    let mut zones = vec![vec![0; 3]; 3];
+    for (i, row) in rows.iter_mut().enumerate().take(9) {
+        for (j, col) in cols.iter_mut().enumerate().take(9) {
+            let c = board[i][j];
+            if c != '.' {
+                let bit = 1 << (c as u8 - b'1');
+                *row |= bit;
+                *col |= bit;
+                zones[i / 3][j / 3] |= bit;
+            }
+        }
+    }
+    dfs(0, board, &mut rows, &mut cols, &mut zones);
 }
 // hash_table backtracking
 #[test]
-#[ignore]
 fn test2_37() {
     let mut board = vec![
         vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
