c8857880b6f64d73b7c3fd5fba6eebba0a89652a
SmiteWindows <x1627283564@hotmail.com>
2020-05-21 15:53:25 +0800

    202005211553

--- src/backtracking/leetcode22.rs
+++ src/backtracking/leetcode22.rs
@@ -1,10 +1,31 @@
 // https://leetcode.com/problems/generate-parentheses/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn generate_parenthesis(n: i32) -> Vec<String> {
-    todo!()
+    let mut res = vec![];
+    let mut v = vec![];
+    gen(&mut res, &mut v, n, n);
+    res
+}
+
+fn gen(res: &mut Vec<String>, v: &mut Vec<char>, left: i32, right: i32) {
+    if left == 0 && right == 0 {
+        res.push(v.iter().collect::<String>());
+    } else {
+        if left > 0 {
+            v.push('(');
+            gen(res, v, left - 1, right);
+            v.pop();
+        }
+        if right > left {
+            v.push(')');
+            gen(res, v, left, right - 1);
+            v.pop();
+        }
+    }
 }
 // string backtracking
 #[test]
-#[ignore]
 fn test1_22() {
     assert_eq!(
         generate_parenthesis(3),
