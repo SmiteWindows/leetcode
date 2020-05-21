76787c835b4246b6e3589848faf094ab77e24749
SmiteWindows <x1627283564@hotmail.com>
2020-05-21 15:14:38 +0800

    202005211514

--- src/string/leetcode17.rs
+++ src/string/leetcode17.rs
@@ -1,10 +1,49 @@
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+use std::collections::HashMap;
 pub fn letter_combinations(digits: String) -> Vec<String> {
-    todo!()
+    if digits.is_empty() {
+        return vec![];
+    }
+    let digits: Vec<char> = digits.chars().collect();
+    let hm: HashMap<char, Vec<char>> = [
+        ('2', "abc"),
+        ('3', "def"),
+        ('4', "ghi"),
+        ('5', "jkl"),
+        ('6', "mno"),
+        ('7', "pqrs"),
+        ('8', "tuv"),
+        ('9', "wxyz"),
+    ]
+    .iter()
+    .map(|(d, v)| (*d, v.chars().collect()))
+    .collect();
+    let mut s: Vec<char> = vec![];
+    let mut res: Vec<String> = vec![];
+    dfs(&hm, &digits, &mut s, &mut res, 0);
+    res
+}
+
+fn dfs(
+    hm: &HashMap<char, Vec<char>>,
+    digits: &[char],
+    s: &mut Vec<char>,
+    res: &mut Vec<String>,
+    index: usize,
+) {
+    if index == digits.len() {
+        res.push(s.iter().collect());
+    } else {
+        let d = digits[index];
+        for &c in hm[&d].iter() {
+            s.push(c);
+            dfs(hm, digits, s, res, index + 1);
+            s.pop();
+        }
+    }
 }
 // string backtracking
 #[test]
-#[ignore]
 fn test2_17() {
     assert_eq!(
         letter_combinations(String::from("23")),
