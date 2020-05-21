71e3ce8b359456530d54a15ccae8c15719a94b47
SmiteWindows <x1627283564@hotmail.com>
2020-05-21 20:47:31 +0800

    202005212047

--- src/backtracking/leetcode89.rs
+++ src/backtracking/leetcode89.rs
@@ -1,10 +1,15 @@
 // https://leetcode.com/problems/gray-code/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn gray_code(n: i32) -> Vec<i32> {
-    todo!()
+    let mut res = vec![];
+    for i in 0..(1 << n) {
+        res.push(i ^ i >> 1);
+    }
+    res
 }
 // backtracking
 #[test]
-#[ignore]
 fn test1_89() {
     assert_eq!(gray_code(2), vec![0, 1, 3, 2]);
     assert_eq!(gray_code(0), vec![0]);
