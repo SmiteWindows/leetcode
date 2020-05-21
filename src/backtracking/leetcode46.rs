6ebd3184acb9b55581a564b53a5c20512a792324
SmiteWindows <x1627283564@hotmail.com>
2020-05-21 16:14:22 +0800

    202005211614

--- src/backtracking/leetcode46.rs
+++ src/backtracking/leetcode46.rs
@@ -1,10 +1,27 @@
 // https://leetcode.com/problems/permutations/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut nums = nums;
+    let mut res = vec![];
+    let n = nums.len();
+    backtrack(&mut nums, &mut res, 0, n);
+    res
+}
+
+fn backtrack(nums: &mut Vec<i32>, res: &mut Vec<Vec<i32>>, start: usize, length: usize) {
+    if start == length {
+        res.push(nums.to_vec());
+        return;
+    }
+    for i in start..length {
+        nums.swap(start, i);
+        backtrack(nums, res, start + 1, length);
+        nums.swap(start, i);
+    }
 }
 // backtracking
 #[test]
-#[ignore]
 fn test1_46() {
     assert_eq!(
         permute(vec![1, 2, 3]),
@@ -13,8 +30,8 @@ fn test1_46() {
             vec![1, 3, 2],
             vec![2, 1, 3],
             vec![2, 3, 1],
-            vec![3, 1, 2],
-            vec![3, 2, 1]
+            vec![3, 2, 1],
+            vec![3, 1, 2]
         ]
     );
 }
