5b76cdac8f2eca394e676c8c2393be084f3ca9ca
SmiteWindows <x1627283564@hotmail.com>
2020-05-21 19:30:56 +0800

    202005211930

--- src/backtracking/leetcode78.rs
+++ src/backtracking/leetcode78.rs
@@ -1,31 +1,25 @@
 // https://leetcode.com/problems/subsets/
 // Runtime: 0 ms
-// Memory Usage: 2 MB
+// Memory Usage: 2.2 MB
 pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    fn backtrack(
-        start: usize,
-        curr: &mut Vec<i32>,
-        nums: &[i32],
-        res: &mut Vec<Vec<i32>>,
-        k: usize,
-        len: usize,
-    ) {
-        if curr.len() == k {
-            return res.push(curr.to_vec());
-        }
-        for i in start..len {
-            curr.push(nums[i]);
-            backtrack(i + 1, curr, nums, res, k, len);
-            curr.pop();
-        }
-    }
     let mut res = vec![vec![]];
     let mut curr = vec![];
     let n = nums.len();
     for k in 1..=n {
-        backtrack(0, &mut curr, &nums, &mut res, k, n);
+        backtrack(0, &mut curr, &nums, &mut res, k);
     }
     res
+}
+
+fn backtrack(start: usize, curr: &mut Vec<i32>, nums: &[i32], res: &mut Vec<Vec<i32>>, k: usize) {
+    if curr.len() == k {
+        return res.push(curr.to_vec());
+    }
+    for (i, &num) in nums.iter().enumerate().skip(start) {
+        curr.push(num);
+        backtrack(i + 1, curr, nums, res, k);
+        curr.pop();
+    }
 }
 // array backtracking bit_manipulation
 #[test]
