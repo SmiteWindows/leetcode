2626f3b915407a92ad6e618b464d1447eb74a1ad
SmiteWindows <x1627283564@hotmail.com>
2020-05-21 10:10:53 +0800

    202005211010

--- src/string/leetcode38.rs
+++ src/string/leetcode38.rs
@@ -1,10 +1,45 @@
 // https://leetcode.com/problems/count-and-say/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+struct Pair {
+    digit: char,
+    count: usize,
+}
 pub fn count_and_say(n: i32) -> String {
-    todo!()
+    fn next(nums: String) -> String {
+        let mut s = String::from("");
+        let mut prev: Option<Pair> = None;
+        for c in nums.chars() {
+            if let Some(prev_pair) = prev {
+                if prev_pair.digit == c {
+                    prev = Some(Pair {
+                        digit: c,
+                        count: prev_pair.count + 1,
+                    });
+                } else {
+                    s.push_str(&prev_pair.count.to_string());
+                    s.push_str(&prev_pair.digit.to_string());
+                    prev = Some(Pair { digit: c, count: 1 });
+                }
+            } else {
+                prev = Some(Pair { digit: c, count: 1 });
+            }
+        }
+        if let Some(prev_pair) = prev {
+            s.push_str(&prev_pair.count.to_string());
+            s.push_str(&prev_pair.digit.to_string());
+        }
+        s
+    }
+
+    match n {
+        1 => "1".to_string(),
+        2..=30 => next(count_and_say(n - 1)),
+        _ => "".to_string(),
+    }
 }
 // string
 #[test]
-#[ignore]
 fn test1_38() {
     assert_eq!(count_and_say(1), String::from("1"));
     assert_eq!(count_and_say(4), String::from("1211"));
