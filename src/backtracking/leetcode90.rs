9c2ab29c3d8ce262d8ec5795f55cc584434bda5a
SmiteWindows <x1627283564@hotmail.com>
2020-05-21 21:15:10 +0800

    202005212115

--- src/backtracking/leetcode90.rs
+++ src/backtracking/leetcode90.rs
@@ -1,20 +1,42 @@
 // https://leetcode.com/problems/subsets-ii/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn subsets_with_dup(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut nums = nums;
+    nums.sort_unstable();
+    let n = nums.len();
+    let mut cur = vec![];
+    let mut res = vec![];
+    dfs(0, &mut cur, &mut res, &nums, n);
+    res
+}
+
+fn dfs(start: usize, cur: &mut Vec<i32>, res: &mut Vec<Vec<i32>>, nums: &[i32], n: usize) {
+    res.push(cur.to_vec());
+    if start == n {
+        return;
+    }
+    for (i, &num) in nums.iter().enumerate().skip(start) {
+        if i > start && num == nums[i - 1] {
+            continue;
+        }
+        cur.push(num);
+        dfs(i + 1, cur, res, nums, n);
+        cur.pop();
+    }
 }
 // array backtracking
 #[test]
-#[ignore]
 fn test1_90() {
     assert_eq!(
         subsets_with_dup(vec![1, 2, 2]),
         vec![
-            vec![2],
-            vec![1],
-            vec![1, 2, 2],
-            vec![2, 2],
-            vec![1, 2],
             vec![],
+            vec![1],
+            vec![1, 2],
+            vec![1, 2, 2],
+            vec![2],
+            vec![2, 2]
         ]
     );
 }
