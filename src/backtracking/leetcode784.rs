86bf38857212ec2636b8ecdefd88c0d3b47fdcbe
SmiteWindows <x1627283564@hotmail.com>
2020-05-22 12:05:16 +0800

    202005221205

--- src/backtracking/leetcode784.rs
+++ src/backtracking/leetcode784.rs
@@ -2,31 +2,31 @@
 // Runtime: 4 ms
 // Memory Usage: 2.3 MB
 pub fn letter_case_permutation(s: String) -> Vec<String> {
-    fn permutation(s: &[char], n: usize, i: usize, t: &mut String, res: &mut Vec<String>) {
-        if i == n {
-            res.push(t.clone());
-        } else if s[i].is_alphabetic() {
-            let lower: char = s[i].to_ascii_lowercase();
-            let upper: char = s[i].to_ascii_uppercase();
-            t.push(lower);
-            permutation(s, n, i + 1, t, res);
-            t.pop();
-            t.push(upper);
-            permutation(s, n, i + 1, t, res);
-            t.pop();
-        } else {
-            t.push(s[i]);
-            permutation(s, n, i + 1, t, res);
-            t.pop();
-        }
-    }
-
     let n = s.len();
     let s: Vec<char> = s.chars().collect();
     let mut res = vec![];
     let mut t = "".to_string();
     permutation(&s, n, 0, &mut t, &mut res);
     res
+}
+
+fn permutation(s: &[char], n: usize, i: usize, t: &mut String, res: &mut Vec<String>) {
+    if i == n {
+        res.push(t.clone());
+    } else if s[i].is_alphabetic() {
+        let lower: char = s[i].to_ascii_lowercase();
+        let upper: char = s[i].to_ascii_uppercase();
+        t.push(lower);
+        permutation(s, n, i + 1, t, res);
+        t.pop();
+        t.push(upper);
+        permutation(s, n, i + 1, t, res);
+        t.pop();
+    } else {
+        t.push(s[i]);
+        permutation(s, n, i + 1, t, res);
+        t.pop();
+    }
 }
 // backtracking bit_manipulation
 #[test]
