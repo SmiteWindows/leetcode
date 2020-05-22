d34eecdd2e016a5498d94daa19cbd9d9381ecf2c
SmiteWindows <x1627283564@hotmail.com>
2020-05-22 11:56:38 +0800

    202005221156

--- src/backtracking/leetcode526.rs
+++ src/backtracking/leetcode526.rs
@@ -1,10 +1,29 @@
 // https://leetcode.com/problems/beautiful-arrangement/
+// Runtime: 32 ms
+// Memory Usage: 2.1 MB
 pub fn count_arrangement(n: i32) -> i32 {
-    todo!()
+    let mut res = 0;
+    let mut nums = (1..=n).collect();
+    let n = n as usize;
+    dfs(0, &mut nums, &mut res, n);
+    res
+}
+
+fn dfs(start: usize, nums: &mut Vec<i32>, res: &mut i32, n: usize) {
+    if start == n {
+        *res += 1;
+    } else {
+        for i in start..n {
+            if nums[i] % (start as i32 + 1) == 0 || (start as i32 + 1) % nums[i] == 0 {
+                nums.swap(start, i);
+                dfs(start + 1, nums, res, n);
+                nums.swap(start, i);
+            }
+        }
+    }
 }
 // backtracking
 #[test]
-#[ignore]
 fn test1_526() {
     assert_eq!(count_arrangement(2), 2);
 }
