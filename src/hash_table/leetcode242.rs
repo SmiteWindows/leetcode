0afc3b0519632c0ecae0d34fe4078783322e5284
SmiteWindows <x1627283564@hotmail.com>
2020-05-25 09:55:57 +0800

    202005250955

--- src/hash_table/leetcode242.rs
+++ src/hash_table/leetcode242.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/valid-anagram/
+// Runtime: 4 ms
+// Memory Usage: 2.3 MB
+use std::collections::HashMap;
 pub fn is_anagram(s: String, t: String) -> bool {
-    todo!()
+    let mut hm: HashMap<char, i32>= HashMap::new();
+    for sc in s.chars() {
+        *hm.entry(sc).or_default() += 1;
+    }
+    for tc in t.chars() {
+        *hm.entry(tc).or_default() -= 1;
+    }
+    hm.values().all(|&x|x==0)
 }
 // sort hash_table
 #[test]
-#[ignore]
 fn test2_242() {
     assert_eq!(
         is_anagram(String::from("anagram"), String::from("nagaram")),
