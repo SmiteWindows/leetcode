3c776efb607b2dc91f1e657c09bc9ae31e7427ca
SmiteWindows <x1627283564@hotmail.com>
2020-05-25 12:18:04 +0800

    202005251218

--- src/binary_search/leetcode349.rs
+++ src/binary_search/leetcode349.rs
@@ -1,29 +1,17 @@
 // https://leetcode.com/problems/intersection-of-two-arrays/
 // Runtime: 0 ms
-// Memory Usage: 2 MB
-use std::{
-    cmp::Ordering::{Equal, Greater, Less},
-    collections::HashSet,
-};
+// Memory Usage: 2.1 MB
+use std::collections::HashSet;
 pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
     let mut nums1 = nums1;
     nums1.sort_unstable();
     let mut nums2 = nums2;
     nums2.sort_unstable();
     let mut hs = HashSet::new();
-    let (mut i, mut j) = (0, 0);
-    while i < nums1.len() && j < nums2.len() {
-        match nums1[i].cmp(&nums2[j]) {
-            Less => {
-                i += 1;
-            }
-            Equal => {
-                hs.insert(nums1[i]);
-                i += 1;
-                j += 1;
-            }
-            Greater => {
-                j += 1;
+    for i in nums2 {
+        if !hs.contains(&i) {
+            if nums1.binary_search(&i).is_ok() {
+                hs.insert(i);
             }
         }
     }
@@ -33,5 +21,5 @@ pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
 #[test]
 fn test2_349() {
     assert_eq!(intersection(vec![1, 2, 2, 1], vec![2, 2]), vec![2]);
-    assert_eq!(intersection(vec![4, 9, 5], vec![9, 4, 9, 8, 4]), vec![4, 9]);
+    assert_eq!(intersection(vec![4, 9, 5], vec![9, 4, 9, 8, 4]), vec![9, 4]);
 }
