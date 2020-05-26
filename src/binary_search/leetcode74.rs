a8d410897dbab629225d584a28fe8c9e101df66a
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 15:42:10 +0800

    202005261542

--- src/binary_search/leetcode74.rs
+++ src/binary_search/leetcode74.rs
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/search-a-2d-matrix/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-    todo!()
+    let mut a = vec![];
+    for row in matrix {
+        for x in row {
+            a.push(x);
+        }
+    }
+    a.binary_search(&target).is_ok()
 }
 // binary_search array
 #[test]
-#[ignore]
 fn test1_74() {
     assert_eq!(
         search_matrix(
