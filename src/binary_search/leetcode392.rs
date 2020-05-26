822d9adc4641b500c9aeb90ff64456648f65b07c
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 08:28:39 +0800

    202005260828

--- src/binary_search/leetcode392.rs
+++ src/binary_search/leetcode392.rs
@@ -1,23 +1,10 @@
 // https://leetcode.com/problems/is-subsequence/
-// Runtime: 0 ms
-// Memory Usage: 2.1 MB
 pub fn is_subsequence(s: String, t: String) -> bool {
-    let mut i = 0;
-    let mut j = 0;
-    let sn = s.len();
-    let tm = t.len();
-    while i < sn && j < tm {
-        if s[i..=i] == t[j..=j] {
-            i += 1;
-            j += 1;
-        } else {
-            j += 1;
-        }
-    }
-    i == sn
+    todo!()
 }
 // binary_search dynamic_programming greedy
 #[test]
+#[ignore]
 fn test1_392() {
     assert_eq!(
         is_subsequence(String::from("abc"), String::from("ahbgdc")),
