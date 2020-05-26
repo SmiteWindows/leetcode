6bd2664b30ac3f191581316636be86056f4bbf6f
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 09:05:15 +0800

    202005260905

--- src/math/leetcode441.rs
+++ src/math/leetcode441.rs
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/arranging-coins/
+// Runtime: 0 ms
+// Memory Usage: 2.4 MB
 pub fn arrange_coins(n: i32) -> i32 {
-    todo!()
+    (((2 * n as i64) as f64 + 0.25).sqrt() - 0.5).floor() as i32
 }
 // math binary_search
 #[test]
-#[ignore]
 fn test1_441() {
     assert_eq!(arrange_coins(5), 2);
     assert_eq!(arrange_coins(8), 3);
