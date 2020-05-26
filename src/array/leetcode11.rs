055acf579da298fa58c9c89ebe9dd3c88bc4a9e2
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 17:48:51 +0800

    202005261748

--- src/array/leetcode11.rs
+++ src/array/leetcode11.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/container-with-most-water/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn max_area(height: Vec<i32>) -> i32 {
-    todo!()
+    let mut l = 0;
+    let mut r = height.len() - 1;
+    let mut max = 0;
+    while l < r {
+        max = max.max(height[l].min(height[r]) * (r - l) as i32);
+        if height[l] < height[r] {
+            l += 1;
+        } else {
+            r -= 1;
+        }
+    }
+    max
 }
 // array two_pointers
 #[test]
-#[ignore]
 fn test2_11() {
     assert_eq!(max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
 }
