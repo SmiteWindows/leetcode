02b359f4b9129e881f09382a09cfc61e4c78fb49
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 17:09:48 +0800

    202005261709

--- src/binary_search/leetcode154.rs
+++ src/binary_search/leetcode154.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+use std::cmp::Ordering::{Equal, Greater, Less};
 pub fn find_min(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut l = 0;
+    let mut r = nums.len() - 1;
+    while l < r {
+        let m = l + (r - l) / 2;
+        match nums[m].cmp(&nums[r]) {
+            Less => r = m,
+            Greater => l = m + 1,
+            Equal => r -= 1,
+        }
+    }
+    nums[l]
 }
 // binary_search array
 #[test]
-#[ignore]
 fn test1_154() {
     assert_eq!(find_min(vec![1, 3, 5]), 1);
     assert_eq!(find_min(vec![2, 2, 2, 0, 1]), 0);
