0bb7d8e5c79b3d9e5c3ef3eb279189b2d9830fbd
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 18:58:24 +0800

    202005261858

--- src/two_pointers/leetcode15.rs
+++ src/two_pointers/leetcode15.rs
@@ -1,10 +1,51 @@
 // https://leetcode.com/problems/3sum/
+// Runtime: 16 ms
+// Memory Usage: 3.4 MB
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut res = vec![];
+    let n = nums.len();
+    if n < 3 {
+        return res;
+    }
+    let mut nums = nums;
+    nums.sort_unstable();
+    for i in 0..n - 2 {
+        let a = nums[i];
+        if i > 0 && nums[i] == nums[i - 1] {
+            continue;
+        }
+        if nums[i] > 0 {
+            break;
+        }
+        let mut j = i + 1;
+        let mut k = n - 1;
+        while j < k {
+            let b = nums[j];
+            let c = nums[k];
+            let sum = a + b + c;
+            if sum == 0 {
+                res.push(vec![a, b, c]);
+                j += 1;
+                k -= 1;
+                while j < k && nums[j] == nums[j - 1] {
+                    j += 1;
+                }
+                while j < k && nums[k] == nums[k + 1] {
+                    k -= 1;
+                }
+            } else {
+                if sum < 0 {
+                    j += 1;
+                } else {
+                    k -= 1;
+                }
+            }
+        }
+    }
+    res
 }
 // array two_pointers
 #[test]
-#[ignore]
 fn test1_15() {
     assert_eq!(
         three_sum(vec![-1, 0, 1, 2, -1, -4]),
