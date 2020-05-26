a26828548decf693fad41caf65ebabcf8e3713a6
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 11:31:27 +0800

    202005261131

--- src/binary_search/leetcode744.rs
+++ src/binary_search/leetcode744.rs
@@ -1,10 +1,14 @@
 // https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+// Runtime: 4 ms
+// Memory Usage: 2.7 MB
 pub fn next_greatest_letter(letters: Vec<char>, target: char) -> char {
-    todo!()
+    match letters.binary_search(&target) {
+        Ok(i) => letters[(i + 1) % letters.len()],
+        Err(i) => letters[i % letters.len()],
+    }
 }
 // binary_search
 #[test]
-#[ignore]
 fn test1_744() {
     assert_eq!(next_greatest_letter(vec!['c', 'f', 'j'], 'a'), 'c');
     assert_eq!(next_greatest_letter(vec!['c', 'f', 'j'], 'c'), 'f');
