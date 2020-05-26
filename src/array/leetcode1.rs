fb7725513f20c91cf5f9ee2b2a9ab8e7420691a5
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 12:06:39 +0800

    202005261206

--- src/array/leetcode1.rs
+++ src/array/leetcode1.rs
@@ -1,21 +1,16 @@
 // https://leetcode.com/problems/two-sum/
 // Runtime: 0 ms
-// Memory Usage: 2.2 MB
+// Memory Usage: 2.4 MB
 use std::collections::HashMap;
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let nums_map: HashMap<i32, usize> = nums
-        .iter()
-        .enumerate()
-        .map(|(index, value)| (target - value, index))
-        .collect();
-    for (index, value) in nums.iter().enumerate() {
-        if let Some(&j) = nums_map.get(value) {
-            if index != j {
-                return vec![index as i32, j as i32];
-            }
+    let mut hm = HashMap::new();
+    for (i, &e) in nums.iter().enumerate() {
+        if let Some(v) = hm.get(&(target - e)) {
+            return vec![*v, i as i32];
         }
+        hm.insert(e, i as i32);
     }
-    unreachable!();
+    return vec![];
 }
 // array hash_table
 #[test]
