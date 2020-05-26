88f6a0e8d04d1bd271a55ddb1ad43ff0118d13ee
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 20:56:44 +0800

    202005262056

--- src/two_pointers/leetcode15.rs
+++ src/two_pointers/leetcode15.rs
@@ -10,7 +10,7 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
     let mut nums = nums;
     nums.sort_unstable();
     for i in 0..n - 2 {
-        let a = nums[i];
+        let ai = nums[i];
         if i > 0 && nums[i] == nums[i - 1] {
             continue;
         }
@@ -20,11 +20,11 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut j = i + 1;
         let mut k = n - 1;
         while j < k {
-            let b = nums[j];
-            let c = nums[k];
-            let sum = a + b + c;
+            let bj = nums[j];
+            let ck = nums[k];
+            let sum = ai + bj + ck;
             if sum == 0 {
-                res.push(vec![a, b, c]);
+                res.push(vec![ai, bj, ck]);
                 j += 1;
                 k -= 1;
                 while j < k && nums[j] == nums[j - 1] {
@@ -33,12 +33,10 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
                 while j < k && nums[k] == nums[k + 1] {
                     k -= 1;
                 }
+            } else if sum < 0 {
+                j += 1;
             } else {
-                if sum < 0 {
-                    j += 1;
-                } else {
-                    k -= 1;
-                }
+                k -= 1;
             }
         }
     }
