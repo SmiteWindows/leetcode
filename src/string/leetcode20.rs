b5dc3905537db89e8a9d62d2e17262ceb8f047a0
SmiteWindows <x1627283564@hotmail.com>
2020-05-26 17:39:51 +0800

    202005261739

--- src/string/leetcode20.rs
+++ src/string/leetcode20.rs
@@ -1,10 +1,29 @@
 // https://leetcode.com/problems/valid-parentheses/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn is_valid(s: String) -> bool {
-    todo!()
+    let mut stack: Vec<char> = vec![];
+    for c in s.chars() {
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' | ']' | '}' => match stack.pop() {
+                Some(t) => {
+                    if !((t == '{' && c == '}') || (t == '(' && c == ')') || (t == '[' && c == ']'))
+                    {
+                        return false;
+                    }
+                }
+                None => {
+                    return false;
+                }
+            },
+            _ => {}
+        }
+    }
+    stack.is_empty()
 }
 // stack string
 #[test]
-#[ignore]
 fn test2_20() {
     assert_eq!(is_valid(String::from("()")), true);
     assert_eq!(is_valid(String::from("()[]{}")), true);
