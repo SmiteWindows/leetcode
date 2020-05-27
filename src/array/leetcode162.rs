d05847c268407693ca44ad24a81810ac9b173c0c
SmiteWindows <x1627283564@hotmail.com>
2020-05-27 14:01:18 +0800

    202005271401

--- src/array/leetcode162.rs
+++ src/array/leetcode162.rs
@@ -21,6 +21,6 @@ fn test2_162() {
     assert_eq!(find_peak_element(vec![1, 2, 3, 1]), 2);
     assert_eq!(
         find_peak_element(vec![1, 2, 1, 3, 5, 6, 4]),
-        1 // 5
+        5
     );
 }
