e8631bc2da989e66d444b31745d95944d152a1cd
SmiteWindows <x1627283564@hotmail.com>
2020-05-27 14:08:16 +0800

    202005271408

--- src/array/leetcode18.rs
+++ src/array/leetcode18.rs
@@ -66,9 +66,9 @@ pub fn four_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
 }
 // array two_pointers hash_table
 #[test]
-fn test2_18() {
+fn test1_18() {
     assert_eq!(
         four_sum(vec![1, 0, -1, 0, -2, 2], 0),
-        vec![vec![-1, 0, 0, 1], vec![-2, -1, 1, 2], vec![-2, 0, 0, 2]]
+        vec![vec![-2, -1, 1, 2], vec![-2, 0, 0, 2], vec![-1, 0, 0, 1]]
     );
 }
