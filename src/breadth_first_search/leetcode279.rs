b0735cf5cbfe5520a66e9b50fa041d4b8117bfb7
SmiteWindows <x1627283564@hotmail.com>
2020-05-29 20:50:40 +0800

    202005292050

--- src/breadth_first_search/leetcode279.rs
+++ src/breadth_first_search/leetcode279.rs
@@ -1,10 +1,42 @@
 // https://leetcode.com/problems/perfect-squares/
+// Runtime: 32 ms
+// Memory Usage: 2.2 MB
+use std::{
+    cmp::Ordering::{Equal, Greater, Less},
+    collections::HashSet,
+};
 pub fn num_squares(n: i32) -> i32 {
-    todo!()
+    let mut square_nums = vec![];
+    let mut i = 1;
+    while i * i <= n {
+        square_nums.push(i * i);
+        i += 1;
+    }
+    let mut queue = HashSet::new();
+    queue.insert(n);
+
+    let mut level = 0;
+    while queue.len() > 0 {
+        level += 1;
+        let mut next_queue = HashSet::new();
+
+        for &remainder in &queue {
+            for &square in &square_nums {
+                match remainder.cmp(&square) {
+                    Less => break,
+                    Equal => return level,
+                    Greater => {
+                        next_queue.insert(remainder - square);
+                    }
+                }
+            }
+        }
+        queue = next_queue;
+    }
+    level
 }
 // math breadth_first_search dynamic_programming
 #[test]
-#[ignore]
 fn test3_279() {
     assert_eq!(num_squares(12), 3);
     assert_eq!(num_squares(13), 2);
