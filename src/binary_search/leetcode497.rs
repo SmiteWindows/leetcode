8d5e56ea05856861d905cfb3993361ea53d32bc8
SmiteWindows <x1627283564@hotmail.com>
2020-05-29 11:29:50 +0800

    202005291129

--- src/binary_search/leetcode497.rs
+++ src/binary_search/leetcode497.rs
@@ -1,21 +1,64 @@
 // https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/
-struct Solution {}
-/**
- * Your Solution object will be instantiated and called as such:
- * let obj = Solution::new(rects);
- * let ret_1: Vec<i32> = obj.pick();
- */
-
+// Runtime: 12 ms
+// Memory Usage: 4.6 MB
+use rand::prelude::*;
+use rand::distributions::WeightedIndex;
+struct Solution {
+    rects:Vec<Vec<i32>>,
+    rng:ThreadRng,
+    len:usize,
+    dist:WeightedIndex<i32>,
+}
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl Solution {
     fn new(rects: Vec<Vec<i32>>) -> Self {
-        todo!()
+        let rng= thread_rng();
+        let len=rects.len();
+        let weights: Vec<i32>=rects.iter().map(|v|(v[2]-v[0]+1)*(v[3]-v[1]+1)).collect();
+        let dist=WeightedIndex::new(weights).unwrap();
+        Self{
+            rects,
+            rng,
+            len,
+            dist,
+        }
     }
 
-    fn pick(&self) -> Vec<i32> {
-        todo!()
+    fn pick(&mut self) -> Vec<i32> {
+        let rect=&self.rects[self.rng.sample(&self.dist)];
+        let x=self.rng.gen_range(rect[0],rect[2]+1);
+        let y=self.rng.gen_range(rect[1],rect[3]+1);
+        vec![x,y]
     }
+}
+/**
+ * Your Solution object will be instantiated and called as such:
+ * let obj = Solution::new(rects);
+ * let ret_1: Vec<i32> = obj.pick();
+ */
+// random binary_search
+#[test]
+fn test1_947() {
+    let mut obj = Solution::new(vec![vec![1,1,5,5]]);
+    let res1=obj.pick();
+    let res2=obj.pick();
+    let res3=obj.pick();
+    println!("{:?}",res1);
+    println!("{:?}",res2);
+    println!("{:?}",res3);
+    println!();
+    let mut obj = Solution::new(vec![vec![-2,-2,-1,-1],vec![1,0,3,0]]);
+    let res1=obj.pick();
+    let res2=obj.pick();
+    let res3=obj.pick();
+    let res4=obj.pick();
+    let res5=obj.pick();
+    println!("{:?}",res1);
+    println!("{:?}",res2);
+    println!("{:?}",res3);
+    println!("{:?}",res4);
+    println!("{:?}",res5);
 }
