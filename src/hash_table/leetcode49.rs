9ca83aa737f6e37e75ffeb5422d782454a00abac
SmiteWindows <x1627283564@hotmail.com>
2020-05-29 16:06:54 +0800

    202005291606

--- src/hash_table/leetcode49.rs
+++ src/hash_table/leetcode49.rs
@@ -1,10 +1,24 @@
 // https://leetcode.com/problems/group-anagrams/
+// Runtime: 8 ms
+// Memory Usage: 4.3 MB
+use std::collections::HashMap;
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-    todo!()
+    let mut hm: HashMap<Vec<char>, Vec<String>> = HashMap::new();
+    for s in strs {
+        let mut v = s.chars().collect::<Vec<_>>();
+        v.sort_unstable();
+        hm.entry(v).or_default().push(s);
+    }
+    let mut res = vec![];
+    for (_, mut v) in hm {
+        v.sort_unstable();
+        res.push(v.to_vec());
+    }
+    res.sort_by(|a, b| a.len().cmp(&b.len()));
+    res
 }
 // hash_table string
 #[test]
-#[ignore]
 fn test1_49() {
     assert_eq!(
         group_anagrams(vec![
