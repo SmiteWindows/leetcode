344fb0f59e65d71619449d57f090e29ce98daab7
SmiteWindows <x1627283564@hotmail.com>
2020-05-30 15:16:56 +0800

    202005301516

--- src/greedy/leetcode881.rs
+++ src/greedy/leetcode881.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/boats-to-save-people/
+// Runtime: 20 ms
+// Memory Usage: 2.3 MB
 pub fn num_rescue_boats(people: Vec<i32>, limit: i32) -> i32 {
-    todo!()
+    let mut people = people;
+    let n = people.len();
+    people.sort();
+    people.reverse();
+    let mut i = 0;
+    let mut j = n - 1;
+    let mut res = 0;
+    while i <= j {
+        res += 1;
+        if people[i] + people[j] <= limit {
+            j -= 1;
+        }
+        i += 1;
+    }
+    res
 }
 // two_pointers greedy
 #[test]
-#[ignore]
 fn test2_881() {
     assert_eq!(num_rescue_boats(vec![1, 2], 3), 1);
     assert_eq!(num_rescue_boats(vec![3, 2, 2, 1], 3), 3);
