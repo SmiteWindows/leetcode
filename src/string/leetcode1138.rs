e5dd2742e7fe854596cd47c1054081071a0dd4dd
SmiteWindows <x1627283564@hotmail.com>
2020-05-30 15:36:17 +0800

    202005301536

--- src/string/leetcode1138.rs
+++ src/string/leetcode1138.rs
@@ -1,10 +1,47 @@
 // https://leetcode.com/problems/alphabet-board-path/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn alphabet_board_path(target: String) -> String {
-    todo!()
+    let mut pos: Vec<(i32, i32)> = vec![];
+    for i in 0..5 {
+        for j in 0..5 {
+            pos.push((i, j));
+        }
+    }
+    pos.push((5, 0));
+    let mut v = vec!['a'];
+    for c in target.chars() {
+        v.push(c);
+    }
+    let n = v.len();
+    let mut res = "".to_string();
+    for i in 1..n {
+        let curr = pos[(v[i] as u8 - b'a') as usize];
+        let prev = pos[(v[i - 1] as u8 - b'a') as usize];
+        let mut r = curr.0 - prev.0;
+        let mut c = curr.1 - prev.1;
+        while r < 0 {
+            res.push('U');
+            r += 1;
+        }
+        while c < 0 {
+            res.push('L');
+            c += 1;
+        }
+        while r > 0 {
+            res.push('D');
+            r -= 1;
+        }
+        while c > 0 {
+            res.push('R');
+            c -= 1;
+        }
+        res.push('!');
+    }
+    res
 }
 // hash_table string
 #[test]
-#[ignore]
 fn test2_1138() {
     assert_eq!(
         alphabet_board_path(String::from("leet")),
