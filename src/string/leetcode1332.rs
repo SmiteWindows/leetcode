accbbc4ed0a6a6119cf25509a61c40e0dcd37f9c
SmiteWindows <x1627283564@hotmail.com>
2020-06-01 21:46:57 +0800

    202006012146

--- src/string/leetcode1332.rs
+++ src/string/leetcode1332.rs
@@ -1,10 +1,34 @@
 // https://leetcode.com/problems/remove-palindromic-subsequences/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn remove_palindrome_sub(s: String) -> i32 {
-    todo!()
+    if s.is_empty() {
+        0
+    } else {
+        if is_palindrome(&s) {
+            1
+        } else {
+            2
+        }
+    }
+}
+
+fn is_palindrome(s: &str) -> bool {
+    let mut i = 0;
+    let n = s.len();
+    let mut j = n - 1;
+    let v = s.chars().collect::<Vec<_>>();
+    while i <= j {
+        if v[i] != v[j] {
+            return false;
+        }
+        i += 1;
+        j -= 1;
+    }
+    true
 }
 // string
 #[test]
-#[ignore]
 fn test1_1332() {
     assert_eq!(remove_palindrome_sub(String::from("ababa")), 1);
     assert_eq!(remove_palindrome_sub(String::from("abb")), 2);
