6ab6c66d28f0aa935e55f8411e77b065be74e76d
SmiteWindows <x1627283564@hotmail.com>
2020-06-01 19:15:46 +0800

    202006011915

--- src/array/leetcode1450.rs
+++ src/array/leetcode1450.rs
@@ -1,10 +1,20 @@
 // https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn busy_student(start_time: Vec<i32>, end_time: Vec<i32>, query_time: i32) -> i32 {
-    todo!()
+    let n = start_time.len();
+    let mut res = 0;
+    for i in 0..n {
+        let start = start_time[i];
+        let end = end_time[i];
+        if start <= query_time && end >= query_time {
+            res += 1;
+        }
+    }
+    res
 }
 // array
 #[test]
-#[ignore]
 fn test1_1450() {
     assert_eq!(busy_student(vec![1, 2, 3], vec![1, 2, 3], 4), 1);
     assert_eq!(busy_student(vec![4], vec![4], 4), 1);
