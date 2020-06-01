28b4acd31e9a5f296a622a499e802bd5099320a2
SmiteWindows <x1627283564@hotmail.com>
2020-06-02 07:56:51 +0800

    202006020756

--- src/math/leetcode1317.rs
+++ src/math/leetcode1317.rs
@@ -1,10 +1,37 @@
 // https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn get_no_zero_integers(n: i32) -> Vec<i32> {
-    todo!()
+    let mut v = vec![true; 10_000];
+    v[0] = false;
+    for i in 0..10 {
+        for j in 0..10 {
+            for k in 0..10 {
+                for l in 0..10 {
+                    let x = i * 1000 + j * 100 + k * 10 + l;
+                    if i != 0 && (j == 0 || k == 0 || l == 0) {
+                        v[x] = false;
+                    }
+                    if j != 0 && (k == 0 || l == 0) {
+                        v[x] = false;
+                    }
+                    if k != 0 && l == 0 {
+                        v[x] = false;
+                    }
+                }
+            }
+        }
+    }
+    for a in 1..n {
+        let b = n - a;
+        if v[a as usize] && v[b as usize] {
+            return vec![a, b];
+        }
+    }
+    vec![]
 }
 // math
 #[test]
-#[ignore]
 fn test1_1317() {
     assert_eq!(get_no_zero_integers(2), vec![1, 1]);
     assert_eq!(get_no_zero_integers(11), vec![2, 9]);
