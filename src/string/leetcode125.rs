7a9836af70765039ec73072508af51392b56e07f
SmiteWindows <x1627283564@hotmail.com>
2020-06-02 12:20:47 +0800

    202006021220

--- src/string/leetcode125.rs
+++ src/string/leetcode125.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/valid-palindrome/
+// Runtime: 0 ms
+// Memory Usage: 2.6 MB
 pub fn is_palindrome(s: String) -> bool {
-    todo!()
+    let s = s
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|c| c.to_ascii_lowercase())
+        .collect::<Vec<_>>();
+    let a = s.iter().collect::<String>();
+    let b = s.iter().rev().collect::<String>();
+    a == b
 }
 // string two_pointers
 #[test]
-#[ignore]
 fn test2_125() {
     assert_eq!(
         is_palindrome(String::from("A man, a plan, a canal: Panama")),
