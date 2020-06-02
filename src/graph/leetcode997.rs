d57fb6309d2ea6194e8a828a838306df0b3f2921
SmiteWindows <x1627283564@hotmail.com>
2020-06-02 10:03:10 +0800

    202006021003

--- src/graph/leetcode997.rs
+++ src/graph/leetcode997.rs
@@ -1,10 +1,24 @@
 // https://leetcode.com/problems/find-the-town-judge/
+// Runtime: 20 ms
+// Memory Usage: 2.7 MB
 pub fn find_judge(n: i32, trust: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let n = n as usize;
+    let mut degree = vec![0; n];
+    for edge in trust {
+        let u = (edge[0] - 1) as usize;
+        let v = (edge[1] - 1) as usize;
+        degree[v] += 1;
+        degree[u] -= 1;
+    }
+    for i in 0..n {
+        if degree[i] as usize == n - 1 {
+            return (i + 1) as i32;
+        }
+    }
+    -1
 }
 // graph
 #[test]
-#[ignore]
 fn test1_997() {
     assert_eq!(find_judge(2, vec![vec![1, 2]]), 2);
     assert_eq!(find_judge(3, vec![vec![1, 3], vec![2, 3]]), 3);
--- src/sort/leetcode976.rs
+++ src/sort/leetcode976.rs
@@ -1,10 +1,19 @@
 // https://leetcode.com/problems/largest-perimeter-triangle/
+// Runtime: 8 ms
+// Memory Usage: 2 MB
 pub fn largest_perimeter(a: Vec<i32>) -> i32 {
-    todo!()
+    let mut a = a;
+    let n = a.len();
+    a.sort_unstable();
+    for i in (0..=n - 3).rev() {
+        if a[i] + a[i + 1] > a[i + 2] {
+            return a[i] + a[i + 1] + a[i + 2];
+        }
+    }
+    0
 }
 // math sort
 #[test]
-#[ignore]
 fn test2_976() {
     assert_eq!(largest_perimeter(vec![2, 1, 2]), 5);
     assert_eq!(largest_perimeter(vec![1, 2, 1]), 0);
