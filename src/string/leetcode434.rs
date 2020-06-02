c40971abc90355dbce9e68a612a74f1ae1259291
SmiteWindows <x1627283564@hotmail.com>
2020-06-02 13:00:37 +0800

    202006021300

--- src/string/leetcode434.rs
+++ src/string/leetcode434.rs
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/number-of-segments-in-a-string/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn count_segments(s: String) -> i32 {
-    todo!()
+    s.split_whitespace().count() as i32
 }
 // string
 #[test]
-#[ignore]
 fn test1_434() {
     assert_eq!(count_segments(String::from("Hello, my name is John")), 5);
 }
