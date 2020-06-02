0ce573c6010e67dace47c32dd5d1cdaa6cfbce32
SmiteWindows <x1627283564@hotmail.com>
2020-06-02 11:13:23 +0800

    202006021113

--- src/two_pointers/leetcode925.rs
+++ src/two_pointers/leetcode925.rs
@@ -1,10 +1,38 @@
 // https://leetcode.com/problems/long-pressed-name/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn is_long_pressed_name(name: String, typed: String) -> bool {
-    todo!()
+    let n = name.len();
+    let m = typed.len();
+    let mut i = 0;
+    let mut j = 0;
+    let s = name.chars().collect::<Vec<_>>();
+    let t = typed.chars().collect::<Vec<_>>();
+    while i < n && j < m {
+        if s[i] == t[j] {
+            let mut a = 0;
+            while i + 1 < n && s[i + 1] == s[i] {
+                i += 1;
+                a += 1;
+            }
+            i += 1;
+            let mut b = 0;
+            while j + 1 < m && t[j + 1] == t[j] {
+                j += 1;
+                b += 1;
+            }
+            j += 1;
+            if a > b {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    i == n && j == m
 }
 // two_pointers string
 #[test]
-#[ignore]
 fn test1_925() {
     assert_eq!(
         is_long_pressed_name(String::from("alex"), String::from("aaleex")),
