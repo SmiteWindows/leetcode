539f727a936b5d33048cd510cdad4aab1efd70d6
SmiteWindows <x1627283564@hotmail.com>
2020-06-02 10:15:11 +0800

    202006021015

--- src/math/leetcode970.rs
+++ src/math/leetcode970.rs
@@ -1,10 +1,36 @@
 // https://leetcode.com/problems/powerful-integers/
+// Runtime: 4 ms
+// Memory Usage: 3 MB
 pub fn powerful_integers(x: i32, y: i32, bound: i32) -> Vec<i32> {
-    todo!()
+    let mut set = vec![false; bound as usize + 1];
+    let mut i = 0;
+    while x.pow(i) < bound {
+        let mut j = 0;
+        while y.pow(j) < bound {
+            let sum = x.pow(i) + y.pow(j);
+            if sum <= bound {
+                set[sum as usize] = true;
+            }
+            j += 1;
+            if y == 1 {
+                break;
+            }
+        }
+        i += 1;
+        if x == 1 {
+            break;
+        }
+    }
+    let mut res = vec![];
+    for i in 0..=bound {
+        if set[i as usize] {
+            res.push(i);
+        }
+    }
+    res
 }
 // math hash_table
 #[test]
-#[ignore]
 fn test1_970() {
     assert_eq!(powerful_integers(2, 3, 10), vec![2, 3, 4, 5, 7, 9, 10]);
     assert_eq!(powerful_integers(3, 5, 15), vec![2, 4, 6, 8, 10, 14]);
