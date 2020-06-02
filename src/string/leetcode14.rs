3b5b85557f7baf202f0f855dd98c7f7c2dcede64
SmiteWindows <x1627283564@hotmail.com>
2020-06-02 12:14:06 +0800

    202006021214

--- src/string/leetcode14.rs
+++ src/string/leetcode14.rs
@@ -1,10 +1,28 @@
 // https://leetcode.com/problems/longest-common-prefix/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn longest_common_prefix(strs: Vec<String>) -> String {
-    todo!()
+    if strs.is_empty() {
+        return String::from("");
+    }
+    let ss = strs
+        .iter()
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let n = ss.iter().map(|s| s.len()).min().unwrap();
+    let mut prefix = vec![];
+    for i in 0..n {
+        let c = ss[0][i];
+        if ss.iter().all(|s| s[i] == c) {
+            prefix.push(c);
+        } else {
+            break;
+        }
+    }
+    prefix.iter().collect()
 }
 // string
 #[test]
-#[ignore]
 fn test1_14() {
     assert_eq!(
         longest_common_prefix(vec![
