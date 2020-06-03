8ef6a78cb6cd7c71312ca7cb354284459bfc60a9
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 15:37:11 +0800

    202006031537

--- src/array/leetcode717.rs
+++ src/array/leetcode717.rs
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/1-bit-and-2-bit-characters/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn is_one_bit_character(bits: Vec<i32>) -> bool {
-    todo!()
+    bits.into_iter()
+        .rev()
+        .skip(1)
+        .take_while(|&x| x == 1)
+        .count()
+        % 2
+        == 0
 }
 // array
 #[test]
-#[ignore]
 fn test1_717() {
     assert_eq!(is_one_bit_character(vec![1, 0, 0]), true);
     assert_eq!(is_one_bit_character(vec![1, 1, 1, 0]), false);
