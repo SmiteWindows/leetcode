f3309f296359bb55e3851b40107a5974869bcfc3
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 22:34:49 +0800

    202006032234

--- src/array/leetcode661.rs
+++ src/array/leetcode661.rs
@@ -1,10 +1,59 @@
 // https://leetcode.com/problems/image-smoother/
+// Runtime: 16 ms
+// Memory Usage: 2.3 MB
 pub fn image_smoother(m: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+    let h = m.len();
+    let w = m[0].len();
+    let mut res = vec![vec![0; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            res[i][j] = smooth(&m, i, j, h, w);
+        }
+    }
+    res
+}
+
+fn smooth(m: &[Vec<i32>], r: usize, c: usize, h: usize, w: usize) -> i32 {
+    let mut sum = 0;
+    let mut n = 0;
+    if r > 0 && c > 0 {
+        sum += m[r - 1][c - 1];
+        n += 1;
+    }
+    if r > 0 {
+        sum += m[r - 1][c];
+        n += 1;
+    }
+    if r > 0 && c < w - 1 {
+        sum += m[r - 1][c + 1];
+        n += 1;
+    }
+    if c > 0 {
+        sum += m[r][c - 1];
+        n += 1;
+    }
+    sum += m[r][c];
+    n += 1;
+    if c < w - 1 {
+        sum += m[r][c + 1];
+        n += 1;
+    }
+    if r < h - 1 && c > 0 {
+        sum += m[r + 1][c - 1];
+        n += 1;
+    }
+    if r < h - 1 {
+        sum += m[r + 1][c];
+        n += 1;
+    }
+    if r < h - 1 && c < w - 1 {
+        sum += m[r + 1][c + 1];
+        n += 1;
+    }
+    sum / n
 }
 // array
 #[test]
-#[ignore]
 fn test1_661() {
     assert_eq!(
         image_smoother(vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]]),
