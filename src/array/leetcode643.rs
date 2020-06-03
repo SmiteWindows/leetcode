2ba0944188f4a647b6c93441a7aa69d67be57ffa
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 23:30:14 +0800

    202006032330

--- src/array/leetcode643.rs
+++ src/array/leetcode643.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/maximum-average-subarray-i/
+// Runtime: 16 ms
+// Memory Usage: 2.1 MB
 pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
-    todo!()
+    let n = nums.len();
+    let k = k as usize;
+    let mut sum = 0;
+    for i in 0..k {
+        sum += nums[i];
+    }
+    let mut max = sum;
+    for i in k..n {
+        sum += nums[i] - nums[i - k];
+        max = max.max(sum);
+    }
+    max as f64 / k as f64
 }
 // array
 #[test]
-#[ignore]
 fn test1_643() {
     assert_eq!(find_max_average(vec![1, 12, -5, -6, 50, 3], 4), 12.75);
 }
