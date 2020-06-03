1b77cbe5b20a1823af84e34da28030f7264d54d9
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 09:01:29 +0800

    202006030901

--- src/string/leetcode459.rs
+++ src/string/leetcode459.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/repeated-substring-pattern/
+// Runtime: 4 ms
+// Memory Usage: 2.1 MB
 pub fn repeated_substring_pattern(s: String) -> bool {
-    todo!()
+    let mut t = "".to_string();
+    let n = s.len();
+    if n < 2 {
+        return false;
+    }
+    t += &s;
+    t += &s;
+    t[1..2 * n - 1].find(&s).is_some()
 }
 // string
 #[test]
-#[ignore]
 fn test1_459() {
     assert_eq!(repeated_substring_pattern(String::from("abab")), true);
     assert_eq!(repeated_substring_pattern(String::from("aba")), false);
--- src/string/leetcode521.rs
+++ src/string/leetcode521.rs
@@ -1,10 +1,15 @@
 // https://leetcode.com/problems/longest-uncommon-subsequence-i/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn find_lu_slength(a: String, b: String) -> i32 {
-    todo!()
+    if a == b {
+        -1
+    } else {
+        a.len().max(b.len()) as i32
+    }
 }
 // string
 #[test]
-#[ignore]
 fn test1_521() {
     assert_eq!(find_lu_slength(String::from("aba"), String::from("cdc")), 3);
     assert_eq!(find_lu_slength(String::from("aaa"), String::from("bbb")), 3);
