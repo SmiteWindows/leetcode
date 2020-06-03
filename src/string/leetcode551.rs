2a7cb9656ea51ccef2a8a5794fef0c2574270318
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 10:02:15 +0800

    202006031002

--- src/string/leetcode551.rs
+++ src/string/leetcode551.rs
@@ -1,10 +1,18 @@
 // https://leetcode.com/problems/student-attendance-record-i/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn check_record(s: String) -> bool {
-    todo!()
+    let s = s.chars().collect::<Vec<_>>();
+    if s.iter().filter(|&c| c == &'A').count() > 1 {
+        return false;
+    }
+    if s.windows(3).filter(|&w| w == ['L', 'L', 'L']).count() > 0 {
+        return false;
+    }
+    true
 }
 // string
 #[test]
-#[ignore]
 fn test1_551() {
     assert_eq!(check_record(String::from("PPALLP")), true);
     assert_eq!(check_record(String::from("PPALLL")), false);
--- src/string/leetcode788.rs
+++ src/string/leetcode788.rs
@@ -1,10 +1,44 @@
 // https://leetcode.com/problems/rotated-digits/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn rotated_digits(n: i32) -> i32 {
-    todo!()
+    let n = n as usize;
+    let mut a = vec![D::Invalid; (n + 1) as usize];
+    for i in 0..=n {
+        if i < 10 {
+            a[i] = D::new(i);
+        } else {
+            let j = i / 10;
+            let k = i % 10;
+            a[i] = match (a[j], a[k]) {
+                (D::Invalid, _) | (_, D::Invalid) => D::Invalid,
+                (D::Different, _) | (_, D::Different) => D::Different,
+                (D::Same, D::Same) => D::Same,
+            }
+        }
+    }
+    a.iter()
+        .fold(0, |sum, &d| if d == D::Different { sum + 1 } else { sum })
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum D {
+    Invalid,
+    Same,
+    Different,
+}
+
+impl D {
+    fn new(d: usize) -> Self {
+        match d {
+            0 | 1 | 8 => D::Same,
+            2 | 5 | 6 | 9 => D::Different,
+            _ => D::Invalid,
+        }
+    }
 }
 // string
 #[test]
-#[ignore]
 fn test1_788() {
     assert_eq!(rotated_digits(10), 4);
 }
