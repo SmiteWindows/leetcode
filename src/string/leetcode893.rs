39786489b75db77c28854a7aa4cf4e89b239eec9
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 10:47:30 +0800

    202006031047

--- src/string/leetcode893.rs
+++ src/string/leetcode893.rs
@@ -1,10 +1,37 @@
 // https://leetcode.com/problems/groups-of-special-equivalent-strings/
+// Runtime: 4 ms
+// Memory Usage: 2.4 MB
+use std::collections::{BTreeMap, HashSet};
 pub fn num_special_equiv_groups(a: Vec<String>) -> i32 {
-    todo!()
+    let mut hs = HashSet::new();
+    for s in a {
+        hs.insert(Count::new(s));
+    }
+    hs.len() as i32
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+struct Count {
+    even: BTreeMap<char, usize>,
+    odd: BTreeMap<char, usize>,
+}
+
+impl Count {
+    fn new(s: String) -> Self {
+        let mut even: BTreeMap<char, usize> = BTreeMap::new();
+        let mut odd: BTreeMap<char, usize> = BTreeMap::new();
+        for (i, c) in s.char_indices() {
+            if i % 2 == 0 {
+                *even.entry(c).or_default() += 1;
+            } else {
+                *odd.entry(c).or_default() += 1;
+            }
+        }
+        Self { even, odd }
+    }
 }
 // string
 #[test]
-#[ignore]
 fn test1_893() {
     assert_eq!(
         num_special_equiv_groups(vec![
