3d95529d70d8363a69d2b3e2bb2100bbe81afd0e
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 00:03:07 +0800

    202006040003

--- src/array/leetcode566.rs
+++ src/array/leetcode566.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/reshape-the-matrix/
+// Runtime: 4 ms
+// Memory Usage: 2.3 MB
 pub fn matrix_reshape(nums: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
-    todo!()
+    let n = nums.len();
+    let m = nums[0].len();
+    let r = r as usize;
+    let c = c as usize;
+    if n * m != r * c {
+        return nums;
+    }
+    let mut res = vec![vec![0; c]; r];
+    for i in 0..n {
+        for j in 0..m {
+            let k = i * m + j;
+            res[k / c][k % c] = nums[i][j];
+        }
+    }
+    res
 }
 // array
 #[test]
-#[ignore]
 fn test1_566() {
     assert_eq!(
         matrix_reshape(vec![vec![1, 2], vec![3, 4]], 1, 4),
