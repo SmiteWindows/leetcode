c33059ae3f9ad5aa7126399b3dc4a1f560645627
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 10:27:03 +0800

    202006031027

--- src/string/leetcode819.rs
+++ src/string/leetcode819.rs
@@ -1,10 +1,39 @@
 // https://leetcode.com/problems/most-common-word/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::{HashMap, HashSet};
 pub fn most_common_word(paragraph: String, banned: Vec<String>) -> String {
-    todo!()
+    let p = paragraph
+        .chars()
+        .map(|c| {
+            if c.is_alphabetic() {
+                c.to_ascii_lowercase()
+            } else {
+                ' '
+            }
+        })
+        .collect::<String>();
+    let mut hm: HashMap<String, usize> = HashMap::new();
+    for word in p.split_whitespace() {
+        *hm.entry(word.to_string()).or_default() += 1;
+    }
+    let mut max = 0;
+    let mut res = "".to_string();
+    let banned = banned.into_iter().collect::<HashSet<_>>();
+    for word in p.split_whitespace() {
+        if !banned.contains(word) {
+            if let Some(&count) = hm.get(word) {
+                if count > max {
+                    max = count;
+                    res = word.to_string();
+                }
+            }
+        }
+    }
+    res
 }
 // string
 #[test]
-#[ignore]
 fn test1_819() {
     assert_eq!(
         most_common_word(
