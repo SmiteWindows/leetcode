d2b0149f59b8f3e0c6e3ed9e82c85e0b3f9be538
SmiteWindows <x1627283564@hotmail.com>
2020-06-03 15:49:01 +0800

    202006031548

--- src/array/leetcode674.rs
+++ src/array/leetcode674.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn find_length_of_lcis(nums: Vec<i32>) -> i32 {
-    todo!()
+    let n = nums.len();
+    if n == 0 {
+        return 0;
+    }
+    let mut i: usize = 0;
+    let mut j: usize = i;
+    let mut res: usize = 1;
+    while i < n {
+        while j + 1 < n && nums[j + 1] > nums[j] {
+            j += 1;
+        }
+        res = res.max(j - i + 1);
+        i = j + 1;
+        j = i;
+    }
+    res as i32
 }
 // array
 #[test]
-#[ignore]
 fn test1_674() {
     assert_eq!(find_length_of_lcis(vec![1, 3, 5, 4, 7]), 3);
     assert_eq!(find_length_of_lcis(vec![2, 2, 2, 2, 2]), 1);
