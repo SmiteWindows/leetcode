f39b8a741ee72b89b29e869de2c8bedeb7f33187
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 09:04:48 +0800

    202006040904

--- src/other/leetcode821.rs
+++ src/other/leetcode821.rs
@@ -1,9 +1,31 @@
 // https://leetcode.com/problems/shortest-distance-to-a-character/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn shortest_to_char(s: String, c: char) -> Vec<i32> {
-    todo!()
+    let s = s.chars().collect::<Vec<_>>();
+    let mut prev = None;
+    let n = s.len();
+    let mut res = vec![n as i32; n];
+    for i in 0..n {
+        if s[i] == c {
+            prev = Some(i);
+        }
+        if let Some(j) = prev {
+            res[i] = res[i].min((i - j) as i32);
+        }
+    }
+    prev = None;
+    for i in (0..n).rev() {
+        if s[i] == c {
+            prev = Some(i);
+        }
+        if let Some(j) = prev {
+            res[i] = res[i].min((j - i) as i32);
+        }
+    }
+    res
 }
 #[test]
-#[ignore]
 fn test821() {
     assert_eq!(
         shortest_to_char("loveleetcode".to_string(), 'e'),
