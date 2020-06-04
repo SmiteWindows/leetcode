b88cf6a52eef61dd881b830855d09b5c1e145d64
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 17:38:22 +0800

    202006041738

--- src/bit_manipulation/leetcode693.rs
+++ src/bit_manipulation/leetcode693.rs
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/binary-number-with-alternating-bits/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn has_alternating_bits(n: i32) -> bool {
-    todo!()
+    let x = (n >> 1) ^ n;
+    (x + 1) & x == 0
 }
 // bit_manipulation
 #[test]
-#[ignore]
 fn test1_693() {
     assert_eq!(has_alternating_bits(5), true);
     assert_eq!(has_alternating_bits(7), false);
