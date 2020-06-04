ad54491b8777ac934501147ec2dc949d3a458194
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 20:09:57 +0800

    202006042009

--- src/design/leetcode706.rs
+++ src/design/leetcode706.rs
@@ -1,13 +1,9 @@
 // https://leetcode.com/problems/design-hashmap/
-struct MyHashMap {}
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * let obj = MyHashMap::new();
- * obj.put(key, value);
- * let ret_2: i32 = obj.get(key);
- * obj.remove(key);
- */
+// Runtime: 24 ms
+// Memory Usage: 8.6 MB
+struct MyHashMap {
+    table: Vec<i32>,
+}
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
@@ -15,21 +11,43 @@ struct MyHashMap {}
 impl MyHashMap {
     /** Initialize your data structure here. */
     fn new() -> Self {
-        todo!()
+        Self {
+            table: vec![-1; 1_000_000],
+        }
     }
 
     /** value will always be non-negative. */
-    fn put(&self, key: i32, value: i32) {
-        todo!()
+    fn put(&mut self, key: i32, value: i32) {
+        self.table[key as usize] = value;
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     fn get(&self, key: i32) -> i32 {
-        todo!()
+        self.table[key as usize]
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    fn remove(&self, key: i32) {
-        todo!()
+    fn remove(&mut self, key: i32) {
+        self.table[key as usize] = -1;
     }
+}
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * let obj = MyHashMap::new();
+ * obj.put(key, value);
+ * let ret_2: i32 = obj.get(key);
+ * obj.remove(key);
+ */
+// design hash_table
+#[test]
+fn test2_706() {
+    let mut hm = MyHashMap::new();
+    hm.put(1, 1);
+    hm.put(2, 2);
+    assert_eq!(hm.get(1), 1);
+    assert_eq!(hm.get(3), -1);
+    hm.put(2, 1);
+    assert_eq!(hm.get(2), 1);
+    hm.remove(2);
+    assert_eq!(hm.get(2), -1);
 }
--- src/hash_table/leetcode705.rs
+++ src/hash_table/leetcode705.rs
@@ -1,13 +1,9 @@
 // https://leetcode.com/problems/design-hashset/
-struct MyHashSet {}
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * let obj = MyHashSet::new();
- * obj.add(key);
- * obj.remove(key);
- * let ret_3: bool = obj.contains(key);
- */
+// Runtime: 20 ms
+// Memory Usage: 5.7 MB
+struct MyHashSet {
+    table: Vec<bool>,
+}
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
@@ -15,19 +11,41 @@ struct MyHashSet {}
 impl MyHashSet {
     /** Initialize your data structure here. */
     fn new() -> Self {
-        todo!()
+        Self {
+            table: vec![false; 1_000_000],
+        }
     }
 
-    fn add(&self, key: i32) {
-        todo!()
+    fn add(&mut self, key: i32) {
+        self.table[key as usize] = true;
     }
 
-    fn remove(&self, key: i32) {
-        todo!()
+    fn remove(&mut self, key: i32) {
+        self.table[key as usize] = false;
     }
 
     /** Returns true if this set contains the specified element */
     fn contains(&self, key: i32) -> bool {
-        todo!()
+        self.table[key as usize]
     }
+}
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * let obj = MyHashSet::new();
+ * obj.add(key);
+ * obj.remove(key);
+ * let ret_3: bool = obj.contains(key);
+ */
+// design hash_table
+#[test]
+fn test1_705() {
+    let mut hs = MyHashSet::new();
+    hs.add(1);
+    hs.add(2);
+    assert_eq!(hs.contains(1), true);
+    assert_eq!(hs.contains(3), false);
+    hs.add(2);
+    assert_eq!(hs.contains(2), true);
+    hs.remove(2);
+    assert_eq!(hs.contains(2), false);
 }
