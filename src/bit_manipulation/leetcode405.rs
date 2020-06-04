0225c1290e1b40014ffcd8b691eb958fbd8b191d
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 17:42:47 +0800

    202006041742

--- src/bit_manipulation/leetcode405.rs
+++ src/bit_manipulation/leetcode405.rs
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn to_hex(num: i32) -> String {
-    todo!()
+    format!("{:x}", num)
 }
 // bit_manipulation
 #[test]
-#[ignore]
 fn test1_405() {
     assert_eq!(to_hex(26), String::from("1a"));
     assert_eq!(to_hex(-1), String::from("ffffffff"));
