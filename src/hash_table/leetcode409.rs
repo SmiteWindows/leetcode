cbceffec748397df2efd721417e55e46d21bf688
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 17:29:07 +0800

    202006041729

--- src/hash_table/leetcode409.rs
+++ src/hash_table/leetcode409.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/longest-palindrome/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashSet;
 pub fn longest_palindrome(s: String) -> i32 {
-    todo!()
+    let mut hs = HashSet::new();
+    let mut half = 0;
+    for c in s.chars() {
+        if hs.contains(&c) {
+            hs.remove(&c);
+            half += 1;
+        } else {
+            hs.insert(c);
+        }
+    }
+    if hs.is_empty() {
+        2 * half
+    } else {
+        2 * half + 1
+    }
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_409() {
     assert_eq!(longest_palindrome(String::from("abccccdd")), 7);
 }
