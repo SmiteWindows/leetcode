fa51d320ab7d7fae33cebaafc7ff0fc0ec27abf5
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 08:52:13 +0800

    202006040852

--- src/other/leetcode492.rs
+++ src/other/leetcode492.rs
@@ -1,9 +1,14 @@
 // https://leetcode.com/problems/construct-the-rectangle/
+// Runtime: 0 ms
+// Memory Usage: 2.3 MB
 pub fn construct_rectangle(area: i32) -> Vec<i32> {
-    todo!()
+    let mut max = (area as f64).sqrt().floor() as i32;
+    while area % max != 0 {
+        max -= 1;
+    }
+    vec![area / max, max]
 }
 #[test]
-#[ignore]
 fn test492() {
     assert_eq!(construct_rectangle(4), vec![2, 2]);
 }
