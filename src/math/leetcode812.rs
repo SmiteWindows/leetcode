18c0613cd9fe65646f7b3231fe82d6821ccd5d6d
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 11:01:10 +0800

    202006041101

--- src/math/leetcode812.rs
+++ src/math/leetcode812.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/largest-triangle-area/
+// Runtime: 4 ms
+// Memory Usage: 2.1 MB
 pub fn largest_triangle_area(points: Vec<Vec<i32>>) -> f64 {
-    todo!()
+    let mut res: f64 = 0_f64;
+    for i in &points {
+        for j in &points {
+            for k in &points {
+                let area = (i[0] * j[1] + j[0] * k[1] + k[0] * i[1]
+                    - j[0] * i[1]
+                    - k[0] * j[1]
+                    - i[0] * k[1])
+                    .abs();
+                let area = area as f64 / 2_f64;
+                res = res.max(area);
+            }
+        }
+    }
+    res
 }
 // math
 #[test]
-#[ignore]
 fn test1_812() {
     assert_eq!(
         largest_triangle_area(vec![
