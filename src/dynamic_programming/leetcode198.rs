1ae2389d2a494824bb0f489aebe695e86053ea3a
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 18:49:07 +0800

    202006041849

--- src/dynamic_programming/leetcode198.rs
+++ src/dynamic_programming/leetcode198.rs
@@ -1,10 +1,15 @@
 // https://leetcode.com/problems/house-robber/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn rob(nums: Vec<i32>) -> i32 {
-    todo!()
+    nums.into_iter()
+        .fold((0, 0), |(prev_prev, prev), x| {
+            (prev, prev.max(prev_prev + x))
+        })
+        .1
 }
 // dynamic_programming
 #[test]
-#[ignore]
 fn test1_198() {
     assert_eq!(rob(vec![1, 2, 3, 1]), 4);
     assert_eq!(rob(vec![2, 7, 9, 3, 1]), 12);
