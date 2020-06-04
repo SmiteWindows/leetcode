e44d2b4a4e0234f87932e13133981c962e8af53e
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 19:40:40 +0800

    202006041940

--- src/greedy/leetcode455.rs
+++ src/greedy/leetcode455.rs
@@ -1,10 +1,27 @@
 // https://leetcode.com/problems/assign-cookies/
+// Runtime: 4 ms
+// Memory Usage: 2.3 MB
 pub fn find_content_children(g: Vec<i32>, s: Vec<i32>) -> i32 {
-    todo!()
+    let mut g = g;
+    let mut s = s;
+    g.sort_unstable();
+    s.sort_unstable();
+    let mut i = 0;
+    let mut j = 0;
+    while i < g.len() && j < s.len() {
+        if g[i] <= s[j] {
+            i += 1;
+            j += 1;
+        } else {
+            while j < s.len() && s[j] < g[i] {
+                j += 1;
+            }
+        }
+    }
+    i as i32
 }
 // greedy
 #[test]
-#[ignore]
 fn test1_455() {
     assert_eq!(find_content_children(vec![1, 2, 3], vec![1, 1]), 1);
     assert_eq!(find_content_children(vec![1, 2], vec![1, 2, 3]), 2);
