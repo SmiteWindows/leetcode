954fb299db3c0acb233b1a1543f4adc591684139
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 09:16:37 +0800

    202006040916

--- src/math/leetcode326.rs
+++ src/math/leetcode326.rs
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/power-of-three/
+// Runtime: 0 ms
+// Memory Usage: 2.8 MB
 pub fn is_power_of_three(n: i32) -> bool {
-    todo!()
+    let n = n as f64;
+    n.log10() / 3.0_f64.log10() % 1.0 == 0.0
 }
 // math
 #[test]
-#[ignore]
 fn test1_326() {
     assert_eq!(is_power_of_three(27), true);
     assert_eq!(is_power_of_three(0), false);
