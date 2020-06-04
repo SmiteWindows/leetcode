ae507e5ab482f724a227ccb94d5ee9fe65749620
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 13:36:29 +0800

    202006041336

--- src/hash_table/leetcode575.rs
+++ src/hash_table/leetcode575.rs
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/distribute-candies/
+// Runtime: 44 ms
+// Memory Usage: 2.1 MB
 pub fn distribute_candies(candies: Vec<i32>) -> i32 {
-    todo!()
+    use std::collections::HashSet;
+    let n = candies.len();
+    let mut hs = HashSet::new();
+    for val in candies {
+        hs.insert(val);
+    }
+    hs.len().min(n / 2) as i32
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_575() {
     assert_eq!(distribute_candies(vec![1, 1, 2, 2, 3, 3]), 3);
     assert_eq!(distribute_candies(vec![1, 1, 2, 3]), 2);
--- src/hash_table/leetcode594.rs
+++ src/hash_table/leetcode594.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/longest-harmonious-subsequence/
+// Runtime: 12 ms
+// Memory Usage: 2.3 MB
 pub fn find_lhs(nums: Vec<i32>) -> i32 {
-    todo!()
+    use std::collections::HashMap;
+    let mut hs: HashMap<i32, i32> = HashMap::new();
+    let mut max = 0;
+    for &x in &nums {
+        *hs.entry(x).or_default() += 1;
+    }
+    for (x, u) in &hs {
+        if let Some(v) = hs.get(&(x - 1)) {
+            max = max.max(u + v);
+        }
+    }
+    max
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_594() {
     assert_eq!(find_lhs(vec![1, 3, 2, 2, 5, 2, 3, 7]), 5);
 }
