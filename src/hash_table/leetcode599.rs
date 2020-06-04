b5869c59114284d1d2760f0db5112e585bc35877
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 13:55:54 +0800

    202006041355

--- src/hash_table/leetcode599.rs
+++ src/hash_table/leetcode599.rs
@@ -1,10 +1,30 @@
 // https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+// Runtime: 12 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashMap;
 pub fn find_restaurant(list1: Vec<String>, list2: Vec<String>) -> Vec<String> {
-    todo!()
+    let mut hm = HashMap::new();
+    let mut min = usize::MAX;
+    let mut res = vec![];
+    for (i, l1) in list1.iter().enumerate() {
+        hm.insert(l1, i);
+    }
+    for (j, l2) in list2.iter().enumerate() {
+        if let Some(i) = hm.get(l2) {
+            let sum = i + j;
+            if sum < min {
+                min = sum;
+                res.clear();
+            }
+            if sum == min {
+                res.push(l2.to_string());
+            }
+        }
+    }
+    res
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_599() {
     assert_eq!(
         find_restaurant(
