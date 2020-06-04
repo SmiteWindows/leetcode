efdc95cc8c907e8dd7d193e4aedf9e5c56b1d73e
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 11:14:34 +0800

    202006041114

--- src/math/leetcode868.rs
+++ src/math/leetcode868.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/binary-gap/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn binary_gap(n: i32) -> i32 {
-    todo!()
+    let mut max = 0;
+    let mut prev = None;
+    for i in 0..32 {
+        let bit = 1 << i;
+        if n & bit != 0 {
+            if let Some(j) = prev {
+                max = max.max(i - j);
+            }
+            prev = Some(i);
+        }
+    }
+    max
 }
 // math
 #[test]
-#[ignore]
 fn test1_868() {
     assert_eq!(binary_gap(22), 2);
     assert_eq!(binary_gap(5), 2);
