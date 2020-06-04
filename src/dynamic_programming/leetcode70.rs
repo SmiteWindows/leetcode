78fc98c6dbd45e2f3141a8c86358c0548a27133f
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 18:32:23 +0800

    202006041832

--- src/dynamic_programming/leetcode70.rs
+++ src/dynamic_programming/leetcode70.rs
@@ -1,10 +1,14 @@
 // https://leetcode.com/problems/climbing-stairs/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn climb_stairs(n: i32) -> i32 {
-    todo!()
+    match n {
+        1 | 2 => n,
+        k => (2..k).fold((1, 2), |(a, b), _| (b, a + b)).1,
+    }
 }
 // dynamic_programming
 #[test]
-#[ignore]
 fn test1_70() {
     assert_eq!(climb_stairs(2), 2);
     assert_eq!(climb_stairs(3), 3);
