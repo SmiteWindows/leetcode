d916136d2487859717b8599c7266aa5f10d4c277
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 12:19:13 +0800

    202006041219

--- src/math/leetcode883.rs
+++ src/math/leetcode883.rs
@@ -1,14 +1,31 @@
 // https://leetcode.com/problems/projection-area-of-3d-shapes/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn projection_area(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let n = grid.len();
+    let m = grid[0].len();
+    let mut x = vec![0; n];
+    let mut y = vec![0; m];
+    let mut sum_z = 0;
+    for i in 0..n {
+        for j in 0..m {
+            if grid[i][j] != 0 {
+                sum_z += 1;
+            }
+            x[i] = x[i].max(grid[i][j]);
+            y[j] = y[j].max(grid[i][j]);
+        }
+    }
+    let sum_x = x.iter().sum::<i32>();
+    let sum_y = y.iter().sum::<i32>();
+    sum_x + sum_y + sum_z
 }
 // math
 #[test]
-#[ignore]
 fn test1_883() {
     assert_eq!(projection_area(vec![vec![2]]), 5);
-    assert_eq!(projection_area(vec![vec![1, 2]]), 17);
-    assert_eq!(projection_area(vec![vec![1, 0]]), 8);
+    assert_eq!(projection_area(vec![vec![1, 2], vec![3, 4]]), 17);
+    assert_eq!(projection_area(vec![vec![1, 0], vec![0, 2]]), 8);
     assert_eq!(
         projection_area(vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]]),
         14
