969091c43a7d6d9eed364ca1968527b2511fba9e
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 18:14:16 +0800

    202006041814

--- src/stack/leetcode844.rs
+++ src/stack/leetcode844.rs
@@ -1,10 +1,23 @@
 // https://leetcode.com/problems/backspace-string-compare/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn backspace_compare(s: String, t: String) -> bool {
-    todo!()
+    get_backspaced(s) == get_backspaced(t)
+}
+
+fn get_backspaced(s: String) -> Vec<char> {
+    let mut chars = Vec::with_capacity(s.len());
+    for c in s.chars() {
+        if c == '#' {
+            chars.pop();
+        } else {
+            chars.push(c);
+        }
+    }
+    chars
 }
 // two_pointers stack
 #[test]
-#[ignore]
 fn test2_844() {
     assert_eq!(
         backspace_compare(String::from("ab#c"), String::from("ad#c")),
--- src/two_pointers/leetcode844.rs
+++ src/two_pointers/leetcode844.rs
@@ -1,10 +1,39 @@
 // https://leetcode.com/problems/backspace-string-compare/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn backspace_compare(s: String, t: String) -> bool {
-    todo!()
+    let mut s_i = s.chars().rev();
+    let mut t_i = t.chars().rev();
+    loop {
+        let s_char = get_next(&mut s_i);
+        let t_char = get_next(&mut t_i);
+        if s_char == None && t_char == None {
+            break true;
+        }
+        if s_char != t_char {
+            break false;
+        }
+    }
+}
+
+fn get_next(iter: &mut impl Iterator<Item = char>) -> Option<char> {
+    let mut skip = 0;
+    loop {
+        let cur = iter.next();
+        match cur {
+            Some('#') => skip += 1,
+            None => break None,
+            Some(x) => {
+                if skip == 0 {
+                    break Some(x);
+                }
+                skip -= 1;
+            }
+        }
+    }
 }
 // two_pointers stack
 #[test]
-#[ignore]
 fn test1_844() {
     assert_eq!(
         backspace_compare(String::from("ab#c"), String::from("ad#c")),
