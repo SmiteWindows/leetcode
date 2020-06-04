a432d70d8d2622cec97926d5c62c5df255731a3b
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 08:39:23 +0800

    202006040839

--- src/other/leetcode412.rs
+++ src/other/leetcode412.rs
@@ -1,9 +1,22 @@
 // https://leetcode.com/problems/fizz-buzz/
+// Runtime: 0 ms
+// Memory Usage: 2.7 MB
 pub fn fizz_buzz(n: i32) -> Vec<String> {
-    todo!()
+    let mut res = vec![];
+    for i in 1..=n {
+        let fizz = i % 3 == 0;
+        let buzz = i % 5 == 0;
+        let s = match (fizz, buzz) {
+            (true, true) => "FizzBuzz".to_string(),
+            (true, false) => "Fizz".to_string(),
+            (false, true) => "Buzz".to_string(),
+            (false, false) => format!("{}", i),
+        };
+        res.push(s);
+    }
+    res
 }
 #[test]
-#[ignore]
 fn test412() {
     assert_eq!(
         fizz_buzz(15),
