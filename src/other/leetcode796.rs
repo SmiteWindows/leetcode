7a20ec92eae6fe788a8b6ee58a35803d4e1119dd
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 08:58:27 +0800

    202006040858

--- src/other/leetcode796.rs
+++ src/other/leetcode796.rs
@@ -1,9 +1,16 @@
 // https://leetcode.com/problems/rotate-string/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn rotate_string(a: String, b: String) -> bool {
-    todo!()
+    if a.len() != b.len() {
+        return false;
+    }
+    let mut c = "".to_string();
+    c += &a;
+    c += &a;
+    c.contains(&b)
 }
 #[test]
-#[ignore]
 fn test796() {
     assert_eq!(
         rotate_string("abcde".to_string(), "cdeab".to_string()),
