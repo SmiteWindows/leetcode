2b3317c3a19e449d709aa0166c4b216e3c17600f
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 09:33:33 +0800

    202006040933

--- src/math/leetcode453.rs
+++ src/math/leetcode453.rs
@@ -1,10 +1,16 @@
 // https://leetcode.com/problems/minimum-moves-to-equal-array-elements/
+// Runtime: 4 ms
+// Memory Usage: 2.2 MB
 pub fn min_moves(nums: Vec<i32>) -> i32 {
-    todo!()
+    let min = nums.iter().min().expect("exist");
+    let mut sum = 0;
+    for &x in &nums {
+        sum += x - min;
+    }
+    sum
 }
 // math
 #[test]
-#[ignore]
 fn test1_453() {
     assert_eq!(min_moves(vec![1, 2, 3]), 3);
 }
--- src/math/leetcode598.rs
+++ src/math/leetcode598.rs
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/range-addition-ii/
+// Runtime: 0 ms
+// Memory Usage: 2.6 MB
 pub fn max_count(m: i32, n: i32, ops: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let mut m = m;
+    let mut n = n;
+    for op in ops {
+        m = m.min(op[0]);
+        n = n.min(op[1]);
+    }
+    m * n
 }
 // math
 #[test]
-#[ignore]
 fn test1_598() {
     assert_eq!(max_count(3, 3, vec![vec![2, 2], vec![3, 3]]), 4);
 }
