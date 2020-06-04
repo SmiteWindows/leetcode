4fe4ecf638273589cd503e1de0a7a29f788ec1ed
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 10:52:43 +0800

    202006041052

--- src/hash_table/leetcode645.rs
+++ src/hash_table/leetcode645.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/set-mismatch/
+// Runtime: 12 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashSet;
 pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let n = nums.len();
+    let mut hs = HashSet::new();
+    let mut res = vec![];
+    for x in nums {
+        if !hs.insert(x) {
+            res.push(x);
+        }
+    }
+    for i in 1..=n {
+        if hs.insert(i as i32) {
+            res.push(i as i32);
+        }
+    }
+    res
 }
 // math hash_table
 #[test]
-#[ignore]
 fn test2_645() {
     assert_eq!(find_error_nums(vec![1, 2, 2, 4]), vec![2, 3]);
 }
