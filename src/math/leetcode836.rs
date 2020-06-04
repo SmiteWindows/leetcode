684e0ce050877d5689db980424985adbf3fa53b0
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 11:06:01 +0800

    202006041105

--- src/math/leetcode836.rs
+++ src/math/leetcode836.rs
@@ -1,10 +1,11 @@
 // https://leetcode.com/problems/rectangle-overlap/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn is_rectangle_overlap(rec1: Vec<i32>, rec2: Vec<i32>) -> bool {
-    todo!()
+    rec1[2] > rec2[0] && rec1[0] < rec2[2] && rec1[1] < rec2[3] && rec1[3] > rec2[1]
 }
 // math
 #[test]
-#[ignore]
 fn test1_836() {
     assert_eq!(
         is_rectangle_overlap(vec![0, 0, 2, 2], vec![1, 1, 3, 3]),
