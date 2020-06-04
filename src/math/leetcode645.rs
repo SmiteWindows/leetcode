55ba11603a39607fa85d8901ad695bc137723d68
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 20:20:38 +0800

    202006042020

--- src/math/leetcode645.rs
+++ src/math/leetcode645.rs
@@ -1,10 +1,24 @@
 // https://leetcode.com/problems/set-mismatch/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut arr = vec![0; nums.len() + 1];
+    let mut dup = 0;
+    let mut missing = 1;
+    for &num in &nums {
+        arr[num as usize] += 1;
+    }
+    for (i, &a) in arr.iter().enumerate() {
+        if a == 0 {
+            missing = i;
+        } else if a == 2 {
+            dup = i;
+        }
+    }
+    vec![dup as i32, missing as i32]
 }
 // math hash_table
 #[test]
-#[ignore]
 fn test1_645() {
     assert_eq!(find_error_nums(vec![1, 2, 2, 4]), vec![2, 3]);
 }
