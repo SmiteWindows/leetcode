f38f445fee37045654953a3e35114671c5a60600
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 16:36:05 +0800

    202006041636

--- src/hash_table/leetcode205.rs
+++ src/hash_table/leetcode205.rs
@@ -1,10 +1,32 @@
 // https://leetcode.com/problems/isomorphic-strings/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashMap;
 pub fn is_isomorphic(s: String, t: String) -> bool {
-    todo!()
+    let mut hmst = HashMap::new();
+    let mut hmts = HashMap::new();
+    let mut is = s.chars();
+    let mut it = t.chars();
+    while let (Some(cs), Some(ct)) = (is.next(), it.next()) {
+        if let Some(&vt) = hmst.get(&cs) {
+            if vt != ct {
+                return false;
+            }
+        } else {
+            hmst.insert(cs, ct);
+        }
+        if let Some(&vs) = hmts.get(&ct) {
+            if vs != cs {
+                return false;
+            }
+        } else {
+            hmts.insert(ct, cs);
+        }
+    }
+    true
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_205() {
     assert_eq!(
         is_isomorphic(String::from("egg"), String::from("add")),
