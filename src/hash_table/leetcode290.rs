93c3c736f09a934ebad6614ca729ebd66cacaf80
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 16:52:13 +0800

    202006041652

--- src/hash_table/leetcode290.rs
+++ src/hash_table/leetcode290.rs
@@ -1,10 +1,39 @@
 // https://leetcode.com/problems/word-pattern/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashMap;
 pub fn word_pattern(pattern: String, str: String) -> bool {
-    todo!()
+    let chars = pattern.chars().collect::<Vec<_>>();
+    let strings = str
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>();
+    if chars.len() != strings.len() {
+        return false;
+    }
+    let mut hm1 = HashMap::new();
+    let mut hm2 = HashMap::new();
+    for (i, &ch) in chars.iter().enumerate() {
+        let string = strings[i].clone();
+        if let Some(s) = hm1.get(&ch) {
+            if *s != string {
+                return false;
+            }
+        } else {
+            hm1.insert(ch, string.clone());
+        }
+        if let Some(c) = hm2.get(&string) {
+            if *c != ch {
+                return false;
+            }
+        } else {
+            hm2.insert(string.clone(), ch);
+        }
+    }
+    true
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_290() {
     assert_eq!(
         word_pattern(String::from("abba"), String::from("dog cat cat dog")),
