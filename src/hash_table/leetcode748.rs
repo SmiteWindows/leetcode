a6c8d8e722b84e81912d20b3e6d38fe441fa7e61
SmiteWindows <x1627283564@hotmail.com>
2020-06-04 16:23:16 +0800

    202006041623

--- src/hash_table/leetcode748.rs
+++ src/hash_table/leetcode748.rs
@@ -1,10 +1,53 @@
 // https://leetcode.com/problems/shortest-completing-word/
+// Runtime: 4 ms
+// Memory Usage: 2.2 MB
 pub fn shortest_completing_word(license_plate: String, words: Vec<String>) -> String {
-    todo!()
+    let mut min: Option<String> = None;
+    let lowercase = license_plate
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .collect::<String>()
+        .to_lowercase();
+    let count_of_lowercase = Count::new(&lowercase);
+    for word in words {
+        if Count::new(&word).completes(&count_of_lowercase) {
+            if let Some(ref s) = min {
+                if word.len() < s.len() {
+                    min = Some(word);
+                }
+            } else {
+                min = Some(word);
+            }
+        }
+    }
+    min.expect("exist")
+}
+
+struct Count {
+    v: Vec<i32>,
+}
+
+impl Count {
+    fn new(s: &str) -> Self {
+        let mut v = vec![0; 256];
+        for c in s.chars() {
+            v[c as usize] += 1;
+        }
+        Self { v }
+    }
+
+    fn completes(&self, other: &Self) -> bool {
+        for i in 0..26 {
+            let c = (b'a' + i) as usize;
+            if self.v[c] < other.v[c] {
+                return false;
+            }
+        }
+        true
+    }
 }
 // hash_table
 #[test]
-#[ignore]
 fn test1_748() {
     assert_eq!(
         shortest_completing_word(
