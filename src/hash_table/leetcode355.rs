860c718012d8670d1d0020adf0de43fbca6f610c
SmiteWindows <x1627283564@hotmail.com>
2020-06-05 09:40:21 +0800

    202006050940

--- src/hash_table/leetcode355.rs
+++ src/hash_table/leetcode355.rs
@@ -42,7 +42,7 @@ impl Twitter {
         for &user in followers.iter() {
             let tweets = self.tweets.entry(user).or_default();
             for tweet in tweets {
-                pq.push(tweet.clone());
+                pq.push(*tweet);
                 if pq.len() > self.limit {
                     pq.pop();
                 }
