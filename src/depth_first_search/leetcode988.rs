b37b3dd342d46a6fa52ced948fa57a94371bf043
SmiteWindows <x1627283564@hotmail.com>
2020-06-06 20:07:34 +0800

    202006062007

--- src/depth_first_search/leetcode988.rs
+++ src/depth_first_search/leetcode988.rs
@@ -1,8 +1,31 @@
 // https://leetcode.com/problems/smallest-string-starting-from-leaf/
-
+// Runtime: 0 ms
+// Memory Usage: 2.7 MB
 use std::{cell::RefCell, rc::Rc};
 pub fn smallest_from_leaf(root: Option<Rc<RefCell<TreeNode>>>) -> String {
-    todo!()
+    let mut cur = vec![];
+    let mut res = "".to_string();
+    preorder(root.as_deref(), &mut cur, &mut res);
+    res
+}
+
+fn preorder(root: Option<&RefCell<TreeNode>>, cur: &mut Vec<char>, min: &mut String) {
+    if let Some(node) = root {
+        let node = node.borrow();
+        let val = (node.val as u8 + b'a') as char;
+        cur.push(val);
+        if node.left.is_none() && node.right.is_none() {
+            let s = cur.iter().rev().copied().collect::<String>();
+            if min == "" {
+                *min = s;
+            } else if s < *min {
+                *min = s;
+            }
+        }
+        preorder(node.left.as_deref(), cur, min);
+        preorder(node.right.as_deref(), cur, min);
+        cur.pop();
+    }
 }
 
 // Definition for a binary tree node.
@@ -25,7 +48,6 @@ impl TreeNode {
 }
 // tree depth_first_search
 #[test]
-#[ignore]
 fn test2_988() {
     let t1 = Some(Rc::new(RefCell::new(TreeNode {
         val: 0,
