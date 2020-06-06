a4080ca848ae805165c4c8e8a810395986f6a77a
SmiteWindows <x1627283564@hotmail.com>
2020-06-06 18:01:02 +0800

    202006061800

--- src/depth_first_search/leetcode638.rs
+++ src/depth_first_search/leetcode638.rs
@@ -1,10 +1,48 @@
 // https://leetcode.com/problems/shopping-offers/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+use std::collections::HashMap;
 pub fn shopping_offers(price: Vec<i32>, special: Vec<Vec<i32>>, needs: Vec<i32>) -> i32 {
-    todo!()
+    let n = price.len();
+    let m = special.len();
+    let mut memo = HashMap::new();
+    memo.insert(vec![0; n], 0);
+    dfs(needs, &mut memo, &price, &special, n, m)
+}
+
+fn dfs(
+    needs: Vec<i32>,
+    memo: &mut HashMap<Vec<i32>, i32>,
+    price: &[i32],
+    special: &[Vec<i32>],
+    n: usize,
+    m: usize,
+) -> i32 {
+    if let Some(&res) = memo.get(&needs) {
+        return res;
+    }
+    let mut res = needs
+        .iter()
+        .zip(price.iter())
+        .map(|(a, b)| a * b)
+        .sum::<i32>();
+    'special: for s in special.iter() {
+        for i in 0..n {
+            if s[i] > needs[i] {
+                continue 'special;
+            }
+        }
+        let mut needs = needs.to_vec();
+        for i in 0..n {
+            needs[i] -= s[i];
+        }
+        res = res.min(dfs(needs, memo, price, special, n, m) + s[n]);
+    }
+    memo.insert(needs, res);
+    res
 }
 // dynamic_programming depth_first_search
 #[test]
-#[ignore]
 fn test2_638() {
     assert_eq!(
         shopping_offers(vec![2, 5], vec![vec![3, 0, 5], vec![1, 2, 10]], vec![3, 2]),
