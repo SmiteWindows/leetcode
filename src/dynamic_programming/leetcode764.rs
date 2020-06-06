972435fd51498e212b34ac97d3b97e480ae18fdc
SmiteWindows <x1627283564@hotmail.com>
2020-06-06 16:08:19 +0800

    202006061608

--- src/dynamic_programming/leetcode764.rs
+++ src/dynamic_programming/leetcode764.rs
@@ -1,12 +1,79 @@
 // https://leetcode.com/problems/largest-plus-sign/
+// Runtime: 60 ms
+// Memory Usage: 11.1 MB
 pub fn order_of_largest_plus_sign(n: i32, mines: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let n = n as usize;
+    let mut grid = vec![vec![1; n]; n];
+    let mut left = vec![vec![0; n]; n];
+    let mut top = vec![vec![0; n]; n];
+    let mut right = vec![vec![0; n]; n];
+    let mut bottom = vec![vec![0; n]; n];
+    for mine in mines {
+        let i = mine[0] as usize;
+        let j = mine[1] as usize;
+        grid[i][j] = 0;
+    }
+    for i in 0..n {
+        for j in 0..n {
+            if grid[i][j] == 1 {
+                if j > 0 {
+                    left[i][j] = left[i][j - 1] + 1;
+                } else {
+                    left[i][j] = 1;
+                }
+            }
+        }
+    }
+    for j in 0..n {
+        for i in 0..n {
+            if grid[i][j] == 1 {
+                if i > 0 {
+                    top[i][j] = top[i - 1][j] + 1;
+                } else {
+                    top[i][j] = 1;
+                }
+            }
+        }
+    }
+    for i in 0..n {
+        for j in (0..n).rev() {
+            if grid[i][j] == 1 {
+                if j + 1 < n {
+                    right[i][j] = right[i][j + 1] + 1;
+                } else {
+                    right[i][j] = 1;
+                }
+            }
+        }
+    }
+    for j in 0..n {
+        for i in (0..n).rev() {
+            if grid[i][j] == 1 {
+                if i + 1 < n {
+                    bottom[i][j] = bottom[i + 1][j] + 1;
+                } else {
+                    bottom[i][j] = 1;
+                }
+            }
+        }
+    }
+    let mut res = 0;
+    for i in 0..n {
+        for j in 0..n {
+            let mut min = n;
+            min = min.min(left[i][j]);
+            min = min.min(right[i][j]);
+            min = min.min(top[i][j]);
+            min = min.min(bottom[i][j]);
+            res = res.max(min);
+        }
+    }
+    res as i32
 }
 // dynamic_programming
 #[test]
-#[ignore]
 fn test1_764() {
     assert_eq!(order_of_largest_plus_sign(5, vec![vec![4, 2]]), 2);
-    assert_eq!(order_of_largest_plus_sign(2, vec![vec![]]), 1);
+    assert_eq!(order_of_largest_plus_sign(2, vec![]), 1);
     assert_eq!(order_of_largest_plus_sign(1, vec![vec![0, 0]]), 0);
 }
--- src/stack/leetcode1124.rs
+++ src/stack/leetcode1124.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/longest-well-performing-interval/
+// Runtime: 4 ms
+// Memory Usage: 2.4 MB
+use std::collections::HashMap;
 pub fn longest_wpi(hours: Vec<i32>) -> i32 {
-    todo!()
+    let mut hm = HashMap::new();
+    let mut score = 0;
+    let mut res = 0;
+    for (i, &hour) in hours.iter().enumerate() {
+        score += if hour > 8 { 1 } else { -1 };
+        if score > 0 {
+            res = i + 1;
+        } else {
+            hm.entry(score).or_insert(i);
+            if let Some(j) = hm.get(&(score - 1)) {
+                res = res.max(i - j);
+            }
+        }
+    }
+    res as i32
 }
 // stack
 #[test]
-#[ignore]
 fn test1_1124() {
     assert_eq!(longest_wpi(vec![9, 9, 6, 0, 6, 6, 9]), 3);
 }
