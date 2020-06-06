e5ba41ee6d8d47ee4ba0ff6405d15c900938008a
SmiteWindows <x1627283564@hotmail.com>
2020-06-06 17:44:15 +0800

    202006061744

--- src/math/leetcode396.rs
+++ src/math/leetcode396.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/rotate-function
+// Runtime: 4 ms
+// Memory Usage: 2.2 MB
 pub fn max_rotate_function(a: Vec<i32>) -> i32 {
-    todo!()
+    let n = a.len();
+    if n == 0 {
+        return 0;
+    }
+    let sum = a.iter().sum::<i32>();
+    let mut f = 0;
+    for (i, ai) in a.iter().enumerate() {
+        f += i as i32 * ai;
+    }
+    let mut res = f;
+    for (i, ai) in a.iter().enumerate().rev() {
+        f = f + sum - n as i32 * ai;
+        res = res.max(f);
+    }
+    res
 }
 // math
 #[test]
-#[ignore]
 fn test1_396() {
     assert_eq!(max_rotate_function(vec![4, 3, 2, 6]), 26);
 }
