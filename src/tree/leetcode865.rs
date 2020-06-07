71c0095a3029a2fa4efb8d42eab8716dd8afbb4f
SmiteWindows <x1627283564@hotmail.com>
2020-06-07 16:02:59 +0800

    202006071602

--- src/tree/leetcode865.rs
+++ src/tree/leetcode865.rs
@@ -1,12 +1,28 @@
 // https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
-
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
+use std::cmp::Ordering::*;
 use std::{cell::RefCell, rc::Rc};
 pub fn subtree_with_all_deepest(
     root: Option<Rc<RefCell<TreeNode>>>,
 ) -> Option<Rc<RefCell<TreeNode>>> {
-    todo!()
+    postorder(root.as_ref()).1
 }
 
+fn postorder(root: Option<&Rc<RefCell<TreeNode>>>) -> (usize, Option<Rc<RefCell<TreeNode>>>) {
+    if let Some(node) = root {
+        let n = node.borrow();
+        let (left_depth, left_tree) = postorder(n.left.as_ref());
+        let (right_depth, rigth_tree) = postorder(n.right.as_ref());
+        match left_depth.cmp(&right_depth) {
+            Equal => (left_depth + 1, Some(node.clone())),
+            Less => (right_depth + 1, rigth_tree),
+            Greater => (left_depth + 1, left_tree),
+        }
+    } else {
+        (0, None)
+    }
+}
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
@@ -27,7 +43,6 @@ impl TreeNode {
 }
 // tree
 #[test]
-#[ignore]
 fn test1_865() {
     let root = Some(Rc::new(RefCell::new(TreeNode {
         val: 3,
