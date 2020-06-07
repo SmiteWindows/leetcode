939e168a9f129f79a4d7193f0196448e00e99a74
SmiteWindows <x1627283564@hotmail.com>
2020-06-07 20:13:50 +0800

    202006072013

--- src/tree/leetcode1104.rs
+++ src/tree/leetcode1104.rs
@@ -1,10 +1,23 @@
 // https://leetcode.com/problems/path-in-zigzag-labelled-binary-tree/
+// Runtime: 0 ms
+// Memory Usage: 1.9 MB
 pub fn path_in_zig_zag_tree(label: i32) -> Vec<i32> {
-    todo!()
+    let mut label = label;
+    let mut level = 0;
+    while label >= 1 << level {
+        level += 1;
+    }
+    let mut res = vec![0; level];
+    for i in (0..level).rev() {
+        res[i] = label;
+        let r = (1 << i) - 1;
+        let l = r / 2 + 1;
+        label = l + r - label / 2;
+    }
+    res
 }
 // tree math
 #[test]
-#[ignore]
 fn test1_1104() {
     assert_eq!(path_in_zig_zag_tree(14), vec![1, 3, 4, 14]);
     assert_eq!(path_in_zig_zag_tree(26), vec![1, 2, 6, 10, 26]);
