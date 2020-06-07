e9478b12a674debeea04bab686d4c6afcf516a61
SmiteWindows <x1627283564@hotmail.com>
2020-06-07 17:37:08 +0800

    202006071737

--- src/tree/leetcode894.rs
+++ src/tree/leetcode894.rs
@@ -1,8 +1,34 @@
 // https://leetcode.com/problems/all-possible-full-binary-trees/
-
+// Runtime: 16 ms
+// Memory Usage: 5.2 MB
 use std::{cell::RefCell, rc::Rc};
 pub fn all_possible_fbt(n: i32) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
-    todo!()
+    if n % 2 == 0 {
+        return vec![];
+    }
+    if n == 1 {
+        vec![Some(Rc::new(RefCell::new(TreeNode::new(0))))]
+    } else {
+        let mut res = vec![];
+        let mut l = 1;
+        let mut r = n - 1 - l;
+        while r > 0 {
+            let left_trees = all_possible_fbt(l);
+            let right_trees = all_possible_fbt(r);
+            for left in &left_trees {
+                for right in &right_trees {
+                    res.push(Some(Rc::new(RefCell::new(TreeNode {
+                        val: 0,
+                        left: left.clone(),
+                        right: right.clone(),
+                    }))));
+                }
+            }
+            r -= 2;
+            l += 2;
+        }
+        res
+    }
 }
 
 // Definition for a binary tree node.
@@ -25,7 +51,6 @@ impl TreeNode {
 }
 // tree recursion
 #[test]
-#[ignore]
 fn test1_894() {
     let t1 = Some(Rc::new(RefCell::new(TreeNode {
         val: 0,
@@ -39,7 +64,7 @@ fn test1_894() {
                 right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
             }))),
         }))),
-    })));
+    }))); // [0,0,0,null,null,0,0,null,null,0,0]
     let t2 = Some(Rc::new(RefCell::new(TreeNode {
         val: 0,
         left: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
@@ -52,7 +77,7 @@ fn test1_894() {
             }))),
             right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
         }))),
-    })));
+    }))); // [0,0,0,null,null,0,0,0,0]
     let t3 = Some(Rc::new(RefCell::new(TreeNode {
         val: 0,
         left: Some(Rc::new(RefCell::new(TreeNode {
@@ -65,7 +90,7 @@ fn test1_894() {
             left: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
             right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
         }))),
-    })));
+    }))); // [0,0,0,0,0,0,0]
     let t4 = Some(Rc::new(RefCell::new(TreeNode {
         val: 0,
         left: Some(Rc::new(RefCell::new(TreeNode {
@@ -78,23 +103,19 @@ fn test1_894() {
             }))),
         }))),
         right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
-    })));
+    }))); // [0,0,0,0,0,null,null,null,null,0,0]
     let t5 = Some(Rc::new(RefCell::new(TreeNode {
         val: 0,
         left: Some(Rc::new(RefCell::new(TreeNode {
             val: 0,
             left: Some(Rc::new(RefCell::new(TreeNode {
                 val: 0,
-                left: Some(Rc::new(RefCell::new(TreeNode {
-                    val: 0,
-                    left: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
-                    right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
-                }))),
+                left: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
                 right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
             }))),
             right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
         }))),
         right: Some(Rc::new(RefCell::new(TreeNode::new(0)))),
-    })));
+    }))); // [0,0,0,0,0,null,null,0,0]
     assert_eq!(all_possible_fbt(7), vec![t1, t2, t3, t4, t5]);
 }
