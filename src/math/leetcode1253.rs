8e7d5f288838d14128e16c7f17a6cc8969771d0d
SmiteWindows <x1627283564@hotmail.com>
2020-06-07 11:22:36 +0800

    202006071122

--- src/math/leetcode1253.rs
+++ src/math/leetcode1253.rs
@@ -1,10 +1,39 @@
 // https://leetcode.com/problems/reconstruct-a-2-row-binary-matrix/
+// Runtime: 36 ms
+// Memory Usage: 3.8 MB
 pub fn reconstruct_matrix(upper: i32, lower: i32, colsum: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut upper = upper;
+    let mut lower = lower;
+    let n = colsum.len();
+    let mut res = vec![vec![0; n]; 2];
+    for j in 0..n {
+        match colsum[j] {
+            2 => {
+                res[0][j] = 1;
+                res[1][j] = 1;
+                upper -= 1;
+                lower -= 1;
+            }
+            1 => {
+                if upper >= lower {
+                    res[0][j] = 1;
+                    upper -= 1;
+                } else {
+                    res[1][j] = 1;
+                    lower -= 1;
+                }
+            }
+            _ => {}
+        }
+    }
+    if upper == 0 && lower == 0 {
+        res
+    } else {
+        vec![]
+    }
 }
 // math greedy
 #[test]
-#[ignore]
 fn test1_1253() {
     assert_eq!(
         reconstruct_matrix(2, 1, vec![1, 1, 1]),
