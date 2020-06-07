57e33677b53b756fa4481352539a2eef9a455c1f
SmiteWindows <x1627283564@hotmail.com>
2020-06-08 07:03:22 +0800

    202006080703

--- src/graph/leetcode990.rs
+++ src/graph/leetcode990.rs
@@ -1,10 +1,62 @@
 // https://leetcode.com/problems/satisfiability-of-equality-equations/
+// Runtime: 0 ms
+// Memory Usage: 2.2 MB
 pub fn equations_possible(equations: Vec<String>) -> bool {
-    todo!()
+    let mut uf = UnionFind::new(26);
+    let mut pairs = vec![];
+    for equation in equations {
+        let s = equation.chars().collect::<Vec<_>>();
+        let i = (s[0] as u8 - b'a') as usize;
+        let j = (s[3] as u8 - b'a') as usize;
+        if s[1] == '=' {
+            uf.union(i, j);
+        } else {
+            pairs.push((i, j));
+        }
+    }
+    for pair in pairs {
+        let i = pair.0;
+        let j = pair.1;
+        if uf.find(i) == uf.find(j) {
+            return false;
+        }
+    }
+    true
+}
+
+struct UnionFind {
+    parent: Vec<usize>,
+    n: usize,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        let parent = (0..n).collect();
+        Self { parent, n }
+    }
+
+    fn find(&mut self, i: usize) -> usize {
+        let j = self.parent[i];
+        if i == j {
+            j
+        } else {
+            let k = self.find(j);
+            self.parent[i] = k;
+            k
+        }
+    }
+
+    fn union(&mut self, i: usize, j: usize) {
+        let i = self.find(i);
+        let j = self.find(j);
+        if i != j {
+            self.parent[i] = j;
+            self.n -= 1;
+        }
+    }
 }
 // union_find graph
 #[test]
-#[ignore]
 fn test2_990() {
     assert_eq!(
         equations_possible(vec![String::from("a==b"), String::from("b!=a")]),
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("Hello, Rust!");
     println!("Hello, LeetCode!");
-    println!("1469");
+    println!("1473");
 }
