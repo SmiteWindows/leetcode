9bd8b34017390bf51756367b95059f1fc929318d
SmiteWindows <x1627283564@hotmail.com>
2020-06-07 12:40:16 +0800

    202006071240

--- src/stack/leetcode1019.rs
+++ src/stack/leetcode1019.rs
@@ -1,7 +1,28 @@
 // https://leetcode.com/problems/next-greater-node-in-linked-list/
-
+// Runtime: 32 ms
+// Memory Usage: 2.6 MB
 pub fn next_larger_nodes(head: Option<Box<ListNode>>) -> Vec<i32> {
-    todo!()
+    let mut head = head;
+    let mut nodes = vec![];
+    while let Some(node) = head {
+        nodes.push(node.val);
+        head = node.next;
+    }
+    let n = nodes.len();
+    let mut stack = vec![];
+    let mut res = vec![0; n];
+    for i in 0..n {
+        while let Some(j) = stack.pop() {
+            if nodes[j] < nodes[i] {
+                res[j] = nodes[i];
+            } else {
+                stack.push(j);
+                break;
+            }
+        }
+        stack.push(i);
+    }
+    res
 }
 
 // Definition for singly-linked list.
@@ -19,7 +40,6 @@ impl ListNode {
 }
 // linked_list stack
 #[test]
-#[ignore]
 fn test2_1019() {
     let l1 = Some(Box::new(ListNode {
         val: 2,
