b0771aac6bb4342c8da9b289b0ccbd73c830d79e
SmiteWindows <x1627283564@hotmail.com>
2020-06-07 20:22:16 +0800

    202006072022

--- src/stack/leetcode1130.rs
+++ src/stack/leetcode1130.rs
@@ -1,10 +1,26 @@
 // https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn mct_from_leaf_values(arr: Vec<i32>) -> i32 {
-    todo!()
+    let mut res = 0;
+    let mut stack = vec![i32::MAX];
+    for x in arr {
+        while x >= *stack.last().unwrap() {
+            let mid = stack.pop().unwrap();
+            let y = *stack.last().unwrap();
+            res += mid * x.min(y);
+        }
+        stack.push(x);
+    }
+    while stack.len() > 2 {
+        let x = stack.pop().unwrap();
+        let y = *stack.last().unwrap();
+        res += x * y;
+    }
+    res
 }
 // tree stack dynamic_programming
 #[test]
-#[ignore]
 fn test3_1130() {
     assert_eq!(mct_from_leaf_values(vec![6, 2, 4]), 32);
 }
