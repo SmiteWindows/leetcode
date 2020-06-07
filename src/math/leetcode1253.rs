e6a66accac24b83b6dd5a89fa34034ec03887b71
SmiteWindows <x1627283564@hotmail.com>
2020-06-07 11:25:47 +0800

    202006071125

--- src/math/leetcode1253.rs
+++ src/math/leetcode1253.rs
@@ -6,8 +6,8 @@ pub fn reconstruct_matrix(upper: i32, lower: i32, colsum: Vec<i32>) -> Vec<Vec<i
     let mut lower = lower;
     let n = colsum.len();
     let mut res = vec![vec![0; n]; 2];
-    for j in 0..n {
-        match colsum[j] {
+    for (j, &cs) in colsum.iter().enumerate().take(n) {
+        match cs {
             2 => {
                 res[0][j] = 1;
                 res[1][j] = 1;
