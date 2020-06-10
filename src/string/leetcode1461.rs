6ae2c626a4bd4b2278652c5247165a8ecb68cdd6
SmiteWindows <x1627283564@hotmail.com>
2020-06-10 08:48:08 +0800

    202006100848

--- src/string/leetcode1461.rs
+++ src/string/leetcode1461.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+// Runtime: 4 ms
+// Memory Usage: 3.3 MB
 pub fn has_all_codes(s: String, k: i32) -> bool {
-    todo!()
+    let k = k as usize;
+    let m = 1 << k;
+    let mut set = vec![false; m];
+    let mut x = 0;
+    let n = s.len();
+    let mask = m as u32 - 1;
+    for (i, c) in s.char_indices().rev() {
+        x <<= 1;
+        x |= (c as u8 - b'0') as u32;
+        x &= mask;
+        if i + k <= n {
+            set[x as usize] = true;
+        }
+    }
+    set.iter().all(|&b| b)
 }
 // string bit_manipulation
 #[test]
-#[ignore]
 fn test1_1461() {
     assert_eq!(has_all_codes(String::from("00110110"), 2), true);
     assert_eq!(has_all_codes(String::from("00110"), 2), true);
