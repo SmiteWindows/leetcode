f8858fd7ac85b25ca7f05437f87ca76de7aa1a5f
SmiteWindows <x1627283564@hotmail.com>
2020-06-10 08:22:16 +0800

    202006100822

--- src/string/leetcode1432.rs
+++ src/string/leetcode1432.rs
@@ -1,10 +1,37 @@
 // https://leetcode.com/problems/max-difference-you-can-get-from-changing-an-integer/
+// Runtime: 4 ms
+// Memory Usage: 2.1 MB
 pub fn max_diff(num: i32) -> i32 {
-    todo!()
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
+    for i in 0..10 {
+        for j in 0..10 {
+            let mut s = num.to_string().bytes().collect::<Vec<_>>();
+            for k in s.iter_mut() {
+                if *k == i + b'0' {
+                    *k = j + b'0';
+                }
+            }
+            if s[0] == b'0' {
+                continue;
+            }
+            let x = s
+                .into_iter()
+                .map(|b| b as char)
+                .collect::<String>()
+                .parse::<i32>()
+                .unwrap();
+            if x == 0 {
+                continue;
+            }
+            min = min.min(x);
+            max = max.max(x);
+        }
+    }
+    max - min
 }
 // string
 #[test]
-#[ignore]
 fn test1_1432() {
     assert_eq!(max_diff(555), 888);
     assert_eq!(max_diff(9), 8);
