1c0f15ec1991f689c2c0a9be7bccc37ea251405e
SmiteWindows <x1627283564@hotmail.com>
2020-06-10 20:16:53 +0800

    202006102016

--- src/array/leetcode56.rs
+++ src/array/leetcode56.rs
@@ -1,10 +1,30 @@
 // https://leetcode.com/problems/merge-intervals/
+// Runtime: 0 ms
+// Memory Usage: 2.6 MB
 pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut intervals = intervals;
+    intervals.sort_unstable_by_key(|a| a[0]);
+    let mut res = vec![];
+    let mut temp: Option<Vec<i32>> = None;
+    for v in intervals {
+        if let Some(t) = temp {
+            if v[0] <= t[1] {
+                temp = Some(vec![t[0], t[1].max(v[1])]);
+            } else {
+                temp = Some(v);
+                res.push(t);
+            }
+        } else {
+            temp = Some(v);
+        }
+    }
+    if let Some(t) = temp {
+        res.push(t);
+    }
+    res
 }
 // array sort
 #[test]
-#[ignore]
 fn test2_56() {
     assert_eq!(
         merge(vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]]),
