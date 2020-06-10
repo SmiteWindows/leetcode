1efb1bc778b21e2c6c29096d63c3ffd32372cfa4
SmiteWindows <x1627283564@hotmail.com>
2020-06-10 10:55:19 +0800

    202006101055

--- src/string/leetcode1404.rs
+++ src/string/leetcode1404.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn num_steps(s: String) -> i32 {
-    todo!()
+    let mut carry = 0;
+    let mut res = 0;
+    let n = s.len();
+    let s = s.bytes().collect::<Vec<_>>();
+    for si in s.iter().skip(1).rev() {
+        res += 1;
+        if si - b'0' + carry == 1 {
+            carry = 1;
+            res += 1;
+        }
+    }
+    res + carry as i32
 }
 // string bit_manipulation
 #[test]
-#[ignore]
 fn test2_1404() {
     assert_eq!(num_steps(String::from("1101")), 6);
     assert_eq!(num_steps(String::from("10")), 1);
