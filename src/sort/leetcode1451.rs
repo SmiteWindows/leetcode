967bbcdaad65eb20ea4e4679f15f7e63e6a27efe
SmiteWindows <x1627283564@hotmail.com>
2020-06-10 10:24:14 +0800

    202006101024

--- src/sort/leetcode1451.rs
+++ src/sort/leetcode1451.rs
@@ -1,10 +1,20 @@
 // https://leetcode.com/problems/rearrange-words-in-a-sentence/
+// Runtime: 12 ms
+// Memory Usage: 2.9 MB
 pub fn arrange_words(text: String) -> String {
-    todo!()
+    let mut words = text
+        .split_whitespace()
+        .enumerate()
+        .map(|(i, s)| (s.len(), i, s.to_lowercase()))
+        .collect::<Vec<_>>();
+    words.sort();
+    let words = words.into_iter().map(|(_, _, s)| s).collect::<Vec<_>>();
+    let mut res = words.join(" ");
+    res[0..1].make_ascii_uppercase();
+    res
 }
 // string sort
 #[test]
-#[ignore]
 fn test2_1451() {
     assert_eq!(
         arrange_words(String::from("Leetcode is cool")),
