59cc2b10e71b05853cdac5d5ad065f849a03c241
SmiteWindows <x1627283564@hotmail.com>
2020-06-11 13:08:16 +0800

    202006111308

--- src/sort/leetcode179.rs
+++ src/sort/leetcode179.rs
@@ -1,10 +1,20 @@
 // https://leetcode.com/problems/largest-number/
+// Runtime: 4 ms
+// Memory Usage: 2.1 MB
 pub fn largest_number(nums: Vec<i32>) -> String {
-    todo!()
+    let mut v = nums.iter().map(|x| x.to_string()).collect::<Vec<_>>();
+    v.sort_unstable_by(|a, b| format!("{}{}", b, a).cmp(&format!("{}{}", a, b)));
+    let mut res: String = "".to_string();
+    if v[0] == "0" {
+        return "0".to_string();
+    }
+    for s in v {
+        res += &s;
+    }
+    res
 }
 // sort
 #[test]
-#[ignore]
 fn test1_179() {
     assert_eq!(largest_number(vec![10, 2]), String::from("210"));
     assert_eq!(
