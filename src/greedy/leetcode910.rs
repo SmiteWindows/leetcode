108a2e8a74592f5e202e2247329b690fc025978c
SmiteWindows <x1627283564@hotmail.com>
2020-06-14 19:25:44 +0800

    202006141925

--- src/greedy/leetcode910.rs
+++ src/greedy/leetcode910.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/smallest-range-ii/
+// Runtime: 4 ms
+// Memory Usage: 2.1 MB
 pub fn smallest_range_ii(a: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut a = a;
+    let n = a.len();
+    a.sort_unstable();
+    let mut max = a[n - 1];
+    let mut min = a[0];
+    let mut res = max - min;
+    for i in 0..n - 1 {
+        max = max.max(a[i] + 2 * k);
+        min = a[i + 1].min(a[0] + 2 * k);
+        res = res.min(max - min);
+    }
+    res
 }
 // math greedy
 #[test]
-#[ignore]
 fn test2_910() {
     assert_eq!(smallest_range_ii(vec![1], 0), 0);
     assert_eq!(smallest_range_ii(vec![0, 10], 2), 6);
--- src/math/leetcode869.rs
+++ src/math/leetcode869.rs
@@ -1,10 +1,20 @@
 // https://leetcode.com/problems/reordered-power-of-2/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
+use std::collections::HashSet;
 pub fn reordered_power_of2(n: i32) -> bool {
-    todo!()
+    let mut hs = HashSet::new();
+    for i in 0..32 {
+        let mut v = (1 << i).to_string().chars().collect::<Vec<_>>();
+        v.sort_unstable();
+        hs.insert(v);
+    }
+    let mut v = n.to_string().chars().collect::<Vec<_>>();
+    v.sort_unstable();
+    hs.contains(&v)
 }
 // math
 #[test]
-#[ignore]
 fn test1_869() {
     assert_eq!(reordered_power_of2(1), true);
     assert_eq!(reordered_power_of2(10), false);
