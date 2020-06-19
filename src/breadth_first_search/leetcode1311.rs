19b497fd1be30ba546c6e1897be746730fcb8271
SmiteWindows <x1627283564@hotmail.com>
2020-06-19 10:23:44 +0800

    202006191023

--- src/breadth_first_search/leetcode1311.rs
+++ src/breadth_first_search/leetcode1311.rs
@@ -1,15 +1,44 @@
 // https://leetcode.com/problems/get-watched-videos-by-your-friends/
+// Runtime: 28 ms
+// Memory Usage: 3.1 MB
+use std::collections::{HashMap, VecDeque};
 pub fn watched_videos_by_friends(
     watched_videos: Vec<Vec<String>>,
     friends: Vec<Vec<i32>>,
     id: i32,
     level: i32,
 ) -> Vec<String> {
-    todo!()
+    let n = watched_videos.len();
+    let mut visited = vec![false; n];
+    let mut queue = VecDeque::new();
+    let id = id as usize;
+    visited[id] = true;
+    queue.push_back((id, 0));
+    let mut freq: HashMap<String, usize> = HashMap::new();
+    while let Some((u, l)) = queue.pop_front() {
+        if l < level {
+            for &friend in &friends[u] {
+                let v = friend as usize;
+                if !visited[v] {
+                    visited[v] = true;
+                    queue.push_back((v, l + 1));
+                }
+            }
+        } else {
+            for video in &watched_videos[u] {
+                *freq.entry(video.to_string()).or_default() += 1;
+            }
+        }
+    }
+    let mut pairs: Vec<(usize, String)> = vec![];
+    for (video, count) in freq {
+        pairs.push((count, video));
+    }
+    pairs.sort_unstable();
+    pairs.into_iter().map(|p| p.1).collect()
 }
 // hash_table string breadth_first_search
 #[test]
-#[ignore]
 fn test3_1311() {
     assert_eq!(
         watched_videos_by_friends(
--- src/math/leetcode1201.rs
+++ src/math/leetcode1201.rs
@@ -1,10 +1,38 @@
 // https://leetcode.com/problems/ugly-number-iii/
+// Runtime: 0 ms
+// Memory Usage: 2 MB
 pub fn nth_ugly_number(n: i32, a: i32, b: i32, c: i32) -> i32 {
-    todo!()
+    let mut left = 0;
+    let mut right = 2_000_000_000;
+    while left < right {
+        let mid = left + (right - left) / 2;
+        if count(mid, a as u64, b as u64, c as u64) < n as u64 {
+            left = mid + 1;
+        } else {
+            right = mid
+        }
+    }
+    left as i32
+}
+
+fn count(num: u64, a: u64, b: u64, c: u64) -> u64 {
+    num / a + num / b + num / c - num / lcm(a, b) - num / lcm(b, c) - num / lcm(a, c)
+        + num / lcm(a, lcm(b, c))
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    if a == 0 {
+        b
+    } else {
+        gcd(b % a, a)
+    }
 }
 // math binary_search
 #[test]
-#[ignore]
 fn test1_1201() {
     assert_eq!(nth_ugly_number(3, 2, 3, 5), 4);
     assert_eq!(nth_ugly_number(4, 2, 3, 4), 6);
