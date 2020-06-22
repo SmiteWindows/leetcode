ab3b793776e51e993b0324eb18f9aeca46d96be4
SmiteWindows <x1627283564@hotmail.com>
2020-06-22 10:14:42 +0800

    202006221014

--- src/queue/leetcode621.rs
+++ src/queue/leetcode621.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/task-scheduler/
+// Runtime: 24 ms
+// Memory Usage: 2.7 MB
 pub fn least_interval(tasks: Vec<char>, n: i32) -> i32 {
-    todo!()
+    use std::collections::HashMap;
+    let n = n as usize;
+    let m = tasks.len();
+    let mut hm: HashMap<char, usize> = HashMap::new();
+    let mut max = 0;
+    for t in tasks {
+        let count = hm.entry(t).or_default();
+        *count += 1;
+        max = max.max(*count);
+    }
+    let v = hm.iter().fold(
+        0,
+        |sum, (_, &count)| if count == max { sum + 1 } else { sum },
+    );
+    m.max((n + 1) * (max - 1) + v) as i32
 }
 // array greedy queue
 #[test]
-#[ignore]
 fn test1_621() {
     assert_eq!(least_interval(vec!['A', 'A', 'A', 'B', 'B', 'B'], 2), 8);
 }
