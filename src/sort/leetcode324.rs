b2d8ae88e6588311a1feea4254d035f5f2a0487a
SmiteWindows <x1627283564@hotmail.com>
2020-07-02 10:13:36 +0800

    202007021013

--- src/sort/leetcode324.rs
+++ src/sort/leetcode324.rs
@@ -18,7 +18,7 @@ pub fn wiggle_sort(nums: &mut Vec<i32>) {
 fn test1_324() {
     let mut nums1 = vec![1, 5, 1, 1, 6, 4];
     wiggle_sort(&mut nums1);
-    assert_eq!(nums1, vec![1, 4, 1, 5, 1, 6]);
+    assert_eq!(nums1, vec![1, 6, 1, 5, 1, 4]);
     let mut nums2 = vec![1, 3, 2, 2, 3, 1];
     wiggle_sort(&mut nums2);
     assert_eq!(nums2, vec![2, 3, 1, 3, 1, 2]);
