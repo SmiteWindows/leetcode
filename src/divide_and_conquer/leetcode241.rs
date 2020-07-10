551024b6b8ffef3b72a1e7a27fc1d69df97722db
SmiteWindows <x1627283564@hotmail.com>
2020-07-10 16:39:25 +0800

    202007101639

--- src/divide_and_conquer/leetcode241.rs
+++ src/divide_and_conquer/leetcode241.rs
@@ -1,14 +1,79 @@
 // https://leetcode.com/problems/different-ways-to-add-parentheses/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn diff_ways_to_compute(input: String) -> Vec<i32> {
-    todo!()
+    let mut val = 0;
+    let mut toks: Vec<Tok> = vec![];
+    for c in input.chars() {
+        match c {
+            '+' | '-' | '*' => {
+                toks.push(Tok::Val(val));
+                toks.push(Tok::Op(c));
+                val = 0;
+            }
+            _ => {
+                val *= 10;
+                val += (c as u8 - b'0') as i32;
+            }
+        }
+    }
+    toks.push(Tok::Val(val));
+    eval(&toks)
+}
+
+fn eval(toks: &[Tok]) -> Vec<i32> {
+    let mut res = vec![];
+    let n = toks.len();
+    for i in 0..n {
+        if let Tok::Op(c) = toks[i] {
+            let left = eval(&toks[0..i]);
+            let right = eval(&toks[i + 1..n]);
+            for l in &left {
+                for r in &right {
+                    match c {
+                        '+' => {
+                            res.push(l + r);
+                        }
+                        '-' => {
+                            res.push(l - r);
+                        }
+                        '*' => {
+                            res.push(l * r);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+    if res.is_empty() {
+        if let Tok::Val(val) = toks[0] {
+            vec![val]
+        } else {
+            unreachable!()
+        }
+    } else {
+        res
+    }
+}
+
+enum Tok {
+    Op(char),
+    Val(i32),
 }
 // divide_and_conquer
 #[test]
-#[ignore]
 fn test1_241() {
-    assert_eq!(diff_ways_to_compute(String::from("2-1-1")), vec![0, 2]);
-    assert_eq!(
-        diff_ways_to_compute(String::from("2*3-4*5")),
-        vec![-34, -14, -10, -10, 10]
-    );
+    let input = "2-1-1".to_string();
+    let mut res = vec![0, 2];
+    let mut ans = diff_ways_to_compute(input);
+    res.sort();
+    ans.sort();
+    assert_eq!(ans, res);
+    let input = "2*3-4*5".to_string();
+    let mut res = vec![-34, -14, -10, -10, 10];
+    let mut ans = diff_ways_to_compute(input);
+    res.sort();
+    ans.sort();
+    assert_eq!(ans, res);
 }
--- src/divide_and_conquer/leetcode932.rs
+++ src/divide_and_conquer/leetcode932.rs
@@ -1,11 +1,21 @@
 // https://leetcode.com/problems/beautiful-array/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn beautiful_array(n: i32) -> Vec<i32> {
-    todo!()
+    if n == 1 {
+        vec![1]
+    } else {
+        let left = beautiful_array(n / 2);
+        let right = beautiful_array((n + 1) / 2);
+        left.into_iter()
+            .map(|x| x * 2)
+            .chain(right.into_iter().map(|x| x * 2 - 1))
+            .collect()
+    }
 }
 // divide_and_conquer
 #[test]
-#[ignore]
 fn test1_932() {
-    assert_eq!(beautiful_array(4), vec![2, 1, 4, 3]);
-    assert_eq!(beautiful_array(5), vec![3, 1, 2, 5, 4]);
+    assert_eq!(beautiful_array(5), vec![4, 2, 3, 5, 1]);
+    assert_eq!(beautiful_array(4), vec![4, 2, 3, 1]);
 }
