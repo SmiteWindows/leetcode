a8bea4b4a595af46a09915a25efc55454d45e943
SmiteWindows <x1627283564@hotmail.com>
2020-07-19 07:58:53 +0800

    202007190758

--- src/brainteaser/leetcode777.rs
+++ src/brainteaser/leetcode777.rs
@@ -7,30 +7,42 @@ pub fn can_transform(start: String, end: String) -> bool {
     while let (Some(mut a), Some(mut b)) = (iter_a.next(), iter_b.next()) {
         while a == 'X' {
             count_a += 1;
-            a = if let Some(c) = iter_a.next() { c } else { 'Y' };
+            a = if let Some(c) = iter_a.next() { c } else { ' ' };
         }
         while b == 'X' {
             count_b += 1;
-            b = if let Some(c) = iter_b.next() { c } else { 'Y' };
+            b = if let Some(c) = iter_b.next() { c } else { ' ' };
         }
         if a != b {
+            dbg!(count_a);
+            dbg!(count_b);
             return false;
         }
-        if a == 'R' && count_a > count_b {
+        if a == 'R' && b == 'R' && count_a > count_b {
+            dbg!(count_a);
+            dbg!(count_b);
             return false;
         }
-        if a == 'L' && count_a < count_b {
+        if a == 'L' && b == 'L' && count_a < count_b {
+            dbg!(count_a);
+            dbg!(count_b);
             return false;
         }
     }
+    dbg!(count_a);
+    dbg!(count_b);
     true
 }
 // brainteaser
 #[test]
 #[ignore]
 fn test1_777() {
+    // assert_eq!(
+    //     can_transform(String::from("RXR"), String::from("XXR")),
+    //     false,
+    // );
     assert_eq!(
+        can_transform(String::from("RXXLRXRXL"), String::from("XRLXXRRLX")),
         true,
-        can_transform(String::from("RXXLRXRXL"), String::from("XRLXXRRLX"))
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("Hello, Rust!");
     println!("Hello, LeetCode!");
-    println!("1516");
+    println!("1517");
 }
