51b6d2aef31943671d389dff0bf6e5eb0d7f6d67
SmiteWindows <x1627283564@hotmail.com>
2020-08-10 19:53:21 +0800

    202008101953

--- leetcode/src/divide_and_conquer/leetcode282.rs
+++ leetcode/src/divide_and_conquer/leetcode282.rs
@@ -1,33 +1,81 @@
 // https://leetcode.com/problems/expression-add-operators/
+// Runtime: 8 ms
+// Memory Usage: 2.3 MB
 pub fn add_operators(num: String, target: i32) -> Vec<String> {
-    todo!()
+    let target = target as i64;
+    let num = num.as_bytes();
+    let len = num.len();
+    let mut path = Vec::new();
+    let mut pathes = vec![];
+    if len > 0 {
+        let mut v = num[0] as i64 - 48;
+        path.push(num[0]);
+        dfs(&num[1..], target, 0, v, &mut path, &mut pathes);
+        if v != 0 {
+            for i in 1..len {
+                path.push(num[i]);
+                v = v * 10 + num[i] as i64 - 48;
+                dfs(&num[i + 1..], target, 0, v, &mut path, &mut pathes);
+            }
+        }
+    }
+    pathes
+}
+
+fn dfs(num: &[u8], target: i64, v1: i64, v2: i64, path: &mut Vec<u8>, pathes: &mut Vec<String>) {
+    let len = num.len();
+    if len == 0 {
+        if v1 + v2 == target {
+            pathes.push(unsafe { String::from_utf8_unchecked(path.clone()) });
+        }
+        return;
+    }
+    let sign_idx = path.len();
+    let mut v = num[0] as i64 - 48;
+    path.push('+' as u8);
+    path.push(num[0]);
+
+    let mut try_ops = |path: &mut Vec<u8>, v, i| {
+        path[sign_idx] = '+' as u8;
+        dfs(&num[i..], target, v1 + v2, v, path, pathes);
+        path[sign_idx] = '-' as u8;
+        dfs(&num[i..], target, v1 + v2, -v, path, pathes);
+        path[sign_idx] = '*' as u8;
+        dfs(&num[i..], target, v1, v2 * v, path, pathes);
+    };
+
+    try_ops(path, v, 1);
+    if v != 0 {
+        for i in 1..len {
+            path.push(num[i]);
+            v = v * 10 + num[i] as i64 - 48;
+            try_ops(path, v, i + 1);
+        }
+    }
+    path.resize(sign_idx, 0);
 }
 // divide_and_conquer
 #[test]
-#[ignore]
-fn test1_241() {
-    assert_eq!(
+fn test1_282() {
+    use leetcode_prelude::{assert_eq_sorted, vec_string};
+    assert_eq_sorted!(
         add_operators(String::from("123"), 6),
-        vec![String::from("1+2+3"), String::from("1*2*3")]
+        vec_string!["1+2+3", "1*2*3"]
     );
-    assert_eq!(
+    assert_eq_sorted!(
         add_operators(String::from("232"), 8),
-        vec![String::from("2*3+2"), String::from("2+3*2")]
+        vec_string!["2*3+2", "2+3*2"]
     );
-    assert_eq!(
+    assert_eq_sorted!(
         add_operators(String::from("105"), 5),
-        vec![String::from("1*0+5"), String::from("10-5")]
+        vec_string!["1*0+5", "10-5"]
     );
-    assert_eq!(
+    assert_eq_sorted!(
         add_operators(String::from("00"), 0),
-        vec![
-            String::from("0+0"),
-            String::from("0-0"),
-            String::from("0*0")
-        ]
+        vec_string!["0+0", "0-0", "0*0"]
     );
-    assert_eq!(
+    assert_eq_sorted!(
         add_operators(String::from("3456237490"), 9191),
-        vec![String::new()]
+        vec![] as Vec<String>
     );
 }
