42b454151e03a801303518fd030a88d66128b3a0
SmiteWindows <x1627283564@hotmail.com>
2020-08-23 09:25:39 +0800

    202008230925

--- leetcode/src/string/leetcode1556.rs
+++ leetcode/src/string/leetcode1556.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/thousand-separator/
+// Runtime: 0 ms
+// Memory Usage: 2.1 MB
 pub fn thousand_separator(n: i32) -> String {
-    todo!()
+    let mut characters: Vec<_> = n.to_string().chars().rev().collect();
+    let mut count = 0;
+    let mut i = 0;
+    while i < characters.len() {
+        if count == 3 {
+            characters.insert(i, '.');
+            count = -1;
+        }
+        count += 1;
+        i += 1;
+    }
+    characters.into_iter().rev().collect()
 }
 // string
 #[test]
-#[ignore]
 fn test1_1556() {
     assert_eq!(thousand_separator(987), "987");
     assert_eq!(thousand_separator(1234), "1.234");
