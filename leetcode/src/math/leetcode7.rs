a79723b4586d5f5d51282967b71eecfdcffeb0d9
SmiteWindows <x1627283564@hotmail.com>
2021-07-22 16:46:14 +0800

    20210722

--- leetcode/src/math/leetcode7.rs
+++ leetcode/src/math/leetcode7.rs
@@ -1,20 +1,16 @@
 // https://leetcode-cn.com/problems/reverse-integer/
 // Runtime: 0 ms
 // Memory Usage: 2 MB
-pub fn reverse(mut x: i32) -> i32 {
-    let mut rev = 0;
-    while x != 0 {
-        let pop = x % 10;
-        x /= 10;
-        if rev > i32::MAX / 10 || rev == i32::MAX / 10 && pop > 7 {
-            return 0;
+pub fn reverse(x: i32) -> i32 {
+    fn helper(mut n: i32) -> Option<i32> {
+        let mut res = 0i32;
+        while n.abs() != 0 {
+            res = res.checked_mul(10)?.checked_add(n % 10)?;
+            n /= 10;
         }
-        if rev < i32::MIN / 10 || rev == i32::MIN / 10 && pop < (-8) {
-            return 0;
-        }
-        rev = rev * 10 + pop;
+        Some(res)
     }
-    rev
+    helper(x).unwrap_or_default()
 }
 // math
 #[test]
